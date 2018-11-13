53dc05f2a34d2ca9dbdf043fbf615caa325bc287
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-11-13 17:28:39 -0600

    use jest-puppeteer via custom jest config

--- src/index.browserspec.ts
+++ src/index.browserspec.ts
@@ -4,8 +4,11 @@ import portfinder from 'portfinder';
 import puppeteer from 'puppeteer';
 import staticServer from 'serve-handler';
 
+// jest-puppeteer will expose the `page` and `browser` globals to Jest tests.
+// declare const browser: puppeteer.Browser
+declare const page: puppeteer.Page;
+
 class SinglePage {
-    public readonly page: puppeteer.Page;
     public messages: {
         warning: string[]
         log: string[]
@@ -13,8 +16,7 @@ class SinglePage {
         debug: string[]
         error: string[]
     };
-    constructor(page: puppeteer.Page) {
-        this.page = page;
+    constructor() {
         this.messages = {
             debug: [],
             error: [],
@@ -23,7 +25,7 @@ class SinglePage {
             warning: [],
         };
         // collect browser console messages to be checked later
-        this.page.on('console', (consoleMessage) => {
+        page.on('console', (consoleMessage) => {
             const type = consoleMessage.type();
             const text = consoleMessage.text();
 
@@ -40,30 +42,17 @@ class SinglePage {
             }
         });
 
-        this.page.on('pageerror', async(e) => {
+        page.on('pageerror', async(e) => {
             throw e;
         });
     }
-}
-
-async function startBrowser(browser: puppeteer.Browser, url: string) {
-
-    const page = await browser.newPage();
-
-    const wrapper = new SinglePage(page);
-    await wrapper.page.goto(url);
-
-    // Wait until React has started
-    await wrapper.page.waitForSelector('#root > h1');
-    return wrapper;
 }
 
 describe('Browser sanity tests', () => {
 
     let devServer: http.Server | null = null;
     let devServerPort = 0;
-    let browser: puppeteer.Browser | null = null;
-    let wrapper: SinglePage | null = null;
+    let consoleMessages: Array<{type: 'debug' | 'error' | 'info' | 'log' | 'warning', message: string}> = [];
 
     beforeAll(async() => {
         // pick an available port
@@ -73,61 +62,56 @@ describe('Browser sanity tests', () => {
             staticServer(request, response, {public: path.join(__dirname, '../build')});
         });
         devServer.listen(devServerPort);
-
-        const puppeteerArgs = [];
-        // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
-        if (process.env.CI === 'true') {
-            puppeteerArgs.push('--no-sandbox');
-        }
-
-        browser = await puppeteer.launch({
-            args: puppeteerArgs,
-            devtools: process.env.NODE_ENV === 'development',
-        });
-
     });
 
     afterAll(async() => {
         if (devServer) {
             devServer.close();
         }
-        if (browser) {
-            await browser.close();
-        }
     });
 
-    // beforeEach does not seem to work well with async functions
-    const before = async() => {
+    beforeEach(async() => {
+        consoleMessages = [];
+
+        page.on('console', (consoleMessage) => {
+            const type = consoleMessage.type();
+            const text = consoleMessage.text();
+
+            switch (type) {
+                case 'debug':
+                case 'error':
+                case 'info':
+                case 'log':
+                case 'warning':
+                    consoleMessages.push({type, message: text});
+                    break;
+                default:
+                    throw new Error(`BUG: Unsupported console type: '${type}'`);
+            }
+        });
+
         jest.setTimeout(5 * 60 * 1000); // dev browser takes a while to spin up
 
         const url = `http://localhost:${devServerPort}/`;
-        if (!browser) {
-            throw new Error(`BUG: Browser has not been initialized. check beforeAll(...)`);
-        }
-        wrapper = await startBrowser(browser, url);
-        return wrapper;
-    };
 
-    afterEach(async() => {
-        if (wrapper) {
-            await wrapper.page.close();
-            wrapper = null;
-        }
+        await page.goto(url);
+        // Wait until React has started
+        await page.waitForSelector('#root > h1');
+
     });
 
     it('displays the "Hello developer" console text', async() => {
-        const wrap = await before();
-        expect(wrap.messages.info.length).toBe(1);
+        const infoMessages = consoleMessages.filter(({type}) => type === 'info');
+        expect(infoMessages.length).toBe(1);
         const str = [
             '%cHowdy! If you want to help out, the source code can be found at ',
             'https://github.com/openstax/books-web',
             ' font-weight:bold',
         ];
-        expect(wrap.messages.info[0]).toBe(str.join(''));
+        expect(infoMessages[0].message).toBe(str.join(''));
     });
 
     it('loads the 404 page', async() => {
-        const {page} = await before();
         // const heading = await page.$eval('#root > h1', (el) => el.text)
         const heading = await (page.evaluate(() => {
             if (document) {
