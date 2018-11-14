9627bc94e2017de3e9599da402fa33792e79c2cf
tom <tom.g.woodward@gmail.com>
2018-11-14 15:43:47 -0500

    add a friendly reminder if jest environment is not set

--- src/helpers/testUtils.ts
+++ src/helpers/testUtils.ts
@@ -4,6 +4,10 @@ import puppeteer from 'puppeteer';
 const browser = (global as any).browser as puppeteer.Browser;
 const page = (global as any).page as puppeteer.Page;
 
+if (!browser || !page) {
+  throw new Error('Browser has not been started! Did you remember to specify `@jest-environment puppeteer`?');
+}
+
 export {
   browser,
   page,
