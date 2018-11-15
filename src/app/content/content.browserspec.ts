d1b70a1f37acf2cdf599596200faa2f8d90a72b3
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-11-15 12:20:19 -0600

    get the computed style of the heading
    to help identify screenshot discrepancies

--- src/app/content/content.browserspec.ts
+++ src/app/content/content.browserspec.ts
@@ -9,6 +9,22 @@ describe('content', () => {
 
   it('looks right', async() => {
     const screen = await page.screenshot({fullPage: true});
+    const headingStyle = await page.evaluate(() => {
+        if (window) {
+            const headings = window.document.querySelectorAll('h1');
+            if (headings.length !== 1) {
+                throw new Error(`BUG: Expected exactly 1 <h1> element but found ${headings.length}`);
+            }
+            const compStyle = window.getComputedStyle(headings[0]);
+            const styleMap: {[name: string]: string} = {};
+            for (let index = 0; index < compStyle.length; index++) { // tslint:disable-line:prefer-for-of
+                const styleName = compStyle[index];
+                styleMap[styleName] = compStyle.getPropertyValue(styleName);
+            }
+            return styleMap;
+        }
+    });
+    expect(headingStyle).toMatchSnapshot();
     expect(screen).toMatchImageSnapshot();
   });
 });
