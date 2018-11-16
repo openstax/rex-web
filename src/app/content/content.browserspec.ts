6576575c8c8915340f9b4ece6fee666b6985ed92
tom <tom.g.woodward@gmail.com>
2018-11-16 11:06:41 -0500

    working on snapshot matching

--- src/app/content/content.browserspec.ts
+++ src/app/content/content.browserspec.ts
@@ -1,8 +1,7 @@
 /** @jest-environment puppeteer */
-import { finishRender, getComputedStyle, navigate, page } from '../../test/browserutils';
+import { finishRender, navigate, page } from '../../test/browserutils';
 
 describe('content', () => {
-
   beforeEach(async() => {
     await navigate(page, '/books/Ax2o07Ul/pages/M_qlK4M9');
   });
@@ -10,8 +9,8 @@ describe('content', () => {
   it('looks right', async() => {
     await finishRender(page);
     const screen = await page.screenshot({fullPage: true});
-    const headingStyle = await getComputedStyle(page, 'h1');
-    expect(headingStyle).toMatchSnapshot();
+    // const headingStyle = await getComputedStyle(page, 'h1');
+    // expect(headingStyle).toMatchSnapshot();
     expect(screen).toMatchImageSnapshot();
   }, 10000);
 });
