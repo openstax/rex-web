19d7090f4a213f47b0ee7519754057a0fb20de26
tom <tom.g.woodward@gmail.com>
2018-11-19 13:46:00 -0500

    remove extra styles from content wrapper

--- src/app/content/components/Wrapper.tsx
+++ src/app/content/components/Wrapper.tsx
@@ -7,14 +7,6 @@ export default styled.div`
   min-height: 6rem;
   outline: none;
 
-  // this stuff could be in a reset.css
-  h1 {
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0.4);
-    color-interpolation: sRGB;
-    color-interpolation-filters: linearRGB;
-    word-wrap: normal;
-  }
-
   // these are only here because the cnx-recipes styles are broken
   font-size: 18px;
   font-family: "Noto Sans", sans-serif;
