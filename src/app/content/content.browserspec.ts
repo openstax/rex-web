f3d896f9c90fc41c96c28990d5ead55fe879304e
tom <tom.g.woodward@gmail.com>
2018-11-19 07:23:15 -0500

    jiggering test timeouts

--- src/app/content/content.browserspec.ts
+++ src/app/content/content.browserspec.ts
@@ -4,17 +4,15 @@ import { finishRender, navigate, page } from '../../test/browserutils';
 describe('content', () => {
   beforeEach(async() => {
     await navigate(page, '/books/Ax2o07Ul/pages/M_qlK4M9');
-  }, 60000);
+    await finishRender(page);
+  });
 
   it('looks right', async() => {
-    await finishRender(page);
     const screen = await page.screenshot({fullPage: true});
-    // const headingStyle = await getComputedStyle(page, 'h1');
-    // expect(headingStyle).toMatchSnapshot();
     (expect(screen).toMatchImageSnapshot as any)({
       failureThreshold: 2,
       failureThresholdType: 'percent',
       updatePassedSnapshot: true,
     });
-  }, 60000);
+  });
 });
