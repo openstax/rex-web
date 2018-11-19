35d9088981e6ae9ac00c666ac3f4eb15ad80f291
tom <tom.g.woodward@gmail.com>
2018-11-19 14:18:11 -0500

    remove some timeouts that might not be necessary

--- src/app/content/content.browserspec.ts
+++ src/app/content/content.browserspec.ts
@@ -1,10 +1,9 @@
 /** @jest-environment puppeteer */
-import { finishRender, navigate } from '../../test/browserutils';
+import { navigate } from '../../test/browserutils';
 
 describe('content', () => {
   beforeEach(async() => {
     await navigate(page, '/books/Ax2o07Ul/pages/M_qlK4M9');
-    await finishRender(page);
   });
 
   it('looks right', async() => {
--- src/test/browserutils.ts
+++ src/test/browserutils.ts
@@ -29,20 +29,11 @@ export const url = (path: string) => `http://localhost:${DEV_SERVER_PORT}/${path
 export const navigate = async(target: puppeteer.Page, path: string) => {
   await target.goto(url(path));
 
-  // HACK - add delay to make sure promises have been registered
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
   await target.evaluate(async() => {
     if (window) {
       await window.__APP_ASYNC_HOOKS.calm();
     }
   });
-};
-
-export const finishRender = async(_: puppeteer.Page) => {
-  // HACK - there is no convenient way to tell if chrome is finished rendering,
-  // we should investigate inconvenient possibilities.
-  await new Promise((resolve) => setTimeout(resolve, 1000));
 };
 
 // tslint:disable-next-line:no-shadowed-variable
