0f2362ef15f5b452ecd6415795f537e09f61ee2d
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-11-27 15:08:49 -0600

    address concerns

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const http = require('http');
 const serveStatic = require('serve-static')
 const path = require('path');
 const setupProxy = require('../src/setupProxy');
-require('dotenv').config();
+require('../src/env');
+
+const SERVER_PORT = process.env.SERVER_PORT;
+
+if (Number.isNaN(SERVER_PORT)) {
+  throw new Error(`BUG: SERVER_PORT is not defined. Add it to .env.${process.env.NODE_ENV}`)
+}
 
 module.exports = (options = {}) => new Promise(resolve => {
   const fallback404 = !!options.fallback404;
-
-  const port = process.env.SERVER_PORT
 
   const serve = serveStatic(path.join(__dirname, '../build'));
   const fallback = (req, res, next) => serve(Object.assign({}, req, {url: '/'}), res, next);
@@ -25,5 +29,5 @@ module.exports = (options = {}) => new Promise(resolve => {
 
   const server = http.createServer(app);
 
-  server.listen(port, () => resolve({server, port}));
+  server.listen(SERVER_PORT, () => resolve({server, port: SERVER_PORT}));
 });
