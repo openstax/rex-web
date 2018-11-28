6d50b35216cc77121a54612e15fb528581ed7970
tom <tom.g.woodward@gmail.com>
2018-11-28 08:25:10 -0500

    double navigation timeout in CI

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -14,10 +14,18 @@ module.exports = {
     args: [
       // https://github.com/GoogleChrome/puppeteer/issues/2410
       '--font-render-hinting=medium',
+
+      // https://github.com/GoogleChrome/puppeteer/issues/3120#issuecomment-415553869
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--single-process',
     ],
   },
   server: {
-    protocol: 'http',
     launchTimeout: 60000,
     // react-scripts start unconditionally sets the NODE_ENV to development,
     // so we're setting CI here even if that isn't necessarily true
--- src/test/browserutils.ts
+++ src/test/browserutils.ts
@@ -31,7 +31,12 @@ if (process.env.CI) {
 export const url = (path: string) => `http://localhost:${puppeteerConfig.server.port}/${path.replace(/^\/+/, '')}`;
 
 export const navigate = async(target: puppeteer.Page, path: string) => {
-  await target.goto(url(path));
+  await target.goto(url(path), {
+    waitUntil: 'networkidle2',
+  });
+
+  // wait to make sure promises are registered.
+  await new Promise((resolve) => setTimeout(resolve, 10));
 
   await target.evaluate(async() => {
     if (window) {
