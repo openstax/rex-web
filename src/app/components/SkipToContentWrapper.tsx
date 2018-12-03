dee31ab26333a49a7ce18f5e5a71777066ac4ecb
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-12-03 15:31:05 -0600

    feat: add Skip to Content link (#19)
    * add Skip to Content link
    
    which is hidden until it is tabbed to. When the link is clicked it scrolls down to the beginning of the content
    
    * Update Content.spec.tsx.snap
    
    * :shirt: lint
    
    * use JS click handler
    
    * contexty scrolltocontent
    
    * clean up scrollTo signature
    
    * test inputs
    
    * verify focus changed
    
    and increase code coverage
    
    * verify scroll occurred
    
    * :shirt: lint
    
    * increase timeout
    
    * :white_check_mark: verify SkipToContent is first tabbed element
    
    * fix travis
    
    * suppress jsdom console.errors
    
    * :shirt: lint
    
    * move expectError into utils

---
+++ src/app/components/SkipToContentWrapper.tsx
@@ -0,0 +1,28 @@
+import React, { Component, RefObject } from 'react';
+import scrollTo from '../../helpers/scrollTo';
+import { MAIN_CONTENT_ID, Provider } from '../context/SkipToContent';
+import HiddenLink from './HiddenLink';
+
+export default class SkipToContentWrapper extends Component {
+  public mainContent: RefObject<any> | undefined;
+
+  public render() {
+    return <Provider value={{registerMainContent: this.registerMainContent}}>
+      <HiddenLink onClick={this.scrollToTarget} href={`#${MAIN_CONTENT_ID}`} tabIndex={1}>Skip to Content</HiddenLink>
+      {this.props.children}
+    </Provider>;
+  }
+
+  private scrollToTarget = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    if (this.mainContent) {
+      event.preventDefault();
+      scrollTo(this.mainContent);
+    } else {
+      throw new Error(`BUG: Expected mainComponent to be defined. Does SkipToContentWrapper contain a MainContent?`);
+    }
+  }
+
+  private registerMainContent = (mainContent: RefObject<any>) => {
+    this.mainContent = mainContent;
+  }
+}
