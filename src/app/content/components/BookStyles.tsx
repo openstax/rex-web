27cb476d6641503791c9b9e3553e50fc09619a83
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-12-03 15:09:13 -0600

    remove unnecessary lint comments

--- src/app/content/components/BookStyles.tsx
+++ src/app/content/components/BookStyles.tsx
@@ -2,7 +2,6 @@ import bookStyles from 'cnx-recipes/styles/output/intro-business.json';
 import { ReactElement } from 'react';
 import styled from 'styled-components';
 
-// tslint:disable-next-line:variable-name
 const hoc = ({children, className}: {className?: string, children: (className?: string) => ReactElement<any>}) =>
   children(className);
 
