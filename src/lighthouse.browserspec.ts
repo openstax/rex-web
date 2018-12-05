256272b5eb83efa7e9349cf8ebe34856d95cdda7
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-12-05 17:24:08 -0600

    audit TEST_PAGE

--- src/lighthouse.browserspec.ts
+++ src/lighthouse.browserspec.ts
@@ -5,6 +5,8 @@ import fetch from 'node-fetch';
 import puppeteer from 'puppeteer';
 import { url } from './test/browserutils';
 
+const TEST_PAGE = '/books/testbook1-shortid/pages/testpage1-shortid';
+
 describe('Lighthouse audits', () => {
   let chrome: chromeLauncher.LaunchedChrome | undefined;
 
@@ -32,11 +34,28 @@ describe('Lighthouse audits', () => {
     // Run Lighthouse.
     const { lhr } = await lighthouse(url('/'), {port: chrome.port}, null);
 
-    expect(lhr.categories.accessibility.score).toBe(1);
+    expect(lhr.categories.accessibility.score).toBeGreaterThanOrEqual(1);
     expect(lhr.categories.seo.score).toBeGreaterThanOrEqual(0.8);
     expect(lhr.categories.pwa.score).toBeGreaterThanOrEqual(0.5);
     expect(lhr.categories['best-practices'].score).toBeGreaterThanOrEqual(0.93);
     // This one depends on how fast chrome executes so maybe we should drop it
     expect(lhr.categories.performance.score).toBeGreaterThanOrEqual(0.4);
   });
+
+  it('reports about the test content page', async() => {
+    if (!chrome) {
+      return expect(chrome).toBeTruthy();
+    }
+
+    // Run Lighthouse.
+    const { lhr } = await lighthouse(url(TEST_PAGE), {port: chrome.port}, null);
+
+    expect(lhr.categories.accessibility.score).toBeGreaterThanOrEqual(0.95);
+    expect(lhr.categories.seo.score).toBeGreaterThanOrEqual(0.8);
+    expect(lhr.categories.pwa.score).toBeGreaterThanOrEqual(0.5);
+    expect(lhr.categories['best-practices'].score).toBeGreaterThanOrEqual(0.93);
+    // This one depends on how fast chrome executes so maybe we should drop it
+    expect(lhr.categories.performance.score).toBeGreaterThanOrEqual(0.4);
+  });
+
 });
