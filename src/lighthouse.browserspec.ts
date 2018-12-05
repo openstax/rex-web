2ee41dfe10469e44f57f443944cd84db6ad92a00
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-12-05 17:16:33 -0600

    add lighthouse

---
+++ src/lighthouse.browserspec.ts
@@ -0,0 +1,42 @@
+/** @jest-environment puppeteer */
+import * as chromeLauncher from 'chrome-launcher';
+import lighthouse from 'lighthouse';
+import fetch from 'node-fetch';
+import puppeteer from 'puppeteer';
+import { url } from './test/browserutils';
+
+describe('Lighthouse audits', () => {
+  let chrome: chromeLauncher.LaunchedChrome | undefined;
+
+  beforeAll(async() => {
+
+    chrome = await chromeLauncher.launch({
+      chromeFlags: ['--headless'],
+    });
+
+    // Connect to puppeteer
+    const resp = await fetch(`http://localhost:${chrome.port}/json/version`);
+    const { webSocketDebuggerUrl } = await resp.json();
+    browser = await puppeteer.connect({browserWSEndpoint: webSocketDebuggerUrl});
+  });
+
+  afterAll(async() => {
+    return chrome && await chrome.kill();
+  });
+
+  it('reports about the root page', async() => {
+    if (!chrome) {
+      return expect(chrome).toBeTruthy();
+    }
+
+    // Run Lighthouse.
+    const { lhr } = await lighthouse(url('/'), {port: chrome.port}, null);
+
+    expect(lhr.categories.accessibility.score).toBe(1);
+    expect(lhr.categories.seo.score).toBeGreaterThanOrEqual(0.8);
+    expect(lhr.categories.pwa.score).toBeGreaterThanOrEqual(0.5);
+    expect(lhr.categories['best-practices'].score).toBeGreaterThanOrEqual(0.93);
+    // This one depends on how fast chrome executes so maybe we should drop it
+    expect(lhr.categories.performance.score).toBeGreaterThanOrEqual(0.4);
+  });
+});
