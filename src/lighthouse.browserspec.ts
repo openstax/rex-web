b608321ec5aa006a17298c19ac1858dc1128b1cf
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-12-06 13:03:58 -0600

    :fire: chrome-launcher

--- src/lighthouse.browserspec.ts
+++ src/lighthouse.browserspec.ts
@@ -1,19 +1,14 @@
 /** @jest-environment puppeteer */
-import * as chromeLauncher from 'chrome-launcher';
 import lighthouse from 'lighthouse';
 import { url } from './test/browserutils';
 
 const TEST_PAGE = '/books/testbook1-shortid/pages/testpage1-shortid';
 
 describe('Lighthouse audits', () => {
-  let chrome: chromeLauncher.LaunchedChrome | undefined;
-
   const checkLighthouse = async(urlPath: string) => {
-    if (!chrome) {
-      return expect(chrome).toBeTruthy();
-    }
 
-    const { lhr } = await lighthouse(url(urlPath), {port: chrome.port}, null);
+    const port = (new URL(browser.wsEndpoint())).port
+    const { lhr } = await lighthouse(url(urlPath), {port}, null);
 
     expect(lhr.categories.accessibility.score).toBeGreaterThanOrEqual(1);
     expect(lhr.categories.seo.score).toBeGreaterThanOrEqual(0.8);
@@ -22,22 +17,6 @@ describe('Lighthouse audits', () => {
     // This one depends on how fast chrome executes so maybe we should drop it
     expect(lhr.categories.performance.score).toBeGreaterThanOrEqual(0.4);
   };
-
-  beforeAll(async() => {
-
-    chrome = await chromeLauncher.launch({
-      chromeFlags: ['--headless'],
-    });
-
-    // Connect to puppeteer
-    // const resp = await fetch(`http://localhost:${chrome.port}/json/version`);
-    // const { webSocketDebuggerUrl } = await resp.json();
-    // browser = await puppeteer.connect({browserWSEndpoint: webSocketDebuggerUrl});
-  });
-
-  afterAll(async() => {
-    return chrome && await chrome.kill();
-  });
 
   it('reports about the root page', async() => {
     await checkLighthouse('/');
