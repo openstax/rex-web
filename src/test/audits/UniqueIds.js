d8aebea4fce7ae136ca2c2145acf113362bf1ea8
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-12-11 15:41:13 -0600

    chore: add custom Lighthouse audits (#47)
    * add custom audit category
    
    * separate audit files

---
+++ src/test/audits/UniqueIds.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Gatherer } = require('lighthouse');
+
+module.exports = class UniqueIds extends Gatherer {
+  static get meta() {
+    return {
+      id: UniqueIds.name,
+      title: 'Element ids are unique',
+      failureTitle: 'Duplicate or invalid id',
+      description: 'Used to ensure that each id attribute is unique to the page.',
+
+      // The name of the custom gatherer class that provides input to this audit.
+      requiredArtifacts: [UniqueIds.name],
+    };
+  }
+
+  static audit(artifacts) {
+    const ids = artifacts[UniqueIds.name];
+    const areUnique = ids.length === new Set(ids).size;
+    return {
+      rawValue: ids,
+      score: Number(areUnique),
+    };
+  }
+
+  // Gatherer function
+  async afterPass({ driver }) {
+    const fn = () => {
+      const elementsWithId = document.querySelectorAll('[id]');
+      return [...elementsWithId].map(el => el.getAttribute('id'))
+    };
+    return driver.evaluateAsync(`((${fn.toString()}) ())`);
+  }
+}
