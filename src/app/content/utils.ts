74fbf7026a6b2a245b5c9395a42d35b8fd4606c7
tom <tom.g.woodward@gmail.com>
2018-12-13 06:04:57 -0500

    :shirt: style

--- src/app/content/utils.ts
+++ src/app/content/utils.ts
@@ -1,7 +1,7 @@
+import { HTMLElement } from '@openstax/types/lib.dom';
 import flatten from 'lodash/fp/flatten';
 import { isArchiveTree } from './guards';
 import { ArchiveTree, ArchiveTreeSection } from './types';
-import { HTMLElement } from '@openstax/types/lib.dom';
 
 export const stripIdVersion = (id: string): string => id.split('@')[0];
 export const getIdVersion = (id: string): string | undefined => id.split('@')[1];
