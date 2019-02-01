21390de6e5afba489cfb63be89173c877a0c3ce5
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-01 11:56:44 -0600

    move messages into json file

--- src/app/MessageProvider.tsx
+++ src/app/MessageProvider.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { addLocaleData, IntlProvider } from 'react-intl';
 import en from 'react-intl/locale-data/en';
+import enMessages from './messages.en.json';
 
 addLocaleData(en);
 
-const messages = {
-  'i18n:404': 'page not found',
-  'i18n:a11y:skipToContent': 'Skip to Content',
-  'i18n:toc:bookDetails': 'book details',
-  'i18n:toc:title': 'Table of Contents',
-  'i18n:toc:toggle:closed': 'Click to open the Table of Contents',
-  'i18n:toc:toggle:opened': 'Click to close the Table of Contents',
-};
-
 // tslint:disable-next-line:variable-name
 const MessageProvider: React.SFC = (props) =>
-  <IntlProvider locale='en' messages={messages}>{props.children}</IntlProvider>;
+  <IntlProvider locale='en' messages={enMessages}>{props.children}</IntlProvider>;
 
 export default MessageProvider;
