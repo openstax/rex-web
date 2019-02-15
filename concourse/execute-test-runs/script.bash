0dd354441938ded3aac3c4e935710d09c85f738e
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-15 12:59:32 -0600

    fix milestone number

--- concourse/execute-test-runs/script.bash
+++ concourse/execute-test-runs/script.bash
@@ -5,15 +5,17 @@ BASE_DIR=$(pwd)
 
 cd rex-web
 
-for MILESTONE_NUMBER in "$BASE_DIR"/unified-issues-milestones/*
+for MILESTONE_NUMBER_PATH in "$BASE_DIR"/unified-issues-milestones/*
 do
+  MILESTONE_NUMBER=$(basename "$MILESTONE_NUMBER_PATH")
   ENV_NAME=release-$MILESTONE_NUMBER
   export BASE_URL=https://$ENV_NAME.sandbox.openstax.org
 
   echo "checking for runs using $BASE_URL"
 
-  for TR_RUN_ID in "$BASE_DIR"/test-plans/*
+  for TR_RUN_PATH in "$BASE_DIR"/test-plans/*
   do
+    TR_RUN_ID=$(basename "$TR_RUN_PATH")
     base_url=$(< "$BASE_DIR/test-plans/$TR_RUN_ID/base_url.txt")
     
     if [[ "$BASE_URL" == "$base_url" ]]; then
