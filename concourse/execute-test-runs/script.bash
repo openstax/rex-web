cdfa9c60674dc12618a7482a69bb9aa61b7d5887
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-02-15 14:25:47 -0600

    reuse tr_run_dir

--- concourse/execute-test-runs/script.bash
+++ concourse/execute-test-runs/script.bash
@@ -5,23 +5,23 @@ base_dir=$(pwd)
 
 cd rex-web
 
-for milestone_number_path in "$base_dir"/unified-issues-milestones/*
+for milestone_number_dir in "$base_dir"/unified-issues-milestones/*
 do
-  milestone_number=$(basename "$milestone_number_path")
+  milestone_number=$(basename "$milestone_number_dir")
   env_name=release-$milestone_number
   export BASE_URL=https://$env_name.sandbox.openstax.org
 
   echo "checking for runs using $BASE_URL"
 
-  for tr_run_path in "$base_dir"/test-plans/*
+  for tr_run_dir in "$base_dir"/test-plans/*
   do
-    tr_run_id=$(basename "$tr_run_path")
-    base_url=$(< "$base_dir/test-plans/$tr_run_id/base_url.txt")
+    tr_run_id=$(basename "$tr_run_dir")
+    base_url=$(< "$tr_run_dir/base_url.txt")
     
     if [[ "$BASE_URL" == "$base_url" ]]; then
       echo "found test plan $tr_run_id"
 
-      browser=$(< "$base_dir/test-plans/$tr_run_id/browser.txt")
+      browser=$(< "$tr_run_dir/browser.txt")
       export TR_RUN_ID=$tr_run_id
       export BROWSER=$browser
 
