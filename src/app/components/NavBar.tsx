dfd99a36f878bfb96d36c07baf5e63ae9212e92a
Karina Mendez <karinamendez132@gmail.com>
2019-02-27 18:34:05 -0600

    centered content

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,60 +2,23 @@ import React, { SFC } from 'react';
 import styled from 'styled-components';
 import openstaxLogo from '../../assets/logo.svg';
 
-//import theme, { ColorSet } from '../theme';
-
-/*const applyColor = (color: ColorSet) => `
-  color: ${color.foreground};
-  background-color: ${color.base};
-
-  :hover {
-    background-color: ${color.darker};
-  }
-  :active {
-    background-color: ${color.darkest};
-  }
-`;*/
-
 const TopBar = styled.div`
-    /* -webkit-align-items: baseline; */
-    /* -webkit-box-align: baseline; */
-    -ms-flex-align: baseline;
-    /* align-items: baseline; */
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
     display: flex;
-    -webkit-box-pack: justify;
-    -webkit-justify-content: space-between;
-    -ms-flex-pack: justify;
     justify-content: space-between;
-    /* box-sizing: border-box; */
     cursor: pointer;
-    display: -webkit-inline-box;
-    /* display: -webkit-inline-flex; */
-    display: -ms-inline-flexbox;
-    /* display: inline-flex; */
     height: 5rem;
-    padding: 0 135px;
-    -webkit-text-decoration: none;
-    text-decoration: none;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    white-space: nowrap;
     width: 100%;
     max-width: 1170px;
     font-size: 1.25rem;
     font-weight: bold;
     background: #FFFFFF;
-    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 1);
+    margin: 0 auto;
 `;
 
 const HeaderImage = styled.img`
-width: auto;
-height: 3rem;
-margin-top: 1rem;
+    width: auto;
+    height: 3rem;
+    margin-top: 1rem;
 `;
 
 const LoginTxt = styled.div`
@@ -76,10 +39,22 @@ const LoginTxt = styled.div`
         border-bottom: 4px solid #63a524;
     }
 `;
-const NavigationBar: SFC = ({children}) => <TopBar>
-  <HeaderImage src = {openstaxLogo}/>
-  <LoginTxt>Login</LoginTxt>
-  {children}
-</TopBar>;
+
+const BarWrapper = styled.div`
+    width: 100%;
+    text-align: center;
+    padding: 0 135px;
+    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.1);
+    display: inline-block;
+`;
+
+const NavigationBar: SFC = ({children}) => 
+    <BarWrapper>
+        <TopBar>
+            <HeaderImage src = {openstaxLogo}/>
+            <LoginTxt>Login</LoginTxt>
+            {children}
+        </TopBar>
+    </BarWrapper>;
 
 export default NavigationBar;
