71a18c1c78cce4c48b9a8bc01cb5d8ecb33dfe43
tom <tom.g.woodward@gmail.com>
2019-03-05 15:12:05 -0500

    use css helper

--- src/app/components/Typography.tsx
+++ src/app/components/Typography.tsx
@@ -1,5 +1,6 @@
+import { css } from 'styled-components';
 
-export const bodyCopyRegularStyle = `
+export const bodyCopyRegularStyle = css`
   font-family: Helvetica Neue;
   font-size: 1.6rem;
   line-height: 2.5rem;
