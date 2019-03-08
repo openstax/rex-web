6dfa65a27f16b366f150526e6dbe37831813aad1
tom <tom.g.woodward@gmail.com>
2019-03-08 12:31:56 -0500

    update developer home route name

--- src/app/developer/routes.ts
+++ src/app/developer/routes.ts
@@ -7,6 +7,6 @@ const ROUTES_PATH = '/';
 export const routes: Route<undefined, undefined> = {
   component: Home,
   getUrl: (): string => pathToRegexp.compile(ROUTES_PATH)(),
-  name: 'Developer / Routes',
+  name: 'Developer Home',
   paths: [ROUTES_PATH],
 };
