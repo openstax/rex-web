df1c4d68176f6bf9e2bd6ead81016830d2c41427
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-03-18 11:58:09 -0500

    :books: add biology

--- src/config.development.js
+++ src/config.development.js
@@ -2,8 +2,11 @@ module.exports = {
   RELEASE_ID: 'development',
   CODE_VERSION: 'development',
 
-  BOOKS: {'031da8d3-b525-429c-80cf-6c8ed997733a':{'defaultVersion':'14.4'}},
-
+  BOOKS: {
+    '031da8d3-b525-429c-80cf-6c8ed997733a':{'defaultVersion':'14.4'},
+    '8d50a0af-948b-4204-a71d-4826cba765b8':{'defaultVersion':'15.3'}
+  },
+  
   SKIP_OS_WEB_PROXY: process.env.SKIP_OS_WEB_PROXY !== undefined,
   FIXTURES: false,
   DEBUG: true,
