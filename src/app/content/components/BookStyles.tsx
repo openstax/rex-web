1054b82ef598515809ffb05640961ef974475e8b
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-03-26 15:29:08 -0500

    load files nicely

--- src/app/content/components/BookStyles.tsx
+++ src/app/content/components/BookStyles.tsx
@@ -1,10 +1,13 @@
 // tslint:disable:variable-name
-import bookStyles from 'cnx-recipes/styles/output/intro-business.json';
+import { getBookStyles } from 'cnx-recipes';
 import { ReactElement } from 'react';
 import styled from 'styled-components';
 
 const Hoc = ({children, className}: {className?: string, children: (className?: string) => ReactElement<any>}) =>
   children(className);
+
+const styles = getBookStyles();
+const bookStyles = styles.get('intro-business');
 
 export default styled(Hoc)`
   ${bookStyles}
