f3795d23118ecca426690bd2f5b65be9ef988e47
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-03-27 10:40:02 -0500

    add books

--- src/config.development.js
+++ src/config.development.js
@@ -3,8 +3,11 @@ module.exports = {
   CODE_VERSION: 'development',
 
   BOOKS: {
-    '031da8d3-b525-429c-80cf-6c8ed997733a':{'defaultVersion':'14.4'},
-    '8d50a0af-948b-4204-a71d-4826cba765b8':{'defaultVersion':'15.3'}
+    '031da8d3-b525-429c-80cf-6c8ed997733a':{'defaultVersion':'14.4'}, // College Physics
+    '8d50a0af-948b-4204-a71d-4826cba765b8':{'defaultVersion':'15.3'}, // Biology 2e
+    '7fccc9cf-9b71-44f6-800b-f9457fd64335':{'defaultVersion':'6.1'},  // Chemistry 2e
+    '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22':{'defaultVersion':'15.1'}, // Anatomy & Physiology
+    '2e737be8-ea65-48c3-aa0a-9f35b4c6a966':{'defaultVersion':'20.1'}, // Astronomy
   },
   
   SKIP_OS_WEB_PROXY: process.env.SKIP_OS_WEB_PROXY !== undefined,
