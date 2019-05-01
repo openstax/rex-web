c6e87d65508a6805d57ca13c0b4dd7fbdeeee26a
tom <tom.g.woodward@gmail.com>
2019-05-01 16:56:21 -0400

    add fixtures for accounts/api/user

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,7 @@
  * this file is shared between webpack-dev-server and the pre-renderer
  */
 const url = require('url');
+const util = require('util');
 const fs = require('fs');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
@@ -16,9 +17,9 @@ const {
 } = require('./config');
 
 const archivePaths = [
-  'contents',
-  'resources',
-  'specials',
+  '/contents',
+  '/resources',
+  '/specials',
 ];
 
 module.exports = function(app) {
@@ -27,32 +28,73 @@ module.exports = function(app) {
     : setupProxy(app);
 };
 
+function getReqInfo(request) {
+  const {search, pathname} = url.parse(request.url);
+  const cookie = request.headers.cookie || '';
+  return {
+    search,
+    pathname,
+    authenticated: cookie.includes('session')
+  };
+}
+
+const isFile = path =>
+  fs.existsSync(path)
+  && fs.existsSync(fs.realpathSync(path))
+  && fs.lstatSync(fs.realpathSync(path)).isFile();
+
+const isDirectory = path => fs.existsSync(path) && fs.lstatSync(path).isDirectory();
+
 function setupTestProxy(app) {
   console.info('WEBSERVER: Including fixtures');
 
   app.use((req, res, next) => {
+    const reqInfo = getReqInfo(req);
     const parts = url.parse(req.url);
-    const filePath = path.join(__dirname, 'test/fixtures', parts.pathname);
-    const queryFilePath = path.join(filePath, parts.search ? encodeURIComponent(parts.search) : '');
-    const indexFilePath = path.join(filePath, 'index.html');
 
-    const sendFile = path => fs.readFile(path, (err, contents) => {
-      res.end(contents);
-    });
+    const sendFile = path => {
+      const body = new Promise((resolve, reject) =>
+        fs.readFile(path, (err, contents) => err ? reject(err) : resolve(contents))
+      );
+      const statusFile = `${path}.status`;
+      const status = new Promise((resolve, reject) => isFile(statusFile)
+        ? fs.readFile(statusFile, (err, contents) => err ? reject(err) : resolve(contents))
+        : resolve(200)
+      );
 
-    const isFile = path =>
-      fs.existsSync(path)
-      && fs.existsSync(fs.realpathSync(path))
-      && fs.lstatSync(fs.realpathSync(path)).isFile();
+      Promise.all([body, status]).then(([body, status]) => {
+        res.status(status);
+        res.end(body);
+      });
+    };
 
-    const isDirectory = path => fs.existsSync(path) && fs.lstatSync(path).isDirectory();
+    const findFileIn = baseDir => {
+      const filePath = path.join(baseDir, reqInfo.pathname);
+      console.log(filePath);
+      const queryFilePath = path.join(filePath,
+        parts.search ? encodeURIComponent(reqInfo.search) : ''
+      );
+      const indexFilePath = path.join(filePath, 'index.html');
 
-    if (isFile(queryFilePath)) {
-      sendFile(queryFilePath);
-    } else if (isFile(filePath)) {
-      sendFile(filePath);
-    } else if (isDirectory(filePath) && isFile(indexFilePath)) {
-      sendFile(indexFilePath);
+      if (isFile(queryFilePath)) {
+        return queryFilePath;
+      } else if (isFile(filePath)) {
+        return filePath;
+      } else if (isDirectory(filePath) && isFile(indexFilePath)) {
+        return indexFilePath;
+      } else {
+        return null;
+      }
+    };
+
+    const fixtureDir = path.join(__dirname, 'test/fixtures');
+    const authFile = findFileIn(path.join(fixtureDir, 'authenticated'));
+    const publicFile = findFileIn(fixtureDir);
+
+    if (authFile && reqInfo.authenticated) {
+      sendFile(authFile);
+    } else if (publicFile) {
+      sendFile(publicFile);
     } else {
       next();
     }
@@ -63,8 +105,8 @@ function setupProxy(app) {
   if (!ARCHIVE_URL) { throw new Error('ARCHIVE_URL configuration must be defined'); }
   if (!OS_WEB_URL) { throw new Error('OS_WEB_URL configuration must be defined'); }
 
-  archivePaths.forEach(path => app.use(proxy(`/${path}`, {
-    target: `${ARCHIVE_URL}/${path}`,
+  archivePaths.forEach(path => app.use(proxy(path, {
+    target: `${ARCHIVE_URL}${path}`,
     prependPath: false,
     changeOrigin: true,
   })));
