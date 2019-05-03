be6be05eae5a32a88b6f7be2d29b7561ff04a996
m1yag1 <mike.arbelaez@rice.edu>
2019-05-03 08:09:17 -0500

    add more context to test docstring

--- pytest-selenium/tests/test_toc.py
+++ pytest-selenium/tests/test_toc.py
@@ -13,7 +13,7 @@ def test_toc_toggle_button_opens_and_closes(selenium, base_url, book_slug, page_
     on the resolution. When the resolution is desktop we
     click the toc button on the sidebar to close and the toc button on the
     toolbar to open. It's the other way around for mobile and tablet
-    resolutions.
+    resolutions and the sidebar is closed by default.
 
     """
     # GIVEN: The selenium driver, base_url, book_slug, and page_slug
