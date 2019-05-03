c11f450a3fbe034032ad4185ac129745d078590b
m1yag1 <mike.arbelaez@rice.edu>
2019-05-03 08:07:47 -0500

    improve docstring on test and fix typo

--- pytest-selenium/tests/test_toc.py
+++ pytest-selenium/tests/test_toc.py
@@ -8,10 +8,12 @@ from . import markers
 def test_toc_toggle_button_opens_and_closes(selenium, base_url, book_slug, page_slug):
     """ Test that table of contents toggle button opens and closes the sidebar
 
-    The table of contents sidebar is open by default for Desktop resolutions
+    The table of contents sidebar is open by default for desktop resolutions
     and closed for mobile and tablet. We need to do different actions based
-    on the resolution. This test depending on the resolution should click the
-    appropriate button depending on the state of sidebar.
+    on the resolution. When the resolution is desktop we
+    click the toc button on the sidebar to close and the toc button on the
+    toolbar to open. It's the other way around for mobile and tablet
+    resolutions.
 
     """
     # GIVEN: The selenium driver, base_url, book_slug, and page_slug
@@ -48,7 +50,7 @@ def test_toc_toggle_button_opens_and_closes(selenium, base_url, book_slug, page_
         assert not sidebar.header.is_displayed
 
         # WHEN: The toc button on the toolbar is clicked
-        # THEN: The sidbar area is opened
+        # THEN: The sidebar area is opened
         # AND: The toc button on the sidebar is clicked
         # AND: the sidebar area is closed
         toolbar.click_toc_toggle_button()
