d1bdb13720629c647b496c233c7053c121e5e1f1
malar <mn45@rice.edu>
2019-05-07 13:51:59 -0500

    test finished

--- pytest-selenium/tests/test_attribution.py
+++ pytest-selenium/tests/test_attribution.py
@@ -2,50 +2,62 @@
 import unittest
 from time import sleep
 
+import pytest
 from selenium import webdriver
-from selenium.common.exceptions import StaleElementReferenceException
+#from selenium.common.exceptions import StaleElementReferenceException
 
-class Citationtexturl(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
-    def teardown(self):
-        self.browser.quit()
+class TestCitationtexturl():
+    #def setUp(self):
+        #self.browser = webdriver.Chrome()
+        #self.browser.implicitly_wait(3)
+    #def teardown(self):
+       # self.browser.quit()
 
 
 
 #c476303 attribution section is initially collapsed, expands when clicked
-    def test_c476303_attribution_initial_status(self):
+    def test_c476303_attribution_initial_status(self, selenium, base_url):
+        
+        page_url = base_url + "/books/college-physics/pages/preface"
+        #page_url = "https://rex-web.herokuapp.com/books/college-physics/pages/preface"
+        
+        attribute_xpath_locator = "//details[contains(@class,'Attribution__Details')]"
 
-        page_url = "https://rex-web.herokuapp.com/books/college-physics/pages/preface"
-        attribute_xpath_locator = '//*[@id="root"]/div[2]/div[4]/div/div[2]/div/div/details'
-
-        self.browser.get(page_url)
+        selenium.get(page_url)
 
         
-        attribute_element = self.browser.find_element_by_xpath(attribute_xpath_locator)
+        attribute_element = selenium.find_element_by_xpath(attribute_xpath_locator)
 
         #verify attribution is closed by default
         is_attribute_closed = attribute_element.get_attribute("open")
-        #print(is_attribute_closed)
-        self.assertNotEqual(is_attribute_closed,"true")
+        print(is_attribute_closed)
+        # self.assertNotEqual(is_attribute_closed,"true")
 
+        assert is_attribute_closed != "true"
+        sleep(1)
 
         #click on attribution and verify it expands
         attribute_element.click()
         is_attribute_open = attribute_element.get_attribute("open")
-        #print(is_attribute_open)
-        self.assertEqual(is_attribute_open,"true")
+        sleep(1)
+        print(is_attribute_open)
+        # self.assertEqual(is_attribute_open,"true")
+
+        assert is_attribute_open == "true"
 
                 
-        #attribute_xpath_after_click = '//*[@id="root"]/div[2]/div[4]/div/div[2]/div/div/details'
-        #attribute_element_after_click = self.browser.find_element_by_xpath(attribute_xpath_after_click)
-        #attribute_element_after_click.click()
-        #is_attribute_closed = attribute_element_after_click.get_attribute("open")
-        #print(is_attribute_closed)
+        #click on attribution and verify it closes
+        attribute_open_xpath_locator = "//summary[contains(@class,'Attribution__Summary')]"
+        attribute_open_element = selenium.find_element_by_xpath(attribute_open_xpath_locator)
+        sleep(1)
+        attribute_open_element.click()
+        is_attribute_closed_again = attribute_open_element.get_attribute("open")
+        print(is_attribute_closed_again)
+        #self.assertNotEqual(is_attribute_closed_again,"true")
+        assert is_attribute_closed_again != "true"
 
         
     
 
-if __name__ == '__main__': 
-    unittest.main() 
+#if __name__ == '__main__': 
+    #unittest.main() 
