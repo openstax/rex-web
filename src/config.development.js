2329faacf1714be5e3fb4f71e2b7a229a894b3a9
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-05-09 10:51:34 -0500

    :skull: use production oscms
    because it contains Chemistry 2e

--- src/config.development.js
+++ src/config.development.js
@@ -20,7 +20,7 @@ module.exports = {
   },
 
   ACCOUNTS_URL: process.env.ACCOUNTS_URL || 'https://accounts-dev.openstax.org',
-  OS_WEB_URL: process.env.OS_WEB_URL || 'https://cms-dev.openstax.org',
+  OS_WEB_URL: process.env.OS_WEB_URL || 'https://openstax.org',
 
   SKIP_OS_WEB_PROXY: process.env.SKIP_OS_WEB_PROXY !== undefined,
   FIXTURES: false,
