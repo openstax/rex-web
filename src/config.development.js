83084c15566c8bd2a87e3924957f18e5a4d2c2c2
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-05-10 14:26:19 -0500

    re-enable chemistry

--- src/config.development.js
+++ src/config.development.js
@@ -5,7 +5,7 @@ module.exports = {
   BOOKS: {
     '031da8d3-b525-429c-80cf-6c8ed997733a':{'defaultVersion':'14.4'}, // College Physics
     '8d50a0af-948b-4204-a71d-4826cba765b8':{'defaultVersion':'15.3'}, // Biology 2e
-    // '7fccc9cf-9b71-44f6-800b-f9457fd64335':{'defaultVersion':'6.1'},  // Chemistry 2e
+    '7fccc9cf-9b71-44f6-800b-f9457fd64335':{'defaultVersion':'6.1'},  // Chemistry 2e
     '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22':{'defaultVersion':'15.1'}, // Anatomy & Physiology
     '2e737be8-ea65-48c3-aa0a-9f35b4c6a966':{'defaultVersion':'20.1'}, // Astronomy
     '4abf04bf-93a0-45c3-9cbc-2cefd46e68cc':{'defaultVersion':'10.16'}, // Psychology
