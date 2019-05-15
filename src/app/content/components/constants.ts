8ea54405774af4cb2bcbce3bd451d4af5e914a3b
tom <tom.g.woodward@gmail.com>
2019-05-15 15:28:12 -0400

    update content size

--- src/app/content/components/constants.ts
+++ src/app/content/components/constants.ts
@@ -13,7 +13,7 @@ export const toolbarIconColor = theme.color.primary.gray;
 export const toolbarDesktopHeight = 5;
 export const toolbarMobileHeight = 4;
 
-export const contentTextWidth = 84;
+export const contentTextWidth = 82.5;
 
 export const mainContentBackground = '#fff';
 
