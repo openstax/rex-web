5f681f40b662a1711c45a285c17af9cb3829f527
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2019-05-21 14:24:55 -0500

    test to validate attribution status

---
+++ pytest-selenium/tests/test_attribution_status.py
@@ -0,0 +1,33 @@
+from pages.content import Content
+from tests import markers
+
+
+@markers.test_case("C476303")
+@markers.parametrize("book_slug,page_slug", [("college-physics", "preface")])
+@markers.nondestructive
+def test_attribution_collapsed_by_default_expands_when_clicked(
+    selenium, base_url, book_slug, page_slug
+):
+
+    """
+    attribution section is initially collapsed, expands when clicked
+
+    GIVEN: A page URL in the format of {base_url}/books/{book_slug}/pages/{page_slug}
+    WHEN: The page is fully loaded
+    THEN: The attribution section is collapsed by default
+    AND The attribution section opens when clicked
+    """
+    content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
+    attribution = content.attribution
+
+    assert not attribution.is_open
+
+    attribution.click_attribution_link()
+
+    assert attribution.is_open
+
+    attribution.click_attribution_link()
+    # """click on attribution again and verify it closes
+
+    # """
+    assert not attribution.is_open
