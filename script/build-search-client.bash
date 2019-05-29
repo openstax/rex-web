f1badda48165f0fa4af3f509d23270f36704de2f
tom <tom.g.woodward@gmail.com>
2019-05-29 16:27:39 -0400

    get ready to use a real swagger file

--- script/build-search-client.bash
+++ script/build-search-client.bash
@@ -4,10 +4,12 @@ if [ -d "./open-search" ]; then
   exit 0
 fi
 
+swagger_url=$(node -e "const {SEARCH_URL, REACT_APP_SEARCH_URL} = require('./src/config'); console.log(SEARCH_URL + REACT_APP_SEARCH_URL + '/swagger')")
+
 tmp_file=$(mktemp -t open-search-client)
 download_url=$(curl -H "Content-type: application/json" -s \
     -X POST \
-    -d '{"options": {"npmName": "@openstax/open-search-client"},"openAPIUrl": "http://petstore.swagger.io/v2/swagger.json"}' \
+    -d "{\"options\": {\"npmName\": \"@openstax/open-search-client\"},\"openAPIUrl\": \"$swagger_url\"}" \
     http://api.openapi-generator.tech/api/gen/clients/typescript-fetch \
     | node -e "console.log(JSON.parse(require('fs').readFileSync(0, 'utf-8'))[\"link\"])")
 
