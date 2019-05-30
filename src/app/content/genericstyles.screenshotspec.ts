3e5592bcf24d66f14d709a0c8f9a8b22c16c5fd2
KarinaMendez2 <13000365@galileo.edum>
2019-05-30 12:19:36 -0600

    Removed styling for h3 in insider perspective (fixed in another PR) added new test page

--- src/app/content/genericstyles.screenshotspec.ts
+++ src/app/content/genericstyles.screenshotspec.ts
@@ -6,17 +6,13 @@ import {
   setMobileViewport
 } from '../../test/browserutils';
 
-const CHEMISTRY_BOOK = '/books/chemistry-2e';
-const SPLASH_IMAGE_FULL_WIDTH = `${CHEMISTRY_BOOK}/pages/6-introduction`;
-/*const SPLASH_IMAGE_FULL_SIZE = `${CHEMISTRY_BOOK}/pages/6-5-periodic-variations-in-element-properties`;
-const TABLE_SEPARATIONS = `${CHEMISTRY_BOOK}/pages/7-5-strengths-of-ionic-and-covalent-bonds`;
-const INSIDER_PERSPECTIVE_PAGE = '/books/american-government-2e/pages/2-4-the-ratification-of-the-constitution#fs-id1166227577868';
-const KEY_TERMS_BOLDED = '/books/business-ethics/pages/1-key-terms';*/
+const TEST_PAGE_URL = '/books/book-slug-1/pages/test-page-for-generic-styles';
 
 describe('content', () => {
   it('looks right', async() => {
     setDesktopViewport(page);
-    await navigate(page, SPLASH_IMAGE_FULL_WIDTH);
+    await navigate(page, TEST_PAGE_URL);
+    await page.emulateMedia('print');
     const screen = await fullPageScreenshot(page);
     expect(screen).toMatchImageSnapshot({
       CI: {
@@ -28,7 +24,8 @@ describe('content', () => {
 
   it('looks right on mobile', async() => {
     setMobileViewport(page);
-    await navigate(page, SPLASH_IMAGE_FULL_WIDTH);
+    await navigate(page, TEST_PAGE_URL);
+    await page.emulateMedia('print');
     const screen = await fullPageScreenshot(page);
     expect(screen).toMatchImageSnapshot({
       CI: {
