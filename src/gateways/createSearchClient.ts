df27b17666ef53b94dd8af8a0ef2dc69ce1d4715
tom <tom.g.woodward@gmail.com>
2019-05-31 15:06:09 -0400

    fixes for ssr

--- src/gateways/createSearchClient.ts
+++ src/gateways/createSearchClient.ts
@@ -3,7 +3,7 @@ import { Configuration, SearchApi } from '@openstax/open-search-client';
 export default (url: string) => {
   const config = new Configuration({
     basePath: url,
-    fetchApi: fetch.bind(window),
+    fetchApi: (...args) => fetch(...args),
   });
   return new SearchApi(config);
 };
