1d7cf67884e298ec92cf19d4bd6dc1ffafb4fc6d
tom <tom.g.woodward@gmail.com>
2019-05-31 15:52:41 -0400

    fix search client test

--- src/gateways/createSearchClient.spec.ts
+++ src/gateways/createSearchClient.spec.ts
@@ -1,20 +1,35 @@
-import { Configuration, SearchApi } from '@openstax/open-search-client';
 import createSearchClient from './createSearchClient';
 
-jest.mock('@openstax/open-search-client');
-
 describe('createSearchClient', () => {
-  it('configures the basePath', () => {
-    const url = 'asdf';
+  const fetchBackup = fetch;
+  let fetchSpy: jest.SpyInstance;
 
-    createSearchClient(url);
+  beforeEach(() => {
+    jest.resetModules();
+    fetchSpy = (global as any).fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => ({}),
+        status: 200,
+      })
+    );
+  });
 
-    expect(Configuration).toBeCalledWith(expect.objectContaining({
-      basePath: url,
-    }));
+  afterEach(() => {
+    (global as any).fetch = fetchBackup;
+  });
 
-    expect(SearchApi).toBeCalledWith(
-      (Configuration as unknown as jest.SpyInstance<Configuration>).mock.instances[0]
+  it('calls fetch with the baseurl', async() => {
+    const client = createSearchClient('asdf');
+
+    await client.search({
+      books: ['book'],
+      indexStrategy: '1',
+      q: 'foo',
+      searchStrategy: '1',
+    });
+
+    expect(fetchSpy.mock.calls[0][0]).toMatchInlineSnapshot(
+      `"asdf/search?q=foo&books=book&index_strategy=1&search_strategy=1"`
     );
   });
 });
