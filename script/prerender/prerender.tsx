fb3da1fce980a7c5c19c62421679c24d7ac2a4df
tom <tom.g.woodward@gmail.com>
2019-06-13 20:11:28 -0400

    remove developer home from prerender

--- script/prerender/prerender.tsx
+++ script/prerender/prerender.tsx
@@ -11,7 +11,6 @@ import createApp from '../../src/app';
 import { AppOptions } from '../../src/app';
 import { content } from '../../src/app/content/routes';
 import { flattenArchiveTree, getUrlParamForPageId, stripIdVersion } from '../../src/app/content/utils';
-import { developerHome } from '../../src/app/developer/routes';
 import { notFound } from '../../src/app/errors/routes';
 import * as errorSelectors from '../../src/app/errors/selectors';
 import * as headSelectors from '../../src/app/head/selectors';
@@ -132,7 +131,6 @@ const preparePages: PreparePages = async(archiveLoader, osWebLoader) => {
 
   const pages: Pages = [
     {code: 404, page: {route: notFound}},
-    {code: 200, page: {route: developerHome}},
   ];
 
   for (const [bookId, {defaultVersion}] of bookEntries) {
