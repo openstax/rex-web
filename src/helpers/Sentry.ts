d91b77a9a252133664ef3a7b76f05035b6ad3d13
Nathan Stitt <nathan@stitt.org>
2019-06-26 21:42:05 -0500

    use RELEASE_ID for dist

--- src/helpers/Sentry.ts
+++ src/helpers/Sentry.ts
@@ -6,7 +6,7 @@ export default {
 
   initializeWithMiddleware() {
     Sentry.init({
-      dist: config.CODE_VERSION,
+      dist: config.RELEASE_ID,
       dsn: 'https://84d2036467d546038347f0ac9ccd8b3b:c815982d89764df583493a60794e54aa@sentry.cnx.org/17',
       environment: config.DEPLOYED_ENV,
     });
