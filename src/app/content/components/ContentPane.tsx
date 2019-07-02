fb78a3e622eba29f67fe73ca06480d78663476b1
tom <tom.g.woodward@gmail.com>
2019-07-02 14:16:09 -0400

    use scroll-snap

--- src/app/content/components/ContentPane.tsx
+++ src/app/content/components/ContentPane.tsx
@@ -7,27 +7,11 @@ import { Dispatch } from '../../types';
 import { closeToc } from '../actions';
 import { State } from '../types';
 import {
-  bookBannerDesktopMiniHeight,
   sidebarDesktopWidth,
   sidebarMobileWidth,
   sidebarTransitionTime,
-  toolbarDesktopHeight
 } from './constants';
 import { isOpenConnector, styleWhenSidebarClosed } from './utils/sidebar';
-
-/*
- * solution in place for display: table
- * elements resets their top margin, so we
- * need to know the original to add it back in
- */
-const displayTableElements = [
-  {selector: '#main-content .os-table', margin: 2},
-  {selector: '#main-content .os-figure', margin: 3},
-  {selector: '#main-content [data-type="equation"]', margin: 0},
-];
-const inlineBlockElements = [
-  {selector: '[data-type="term"]'},
-];
 
 // tslint:disable-next-line:variable-name
 const Wrapper = styled.div<{isOpen: State['tocOpen']}>`
@@ -42,28 +26,6 @@ const Wrapper = styled.div<{isOpen: State['tocOpen']}>`
 
     ${theme.breakpoints.mobile(css`
       margin-left: -${sidebarMobileWidth}rem;
-    `)}
-
-    ${displayTableElements.map((elementType) => css`
-      ${elementType.selector}:target {
-        margin-top: -${bookBannerDesktopMiniHeight + toolbarDesktopHeight}rem;
-
-        ::before {
-          content: ' ';
-          display: table-row;
-          height: ${bookBannerDesktopMiniHeight + toolbarDesktopHeight + elementType.margin}rem;
-        }
-      }
-    `)}
-    ${inlineBlockElements.map((elementType) => css`
-      ${elementType.selector}:target::before {
-        content: ' ';
-        display: inline-block;
-        position: relative;
-        margin-top: -${bookBannerDesktopMiniHeight + toolbarDesktopHeight}rem;
-        height: ${bookBannerDesktopMiniHeight + toolbarDesktopHeight}rem;
-        visibility: hidden;
-      }
     `)}
   }
 `;
