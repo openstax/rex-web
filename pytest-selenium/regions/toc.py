b819bd2327c2c2e4768e3bdf8de1050401069bee
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2019-07-08 16:20:12 -0500

    working on C476819

--- pytest-selenium/regions/toc.py
+++ pytest-selenium/regions/toc.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.keys import Keys
 
 class TableOfContents(Region):
     _root_locator = (By.CSS_SELECTOR, "ol")
+    _page_link_locator = (By.XPATH, "(.//li/a)")
     _active_page_locator = (By.CSS_SELECTOR, "[aria-label='Current Page']")
 
     @property
@@ -17,3 +18,16 @@ class TableOfContents(Region):
     def assert_page_name_in_TOC_is_bolded(self):
         bold = self.active_page.value_of_css_property("font-weight")
         assert bold == "400"
+
+    @property
+    def pages(self):
+        return [
+            self.ContentPage(self.page, self.root, index)
+            for index in range(len(self.find_elements(*self._page_link_locator)))
+        ]
+
+    class ContentPage(ContentItem):
+        _root_locator_template = "(//li/a)[{index}]"
+
+        def click(self):
+            self.root.send_keys(Keys.ENTER)
--- pytest-selenium/tests/test_toc.py
+++ pytest-selenium/tests/test_toc.py
@@ -3,7 +3,7 @@ from pages.content import Content
 from . import markers
 
 
-@markers.test_case("C250849", "C476816", "C476817")
+@markers.test_case("C250849", "C242270")
 @markers.parametrize("book_slug,page_slug", [("college-physics", "preface")])
 @markers.nondestructive
 def test_toc_toggle_button_opens_and_closes(selenium, base_url, book_slug, page_slug):
@@ -71,3 +71,34 @@ def test_toc_toggle_button_opens_and_closes(selenium, base_url, book_slug, page_
     else:
 
         pytest.fail("window must be either mobile or desktop size")
+
+
+@markers.test_case("C476819")
+@markers.parametrize(
+    "book_slug,page_slug",
+    [
+        (
+            "college-physics",
+            "1-introduction-to-science-and-the-realm-of-physics-physical-quantities-and-units",
+        )
+    ],
+)
+@markers.nondestructive
+def test_toc_closes_after_selecting_page_in_mobile(selenium, base_url, book_slug, page_slug):
+    # GIVEN: The selenium driver, base_url, book_slug, and page_slug is opened in mobile resolution
+    # AND: The TOC is opened
+
+    content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
+
+    toolbar = content.toolbar
+    sidebar = content.sidebar
+    toc = content.sidebar.toc
+
+    toolbar.click_toc_toggle_button()
+
+    # WHEN: The page in the ToC is clicked
+    pages = toc.pages[5]
+    pages.click()
+
+    # THEN: The page loads and the ToC is automatically closed
+    assert not sidebar.is_displayed
