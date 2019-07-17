bf459fdf0c710bcb1388ee7bd9b74b1ff55dc98e
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2019-07-17 16:39:14 -0500

    toc region added

---
+++ pytest-selenium/regions/toc.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.common.by import By
+
+from regions.base import Region
+from regions.content_item import ContentItem
+from pages import content
+from pages import base
+
+from selenium.webdriver.common.keys import Keys
+
+
+class TableOfContents(Region):
+    _root_locator = (By.CSS_SELECTOR, "ol")
+    _page_link_locator = (By.XPATH, "(.//li/a)")
+    _active_page_locator = (By.CSS_SELECTOR, "[aria-label='Current Page']")
+
+    @property
+    def active_page(self):
+        return self.find_element(*self._active_page_locator)
+
+    def assert_page_name_in_TOC_is_bolded(self):
+        bold = self.active_page.value_of_css_property("font-weight")
+        assert bold == "400"
+
+    @property
+    def pages(self):
+        return [
+            self.ContentPage(self.page, self.root, index)
+            for index in range(len(self.find_elements(*self._page_link_locator)))
+        ]
+
+    class ContentPage(ContentItem):
+        _root_locator_template = "(//li/a)[{index}]"
+
+        def click(self):
+            title_before_click = self.title_before_click
+            print(title_before_click)
+            self.root.send_keys(Keys.ENTER)
+            # page = self.__class__(self.page, self.parent_root, self.index)
+            return self.page.wait_for_region_to_display()
---
+++ pytest-selenium/tests/test_navigation.py
@@ -0,0 +1,39 @@
+from pages.content import Content
+from tests import markers
+import pytest
+
+
+@markers.test_case("C477321")
+@markers.parametrize("book_slug,page_slug", [("prealgebra", "preface")])
+@markers.nondestructive
+def test_previous_link_hidden_on_first_page(selenium, base_url, book_slug, page_slug):
+
+    content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
+    toc = content.sidebar.toc
+
+    # confirm first page is selected
+    assert toc.active_page.text == toc.pages[0].page_title()
+
+    with pytest.raises(Exception) as exc_info:
+        assert not content.previous_link.is_displayed
+
+    exception_raised = exc_info.type
+    assert "NoSuchElementException" in str(exception_raised)
+
+
+@markers.test_case("C477322")
+@markers.parametrize("book_slug,page_slug", [("prealgebra", "index")])
+@markers.nondestructive
+def test_next_link_hidden_on_last_page(selenium, base_url, book_slug, page_slug):
+
+    content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
+    toc = content.sidebar.toc
+
+    # -1 gives the last element in the list. confirm last page is selected
+    assert toc.active_page.text == toc.pages[-1].page_title()
+
+    with pytest.raises(Exception) as exc_info:
+        assert not content.next_link.is_displayed
+
+    exception_raised = exc_info.type
+    assert "NoSuchElementException" in str(exception_raised)
