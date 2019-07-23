0d48772633952784788ecd756ccf42748a3c0ac4
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2019-07-23 13:39:18 -0500

    updated test parameters handle test if different page is opened

--- pytest-selenium/tests/test_navigation.py
+++ pytest-selenium/tests/test_navigation.py
@@ -1,19 +1,24 @@
+import pytest
 from pages.content import Content
 from tests import markers
-import pytest
 
 
 @markers.test_case("C477321")
-@markers.parametrize("book_slug,page_slug", [("college-physics", "preface")])
+@markers.parametrize("page_slug", [("preface")])
 @markers.nondestructive
 def test_previous_link_hidden_on_first_page(selenium, base_url, book_slug, page_slug):
 
     # GIVEN: The page is loaded
     content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
+    toolbar = content.toolbar
     toc = content.sidebar.toc
 
     # confirm first page is selected
-    assert toc.active_section.get_attribute("textContent") == toc.sections[0].section_title
+    if toc.active_section.get_attribute("textContent") != toc.sections[0].section_title:
+        while content.is_mobile:
+            toolbar.click_toc_toggle_button()
+            break
+        toc.sections[0].click()
 
     # THEN: The "previous" link should be hidden
     with pytest.raises(Exception) as exc_info:
@@ -27,16 +32,21 @@ def test_previous_link_hidden_on_first_page(selenium, base_url, book_slug, page_
 
 
 @markers.test_case("C477322")
-@markers.parametrize("book_slug,page_slug", [("prealgebra", "index")])
+@markers.parametrize("page_slug", [("index")])
 @markers.nondestructive
 def test_next_link_hidden_on_last_page(selenium, base_url, book_slug, page_slug):
 
     # GIVEN: The page is loaded
     content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
+    toolbar = content.toolbar
     toc = content.sidebar.toc
 
     # confirm last page is selected. -1 gives the last element in the list.
-    assert toc.active_section.get_attribute("textContent") == toc.sections[-1].section_title
+    if toc.active_section.get_attribute("textContent") != toc.sections[-1].section_title:
+        while content.is_mobile:
+            toolbar.click_toc_toggle_button()
+            break
+        toc.sections[-1].click()
 
     # THEN:The "next" link should be hidden
     with pytest.raises(Exception) as exc_info:
