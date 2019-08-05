0c0eb957aa3d49ee78802f63589278dca795acf2
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-08-05 07:08:27 -0500

    determine canonical URL from mapping

--- src/app/content/hooks/receiveContent.ts
+++ src/app/content/hooks/receiveContent.ts
@@ -3,15 +3,10 @@ import theme from '../../theme';
 import { ActionHookBody } from '../../types';
 import { receiveBook, receivePage } from '../actions';
 import * as select from '../selectors';
-import { getUrlParamForPageId } from '../utils/urlUtils';
-import { getBook } from '../../developer/components/utils';
+import { getCanonicalUrl } from '../../developer/components/utils';
 import { Link } from '../../head/types';
 
 const hookBody: ActionHookBody<typeof receivePage | typeof receiveBook> = ({getState, dispatch, archiveLoader, osWebLoader}) => async () => {
-
-  const CANONICAL_LIST = [
-    {id: '7fccc9cf-9b71-44f6-800b-f9457fd64335', version: '7.4'}, // Chemistry
-  ];
 
   const state = getState();
   const book = select.book(state);
@@ -31,19 +26,10 @@ const hookBody: ActionHookBody<typeof receivePage | typeof receiveBook> = ({getS
 
   const links: Link[] = [];
 
-  for (const entry of CANONICAL_LIST) {
-    const id = entry.id;
-    const version = entry.version;
-
-    const canonicalBook = await getBook(id, version, archiveLoader, osWebLoader);
-    const pageInBook = getUrlParamForPageId(canonicalBook, page.shortId, true);
-
-    if (pageInBook) {
-      links.push({rel: 'canonical', href: `../../${canonicalBook.slug}/pages/${pageInBook}`})
-      break
-    }
+  const canonical = await getCanonicalUrl(archiveLoader, osWebLoader, book.id, page.shortId)
+  if (canonical) {
+    links.push({rel: 'canonical', href: `../../${canonical.bookSlug}/pages/${canonical.pageSlug}`})
   }
-
 
   dispatch(setHead({
     meta: [
