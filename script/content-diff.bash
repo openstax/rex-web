c1a40fa01c9bbd702ee43c18891c122e6b3012e8
tom <tom.g.woodward@gmail.com>
2019-08-06 10:14:38 -0400

    wip - fixing content diff

--- script/content-diff.bash
+++ script/content-diff.bash
@@ -17,11 +17,19 @@ function stripToContent() {
   while IFS= read -r -d '' -u 9
   do
     path=$REPLY
+
     sed -Ei '' \
       -e '1h;2,$H;$!d;g' \
-      -e 's/^.*<div id="main-content"[^>]*>(.*)<div class="PrevNextBar.*$/\1/' \
-      -e 's/id="[0-9]+(_copy_[0-9]+)?"/id="[redacted for uniqueness]"/g' \
-      -e 's/(href="[^"#]+#)[0-9]+(_copy_[0-9]+)?"/\1[redacted for uniqueness]"/g' \
+      -e 's/a target="_blank" rel="noopener nofollow"nn/ann/' \
+      "$path"
+
+    prettyhtml --sortAttributes "$path"
+
+    sed -Ei '' \
+      -e '1h;2,$H;$!d;g' \
+      -e 's/^.*id="main-content"[^>]*>(.*)<div class="PrevNextBar.*$/\1/' \
+      -e 's/id="(term)?[0-9]+(_copy_[0-9]+)?"/id="[redacted for uniqueness]"/g' \
+      -e 's/(href="[^"#]+#)(term)?[0-9]+(_copy_[0-9]+)?"/\1[redacted for uniqueness]"/g' \
       -e 's/(data-cnxml-to-html-ver=")[\.0-9]+"/\1[redacted for uniqueness]"/' \
       "$path"
   done 9< <( find books -type f -exec printf '%s\0' {} + )
@@ -35,16 +43,18 @@ fi
 git checkout --orphan "content-diffs--$first_release-$second_release"
 git clean -fd
 
-aws s3 sync "s3://unified-web-primary/rex/releases/$first_release/books" books/
+aws s3 sync "s3://unified-web-primary/rex/releases/$first_release/books/chemistry-2e" books/chemistry-2e
+aws s3 sync "s3://unified-web-primary/rex/releases/$first_release/books/chemistry-atoms-first-2e" books/chemistry-atoms-first-2e
 stripToContent
 
 git add books
-git commit -m "stripped content for $first_release"
+git -c commit.gpgsign=false commit -m "stripped content for $first_release"
 
 rm -rf books/*
 
-aws s3 sync "s3://unified-web-primary/rex/releases/$second_release/books" books/
+aws s3 sync "s3://unified-web-primary/rex/releases/$second_release/books/chemistry-2e" books/chemistry-2e
+aws s3 sync "s3://unified-web-primary/rex/releases/$second_release/books/chemistry-atoms-first-2e" books/chemistry-atoms-first-2e
 stripToContent
 
 git add books
-git commit -m "stripped content for $second_release"
+git -c commit.gpgsign=false commit -m "stripped content for $second_release"
