acd4e3017805667ab5d14bc925540fc67c23de64
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2019-08-12 14:43:23 -0500

    yet to parametrize and remove sleep

--- pytest-selenium/tests/test_login.py
+++ pytest-selenium/tests/test_login.py
@@ -1,6 +1,6 @@
 from pages.content import Content
-from tests import markers
 from pages.accounts import Login
+from tests import markers
 
 from time import sleep
 
@@ -10,7 +10,6 @@ from time import sleep
 @markers.nondestructive
 def test_login(selenium, base_url, book_slug, page_slug):
     content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
-    selenium.implicitly_wait(1)
     page_url_before_login = selenium.current_url
     content.click_login()
 
@@ -20,15 +19,27 @@ def test_login(selenium, base_url, book_slug, page_slug):
     assert expected_page_url == selenium.current_url
 
     accounts = Login(selenium)
-    accounts.enter_user_info(name_or_email="teacher01")
+    accounts.enter_user_info()
     accounts.next_click()
-    accounts.password(password="nope")
+    accounts.enter_password()
     accounts.login_click()
-    sleep(5)
+    selenium.implicitly_wait(10)
 
     # AND after successful login, redirects back to the preface page
     assert page_url_before_login == selenium.current_url
 
     # AND the user menu displayed in the nav includes Account Profile and Logout
+    user_nav = content.navbar
+
+    if content.is_mobile:
+        user_nav.click_user_name()
+
+    user_nav.hover_over_user_name()
+    selenium.implicitly_wait(10)
+    assert user_nav.account_profile.is_displayed()
+    assert user_nav.logout.is_displayed()
+    sleep(5)
 
     # AND reloading does not reset the state back to logged out
+    selenium.refresh()
+    assert user_nav.user_is_logged_in
