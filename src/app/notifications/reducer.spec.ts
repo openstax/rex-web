9fd3cd666543f253cad72c81aced17355d3e5b4e
Thomas Woodward <tom.g.woodward@gmail.com>
2019-08-13 16:03:51 -0400

    feat: design prompt to accept cookies (#251)
    * design prompt to accept cookies
    
    * hook up dismissing notifications
    
    * buttons get cursor pointer
    
    * :camera:
    
    * style updates
    
    * :camera:
    
    * Add min-width for medium buttons
    
    * add unit tests

--- src/app/notifications/reducer.spec.ts
+++ src/app/notifications/reducer.spec.ts
@@ -1,3 +1,4 @@
+import flow from 'lodash/fp/flow';
 import * as actions from './actions';
 import reducer, { initialState } from './reducer';
 
@@ -12,5 +13,22 @@ describe('notifications reducer', () => {
     const state = [actions.updateAvailable()];
     const newState = reducer(state, actions.updateAvailable());
     expect(newState).toBe(state);
+  });
+
+  it('dismissesNotification', () => {
+    const acceptCookiesNotification = actions.acceptCookies();
+
+    const newState = flow(
+      (state) => reducer(state, actions.updateAvailable()),
+      (state) => reducer(state, acceptCookiesNotification),
+      (state) => reducer(state, actions.dismissNotification(acceptCookiesNotification))
+    )(initialState);
+
+    expect(newState).not.toContainEqual(actions.acceptCookies());
+  });
+
+  it('reduces acceptCookies', () => {
+    const newState = reducer(initialState, actions.acceptCookies());
+    expect(newState).toContainEqual(actions.acceptCookies());
   });
 });
