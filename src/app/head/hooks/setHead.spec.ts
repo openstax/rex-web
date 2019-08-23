a15041cc718057703c8b88a3b0395729a2c959c6
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-08-23 07:41:49 -0500

    add test

--- src/app/head/hooks/setHead.spec.ts
+++ src/app/head/hooks/setHead.spec.ts
@@ -34,6 +34,38 @@ describe('setHead hook', () => {
     }
   });
 
+  it.skip('sets the og:title', () => {
+    if (typeof(document) === 'undefined') {
+      expect(document).toBeTruthy();
+    } else {
+      hookBody(helpers)(action);
+      const titles = document.querySelectorAll('meta[property="og:title"]');
+      expect(titles.length).toBe(1);
+      expect(titles[0].getAttribute('content')).toEqual('cool title');
+    }
+  });
+
+  it('sets the og:description using the content text', () => {
+    if (typeof(document) === 'undefined') {
+      expect(document).toBeTruthy();
+    } else {
+      const main = document.createElement('div');
+      main.setAttribute('id', 'main-content');
+      main.innerHTML = `this <b>is a test</b> with "content" that is more
+than 160 characters long to verify that the text is trimmed to be
+short enough to act as a preview when the link is shared on social
+media and unfurled.`;
+
+      document.body.appendChild(main);
+      hookBody(helpers)(action);
+      const descriptions = document.querySelectorAll('meta[property="og:description"]');
+      expect(descriptions.length).toBe(1);
+      expect(descriptions[0].getAttribute('content')).toEqual(`this is a test with "content" that is more
+than 160 characters long to verify that the text is trimmed to be
+short enough to act as a preview when the link`);
+    }
+  });
+
   describe('outside the browser', () => {
     const documentBackup = document;
 
