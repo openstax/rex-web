787fd00293c18a62212f6f65758e714c8ac6d705
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-08-26 15:06:47 -0500

    look up URL in the canonical book (#244)
    * look up URL in the canonical book
    
    does not seem to always work for pages in "Chemistry Atoms First"
    
    * add canonical URL when necessary
    
    * determine canonical URL from mapping
    
    * :shirt: lint
    
    * fix tests
    
    * add new tests
    
    * add selector test
    
    * :100: tests
    
    * create separate book map file
    
    * compute the canonical URL
    
    * use findArchiveTreeNode directly
    
    instead of caching in getUrlParamForPageId
    
    * fix urlUtils
    
    * always include canonical url
    
    * nitpicks
    
    * add Organizational Behavior
    
    * absolute host for canonical URL
    
    * seo score is hurt by canonical url pointing to different domain

--- src/app/head/types.ts
+++ src/app/head/types.ts
@@ -1,7 +1,9 @@
 
 export type Meta = {property: string; content: string} | {name: string; content: string; };
+export interface Link {rel: 'canonical'; href: string; }
 
 export interface State {
   title: string;
   meta: Meta[];
+  links: Link[];
 }
