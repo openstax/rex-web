cbc27563129d300d9c218dce8caa05470d3e8330
Thomas Woodward <tom.g.woodward@gmail.com>
2019-08-29 09:50:06 -0400

    update a&p (#284)

--- src/config.books.js
+++ src/config.books.js
@@ -1,5 +1,5 @@
 const books = {
-  /* Anatomy & Physiology */ '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22': {defaultVersion: '16.5'},
+  /* Anatomy & Physiology */ '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22': {defaultVersion: '16.7'},
   /* Biology 2e */ '8d50a0af-948b-4204-a71d-4826cba765b8': {defaultVersion: '15.47'},
   /* Biology for AP Courses */ '6c322e32-9fb0-4c4d-a1d7-20c95c5c7af2': {defaultVersion: '18.4'},
   /* Chemistry 2e */ '7fccc9cf-9b71-44f6-800b-f9457fd64335': {defaultVersion: '9.18'},
