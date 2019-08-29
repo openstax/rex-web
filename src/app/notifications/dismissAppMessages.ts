5af38ffe1de3c8f4b8a280c06382b35dce1a4523
pandafulmanda <pandafulmanda@gmail.com>
2019-08-29 13:47:29 -0500

    set expiration on cookie close to end_at date for message

--- src/app/notifications/dismissAppMessages.ts
+++ src/app/notifications/dismissAppMessages.ts
@@ -1,13 +1,27 @@
 import * as Cookies from 'js-cookie';
 import { Message } from './types'
+import {
+  differenceInDays,
+  endOfToday,
+} from 'date-fns';
 
 const messageDismissedPrefix: string = 'message_dismissed';
 
 const dismissedPhrase: string = 'true';
 
 export const dismissAppMessage = (message: Message) => {
-  const expires = 365 * 20;
-  Cookies.set(`${messageDismissedPrefix}_${message.id}`, dismissedPhrase, { expires });
+  const daysToExpire = (
+    message.end_at && (
+      differenceInDays(
+        new Date(message.end_at),
+        endOfToday()
+      ) + 7
+    )
+  ) || (
+    365 * 20
+  );
+
+  Cookies.set(`${messageDismissedPrefix}_${message.id}`, dismissedPhrase, { expires: daysToExpire });
 };
 
 export const isAppMessageDismissed = (message: Message) => {
