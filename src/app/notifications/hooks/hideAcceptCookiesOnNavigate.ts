54ffb60738a0e8d8260dd8c0c007ead977c10550
JP Slavinsky <jpslav@gmail.com>
2019-08-29 07:49:59 -0600

    use notifications selector

--- src/app/notifications/hooks/hideAcceptCookiesOnNavigate.ts
+++ src/app/notifications/hooks/hideAcceptCookiesOnNavigate.ts
@@ -3,10 +3,11 @@ import { locationChange } from '../../navigation/actions';
 import { ActionHookBody } from '../../types';
 import { actionHook } from '../../utils';
 import { acceptCookies, dismissNotification } from '../actions';
+import * as select from '../selectors';
 
 export const hideAcceptCookiesOnNavigateHookBody: ActionHookBody<typeof locationChange> =
   (middleware) => () => {
-    const notifications = middleware.getState().notifications;
+    const notifications = select.notifications(middleware.getState());
     const acceptCookiesNotification = notifications.find((notification) =>
       notification.type === getType(acceptCookies)
     );
