8cdfc300c117b32838d412e447b15355a9605c2a
Thomas Woodward <tom.g.woodward@gmail.com>
2019-08-30 13:36:25 -0400

    fix: updates scroll offset to compensate when mobile search toolbar is expanded (#287)
    * handle expando mobile toolbr in search offset
    
    * add specs

--- src/app/content/components/Content.spec.tsx
+++ src/app/content/components/Content.spec.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import createTestStore from '../../../test/createTestStore';
-import mockArchiveLoader, { book, shortPage } from '../../../test/mocks/archiveLoader';
+import mockArchiveLoader, {
+  book,
+  shortPage
+} from '../../../test/mocks/archiveLoader';
 import { mockCmsBook } from '../../../test/mocks/osWebLoader';
 import MobileScrollLock from '../../components/MobileScrollLock';
+import ScrollOffset from '../../components/ScrollOffset';
 import * as Services from '../../context/Services';
 import MessageProvider from '../../MessageProvider';
 import { AppServices, AppState, Store } from '../../types';
 import { openToc } from '../actions';
+import { openMobileToolbar } from '../search/actions';
 import { Book } from '../types';
 import { formatBookData } from '../utils';
 import Content from './Content';
@@ -22,7 +27,11 @@ describe('content', () => {
   const bookState: Book = formatBookData(book, mockCmsBook);
 
   beforeEach(() => {
-    store = createTestStore({navigation: new URL('https://localhost/books/book-slug-1/pages/doesnotmatter')});
+    store = createTestStore({
+      navigation: new URL(
+        'https://localhost/books/book-slug-1/pages/doesnotmatter'
+      ),
+    });
     state = store.getState();
 
     archiveLoader = mockArchiveLoader();
@@ -33,45 +42,99 @@ describe('content', () => {
     state.content.book = bookState;
     state.content.page = shortPage;
 
-    const component = renderer.create(<Provider store={store}>
-      <Services.Provider value={services}>
-        <MessageProvider>
-          <Content />
-        </MessageProvider>
-      </Services.Provider>
-    </Provider>);
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders empty state', () => {
-    const component = renderer.create(<Provider store={store}>
-      <Services.Provider value={services}>
-        <MessageProvider>
-          <Content />
-        </MessageProvider>
-      </Services.Provider>
-    </Provider>);
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('provides the right scroll offset when mobile search collapsed', () => {
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
+
+    const scrollOffset = component.root.findByType(ScrollOffset);
+
+    expect(scrollOffset.props).toMatchInlineSnapshot(`
+            Object {
+              "desktopOffset": 12,
+              "mobileOffset": 10,
+            }
+        `);
+  });
+
+  it('provides the right scroll offset when mobile search collapsed', () => {
+    store.dispatch(openMobileToolbar());
+
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
+
+    const scrollOffset = component.root.findByType(ScrollOffset);
+
+    expect(scrollOffset.props).toMatchInlineSnapshot(`
+      Object {
+        "desktopOffset": 12,
+        "mobileOffset": 15.3,
+      }
+    `);
   });
 
   it('gets page content out of cached archive query', () => {
     state.content.book = bookState;
     state.content.page = shortPage;
 
-    renderer.create(<Provider store={store}>
-      <Services.Provider value={services}>
-        <MessageProvider>
-          <Content />
-        </MessageProvider>
-      </Services.Provider>
-    </Provider>);
+    renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
 
     expect(archiveLoader.mock.cachedPage).toHaveBeenCalledTimes(1);
-    expect(archiveLoader.mock.cachedPage).toHaveBeenCalledWith('testbook1-uuid', '1.0', 'testbook1-testpage4-uuid');
+    expect(archiveLoader.mock.cachedPage).toHaveBeenCalledWith(
+      'testbook1-uuid',
+      '1.0',
+      'testbook1-testpage4-uuid'
+    );
   });
 
   it('page element is still rendered if archive content is unavailable', () => {
@@ -80,27 +143,31 @@ describe('content', () => {
 
     archiveLoader.mock.cachedPage.mockReturnValue(undefined);
 
-    const component = renderer.create(<Provider store={store}>
-      <Services.Provider value={services}>
-        <MessageProvider>
-          <Content />
-        </MessageProvider>
-      </Services.Provider>
-    </Provider>);
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
 
-    const pageComponent = component.root.findByProps({id: 'main-content'});
+    const pageComponent = component.root.findByProps({ id: 'main-content' });
 
     expect(pageComponent).toBeDefined();
   });
 
   it('renders with ToC in null state', () => {
-    const component = renderer.create(<Provider store={store}>
-      <Services.Provider value={services}>
-        <MessageProvider>
-          <Content />
-        </MessageProvider>
-      </Services.Provider>
-    </Provider>);
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
 
     const tableOfContentsComponent = component.root.findByType(TableOfContents);
 
@@ -112,13 +179,15 @@ describe('content', () => {
       store.dispatch(openToc());
     });
 
-    const component = renderer.create(<Provider store={store}>
-      <Services.Provider value={services}>
-        <MessageProvider>
-          <Content />
-        </MessageProvider>
-      </Services.Provider>
-    </Provider>);
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
 
     const tableOfContentsComponent = component.root.findByType(TableOfContents);
     const mobileScrollLock = component.root.findByType(MobileScrollLock);
@@ -131,19 +200,21 @@ describe('content', () => {
   });
 
   it('SidebarControl opens and closes ToC', () => {
-    const component = renderer.create(<Provider store={store}>
-      <Services.Provider value={services}>
-        <MessageProvider>
-          <Content />
-        </MessageProvider>
-      </Services.Provider>
-    </Provider>);
+    const component = renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider>
+            <Content />
+          </MessageProvider>
+        </Services.Provider>
+      </Provider>
+    );
 
     expect(component.root.findByType(TableOfContents).props.isOpen).toBe(null);
 
     renderer.act(() => {
       component.root
-        .findByProps({'aria-label': 'Click to close the Table of Contents'})
+        .findByProps({ 'aria-label': 'Click to close the Table of Contents' })
         .props.onClick();
     });
 
@@ -151,7 +222,7 @@ describe('content', () => {
 
     renderer.act(() => {
       component.root
-        .findByProps({'aria-label': 'Click to open the Table of Contents'})
+        .findByProps({ 'aria-label': 'Click to open the Table of Contents' })
         .props.onClick();
     });
 
