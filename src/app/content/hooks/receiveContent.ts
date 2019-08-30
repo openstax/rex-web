d95b4472e89a05546c3d4de36797fb8ae410bf92
Philip Schatz <253202+philschatz@users.noreply.github.com>
2019-08-30 10:46:05 -0500

    trim the abstract
    because it may just contain an empty div.

--- src/app/content/hooks/receiveContent.ts
+++ src/app/content/hooks/receiveContent.ts
@@ -8,6 +8,13 @@ import * as select from '../selectors';
 import { getCanonicalUrlParams } from '../utils/canonicalUrl';
 import getCleanContent from '../utils/getCleanContent';
 
+const stripHtmlAndTrim = (str: string) => str
+  .replace(/<[^>]*>/g, ' ')
+  .replace(/ +/g, ' ')
+  .trim()
+  .substring(0, 155)
+  .trim();
+
 const hookBody: ActionHookBody<typeof receivePage | typeof receiveBook> = ({
   getState,
   dispatch,
@@ -32,12 +39,9 @@ const hookBody: ActionHookBody<typeof receivePage | typeof receiveBook> = ({
 
   const title = `${page.title} - ${book.title} - OpenStax`;
 
-  const description = (page.abstract ? page.abstract : getCleanContent(book, page, archiveLoader))
-    .replace(/<[^>]*>/g, ' ')
-    .replace(/ +/g, ' ')
-    .trim()
-    .substring(0, 155)
-    .trim();
+  // the abstract could be '<div/>'.
+  const abstract = stripHtmlAndTrim(page.abstract ? page.abstract : '');
+  const description = abstract || stripHtmlAndTrim(getCleanContent(book, page, archiveLoader));
   const canonical = assertDefined(
     await getCanonicalUrlParams(archiveLoader, osWebLoader, book.id, page.shortId),
     'should have found a canonical book and page'
