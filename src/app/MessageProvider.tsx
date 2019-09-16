49be117ab5ff62bd09033673640df17764e2cf92
Thomas Woodward <tom.g.woodward@gmail.com>
2019-09-16 14:22:05 -0400

    polyfill for safari intl (#312)

--- src/app/MessageProvider.tsx
+++ src/app/MessageProvider.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 import enMessages from './messages/en';
 
+if (!Intl.PluralRules) {
+  require('@formatjs/intl-pluralrules/polyfill'); // tslint:disable-line:no-var-requires
+  require('@formatjs/intl-pluralrules/dist/locale-data/en'); // tslint:disable-line:no-var-requires
+}
+
 // tslint:disable-next-line:variable-name
 const MessageProvider: React.SFC = (props) =>
   <IntlProvider locale='en' messages={enMessages}>{props.children}</IntlProvider>;
