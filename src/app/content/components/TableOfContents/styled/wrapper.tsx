7dac4fb44e690a62aeb273f7751061d04aeb2d22
KarinaMendez2 <13000365@galileo.edum>
2019-09-23 11:04:55 -0600

    reverted changes

--- src/app/content/components/TableOfContents/styled/wrapper.tsx
+++ src/app/content/components/TableOfContents/styled/wrapper.tsx
@@ -41,7 +41,6 @@ export const SidebarBody = styled.div<{isOpen: State['tocOpen']}>`
   margin-top: -${toolbarDesktopHeight}rem;
   overflow-y: auto;
   height: calc(100vh - ${navDesktopHeight + bookBannerDesktopMiniHeight}rem);
-  max-height: calc(100vh - ${bookBannerDesktopMiniHeight}rem);
   transition:
     transform ${sidebarTransitionTime}ms ease-in-out,
     box-shadow ${sidebarTransitionTime}ms ease-in-out,
