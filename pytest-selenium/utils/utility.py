f63fafee03419dced0f64a6e0589ec902cc98991
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2019-09-24 11:31:46 -0500

    created dict for book slug and default page

--- pytest-selenium/utils/utility.py
+++ pytest-selenium/utils/utility.py
@@ -2,12 +2,27 @@ import random
 
 
 class Library(object):
+    #     def __init__(self):
+    #         self._book_slug_list = ["chemistry-2e", "chemistry-atoms-first-2e"]
+    #
+    #     @property
+    #     def books(self) -> [str]:
+    #         return self._book_slug_list
+    #
+    #     def random_book_slug(self):
+    #         return random.choice(self.books)
+
     def __init__(self):
-        self._book_slug_list = ["chemistry-2e", "chemistry-atoms-first-2e"]
+        self._book_dict = {
+            "chemistry-2e": "1-introduction",
+            "college-physics": "1-introduction-to-science-and-the-realm-of-physics-physical-quantities-and-units",
+            "chemistry-atoms-first-2e": "1-introduction",
+        }
 
     @property
     def books(self) -> [str]:
-        return self._book_slug_list
+        return self._book_dict
 
     def random_book_slug(self):
-        return random.choice(self.books)
+        random_book_slug = random.choice(list(self.books.keys()))
+        return random_book_slug
