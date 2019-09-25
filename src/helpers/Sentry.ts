68f4f70c0e1da45baafe4b7f155263ca8b0129d4
tom <tom.g.woodward@gmail.com>
2019-09-25 11:48:24 -0400

    add sentry integrations

--- src/helpers/Sentry.ts
+++ src/helpers/Sentry.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 import createSentryMiddleware from 'redux-sentry-middleware';
 import config from '../config';
 
@@ -11,6 +12,11 @@ export default {
       dist: config.RELEASE_ID,
       dsn: 'https://84d2036467d546038347f0ac9ccd8b3b:c815982d89764df583493a60794e54aa@sentry.cnx.org/17',
       environment: config.DEPLOYED_ENV,
+      integrations: [
+        new Integrations.ExtraErrorData(),
+        new Integrations.CaptureConsole(),
+        new Integrations.Dedupe(),
+      ],
       release: `rex@${config.RELEASE_ID}`,
     });
     IS_INITIALIZED = true;
