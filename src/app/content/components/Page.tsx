9bb0cce4cd0476c4344d22ff186d6810bda4c007
tom <tom.g.woodward@gmail.com>
2019-10-10 11:41:39 -0400

    move dom transformations out of pge

--- src/app/content/components/Page.tsx
+++ src/app/content/components/Page.tsx
@@ -1,4 +1,4 @@
-import { HTMLAnchorElement, HTMLButtonElement, HTMLDivElement, HTMLElement, MouseEvent } from '@openstax/types/lib.dom';
+import { HTMLAnchorElement, HTMLDivElement, HTMLElement, MouseEvent } from '@openstax/types/lib.dom';
 import flow from 'lodash/fp/flow';
 import React, { Component } from 'react';
 import { injectIntl, IntlShape } from 'react-intl';
@@ -16,7 +16,7 @@ import * as selectNavigation from '../../navigation/selectors';
 import theme from '../../theme';
 import { AppServices, AppState } from '../../types';
 import { Dispatch } from '../../types';
-import { assertDefined, assertWindow, resetTabIndex, scrollTo } from '../../utils';
+import { assertWindow, resetTabIndex, scrollTo } from '../../utils';
 import { content } from '../routes';
 import * as select from '../selectors';
 import { State } from '../types';
@@ -24,6 +24,7 @@ import getCleanContent from '../utils/getCleanContent';
 import { getBookPageUrlAndParams, toRelativeUrl } from '../utils/urlUtils';
 import { contentTextWidth } from './constants';
 import allImagesLoaded from './utils/allImagesLoaded';
+import { mapSolutions, toggleSolution, transformContent } from './utils/contentDOMTransformations';
 import searchHighlightManager, { mapStateToSearchHighlightProp, stubManager } from './utils/searchHighlightManager';
 
 if (typeof(document) !== 'undefined') {
@@ -152,95 +153,6 @@ export class PageComponent extends Component<PropTypes> {
     return '';
   }
 
-  // from https://github.com/openstax/webview/blob/f95b1d0696a70f0b61d83a85c173102e248354cd
-  // .../src/scripts/modules/media/body/body.coffee#L123
-  private addGenericJs(rootEl: HTMLElement) {
-    this.addScopeToTables(rootEl);
-    this.wrapElements(rootEl);
-    this.tweakFigures(rootEl);
-    this.fixLists(rootEl);
-    this.wrapSolutions(rootEl);
-
-    this.listenersOn();
-  }
-
-  private addScopeToTables(rootEl: HTMLElement) {
-    rootEl.querySelectorAll('table th').forEach((el) => el.setAttribute('scope', 'col'));
-  }
-
-  // Wrap title and content elements in header and section elements, respectively
-  private wrapElements(rootEl: HTMLElement) {
-    rootEl.querySelectorAll(`.example, .exercise, .note, .abstract,
-      [data-type="example"], [data-type="exercise"],
-      [data-type="note"], [data-type="abstract"]`).forEach((el) => {
-
-      // JSDOM does not support `:scope` in .querySelectorAll() so use .matches()
-      const titles = Array.from(el.children).filter((child) => child.matches('.title, [data-type="title"], .os-title'));
-
-      const bodyWrap = assertDefined(document, 'document should be defined').createElement('section');
-      bodyWrap.append(...Array.from(el.childNodes));
-
-      const titleWrap = assertDefined(document, 'document should be defined').createElement('header');
-      titleWrap.append(...titles);
-
-      el.append(titleWrap, bodyWrap);
-
-      // Add an attribute for the parents' `data-label`
-      // since CSS does not support `parent(attr(data-label))`.
-      // When the title exists, this attribute is added before it
-      const label = el.getAttribute('data-label');
-      if (label) {
-        titles.forEach((title) => title.setAttribute('data-label-parent', label));
-      }
-
-      // Add a class for styling since CSS does not support `:has(> .title)`
-      // NOTE: `.toggleClass()` explicitly requires a `false` (not falsy) 2nd argument
-      if (titles.length > 0) {
-        el.classList.add('ui-has-child-title');
-      }
-    });
-  }
-
-  private tweakFigures(rootEl: HTMLElement) {
-    // move caption to bottom of figure
-    rootEl.querySelectorAll('figure > figcaption').forEach((el) => {
-      const parent = assertDefined(el.parentElement, 'figcaption parent should always be defined');
-      parent.classList.add('ui-has-child-figcaption');
-      parent.appendChild(el);
-    });
-  }
-
-  private fixLists(rootEl: HTMLElement) {
-    // Copy data-mark-prefix and -suffix from ol to li so they can be used in css
-    rootEl.querySelectorAll(`ol[data-mark-prefix] > li, ol[data-mark-suffix] > li,
-    [data-type="list"][data-list-type="enumerated"][data-mark-prefix] > [data-type="item"],
-    [data-type="list"][data-list-type="enumerated"][data-mark-suffix] > [data-type="item"]`).forEach((el) => {
-      const parent = assertDefined(el.parentElement, 'list parent should always be defined');
-      const markPrefix = parent.getAttribute('data-mark-prefix');
-      const markSuffix = parent.getAttribute('data-mark-suffix');
-      if (markPrefix) { el.setAttribute('data-mark-prefix', markPrefix); }
-      if (markSuffix) { el.setAttribute('data-mark-suffix', markSuffix); }
-    });
-    rootEl.querySelectorAll('ol[start], [data-type="list"][data-list-type="enumerated"][start]').forEach((el) => {
-      el.setAttribute('style', `counter-reset: list-item ${el.getAttribute('start')}`);
-    });
-  }
-
-  private wrapSolutions(rootEl: HTMLElement) {
-    const title = this.props.intl.formatMessage({id: 'i18n:content:solution:toggle-title'});
-
-    // Wrap solutions in a div so "Show/Hide Solutions" work
-    rootEl.querySelectorAll('.exercise .solution, [data-type="exercise"] [data-type="solution"]').forEach((el) => {
-      const contents = el.innerHTML;
-      el.innerHTML = `
-        <div class="ui-toggle-wrapper">
-          <button class="btn-link ui-toggle" title="${title}"></button>
-        </div>
-        <section class="ui-body" role="alert">${contents}</section>
-      `;
-    });
-  }
-
   private getScrollTarget(): Element | null {
     return this.container.current && typeof(window) !== 'undefined' && this.props.hash
       ? this.container.current.querySelector(`[id="${this.props.hash.replace(/^#/, '')}"]`)
@@ -253,14 +165,6 @@ export class PageComponent extends Component<PropTypes> {
     }
   }
 
-  private mapSolutions(cb: (a: HTMLButtonElement) => void) {
-    if (this.container.current) {
-      Array.from(this.container.current.querySelectorAll<HTMLButtonElement>(
-        '[data-type="solution"] > .ui-toggle-wrapper > .ui-toggle, .solution > .ui-toggle-wrapper > .ui-toggle'
-      )).forEach(cb);
-    }
-  }
-
   private listenersOn() {
     this.listenersOff();
 
@@ -270,8 +174,8 @@ export class PageComponent extends Component<PropTypes> {
       a.addEventListener('click', handler);
     });
 
-    this.mapSolutions((button) => {
-      const handler = this.toggleSolution(button);
+    mapSolutions(this.container.current, (button) => {
+      const handler = toggleSolution(button, this.props.intl);
       this.clickListeners.set(button, handler);
       button.addEventListener('click', handler);
     });
@@ -286,25 +190,8 @@ export class PageComponent extends Component<PropTypes> {
     };
 
     this.mapLinks(removeIfExists);
-    this.mapSolutions(removeIfExists);
+    mapSolutions(this.container.current, removeIfExists);
   }
-
-  private toggleSolution = (button: HTMLElement) => () => {
-    if (!button.parentElement || !button.parentElement.parentElement) {
-      return;
-    }
-    const solution = button.parentElement.parentElement;
-
-    if (solution.classList.contains('ui-solution-visible')) {
-      solution.classList.remove('ui-solution-visible');
-      solution.removeAttribute('aria-expanded');
-      solution.setAttribute('aria-label', this.props.intl.formatMessage({id: 'i18n:content:solution:show'}));
-    } else {
-      solution.className += ' ui-solution-visible';
-      solution.setAttribute('aria-expanded', '');
-      solution.setAttribute('aria-label', this.props.intl.formatMessage({id: 'i18n:content:solution:hide'}));
-    }
-  };
 
   private clickListener = (anchor: HTMLAnchorElement) => (e: MouseEvent) => {
     const {references, navigate, book, page, locationState} = this.props;
@@ -348,7 +235,7 @@ export class PageComponent extends Component<PropTypes> {
       return;
     }
 
-    this.addGenericJs(container);
+    transformContent(container, this.props.intl);
     this.listenersOn();
 
     const promise = typesetMath(container, assertWindow());
