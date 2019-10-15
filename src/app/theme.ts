71398bd464de18e5fcbcb9efc995ebf3ba77683d
tom <tom.g.woodward@gmail.com>
2019-10-15 11:03:28 -0400

    reorganize z-indexes

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -96,14 +96,17 @@ export default {
   },
   color,
   padding,
-  zIndex: {
-    contentNotifications: 4,
-    focusedHiddenLink: 20,
-    modal: 12,
-    navbar: 5,
-    overlay: 4,
-    searchSidebar: 2,
-    sidebar: 4,
-    toolbar: 3,
-  },
+  zIndex: [
+    'contentNotifications',
+    'searchSidebar',
+    'toolbar',
+    'overlay',
+    'sidebar',
+    'navbar',
+    'modal',
+    'focusedHiddenLink',
+  ].reduce((result, key, index) => {
+    result[key] = (index + 1) * 10;
+    return result;
+  }, {} as {[key: string]: number}),
 };
