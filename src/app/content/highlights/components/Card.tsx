f8bb4749e602d2f2d1302cf7ff17f9fe1e0d4482
tom <tom.g.woodward@gmail.com>
2019-10-22 11:16:26 -0400

    breakpoints for showing note card next to content are working

--- src/app/content/highlights/components/Card.tsx
+++ src/app/content/highlights/components/Card.tsx
@@ -1,10 +1,14 @@
 import { Highlight } from '@openstax/highlighter';
 import React from 'react';
+import { connect } from 'react-redux';
 import styled, { css } from 'styled-components/macro';
 import theme from '../../../theme';
+import { AppState } from '../../../types';
 import { remsToEms } from '../../../utils';
-import { contentTextWidth, sidebarDesktopWidth } from '../../components/constants';
-import { isOpenConnector, styleWhenSidebarClosed } from '../../components/utils/sidebar';
+import { contentTextWidth, searchResultsBarDesktopWidth, sidebarDesktopWidth } from '../../components/constants';
+import { styleWhenSidebarClosed } from '../../components/utils/sidebar';
+import * as selectSearch from '../../search/selectors';
+import * as contentSelect from '../../selectors';
 import { cardContentMargin, cardMinWindowMargin, cardWidth } from '../constants';
 
 interface Props {
@@ -20,6 +24,22 @@ const Card = ({highlight, className}: Props) => {
     hello {highlight.id}
   </div>;
 };
+
+/*
+ * putting overflow hidden on a page wrapper that aligns with the window edge would
+ * include the sidebar, which would break position: sticky.
+ *
+ * avoiding using an overflow hidden to hide cards when there is not enough space
+ * means being very explicit about hiding them so they don't create a horizontal
+ * scrollbar.
+ *
+ * in this case that means using extensive knowledge about the container widths,
+ * which unfortunately means knowledge of all the sidebar widths and their state
+ * too.
+ *
+ * consider making a helper like `styleWhenSidebarClosed` maybe `styleWhenContentWidth`
+ * that has a selector to get the relevant stuff.
+ */
 
 const additionalWidthForCard = (cardWidth + cardContentMargin + cardMinWindowMargin) * 2;
 
@@ -41,15 +61,29 @@ const StyledCard = styled(Card)`
     `)}
   }
 
-  ${theme.breakpoints.mobile(css`
-    display: unset;
-  `)}
+  @media (max-width: ${remsToEms(contentTextWidth + searchResultsBarDesktopWidth + additionalWidthForCard)}em) {
+    /* the window is too small to show note cards next to content when search is open */
 
-  @media (max-width: ${remsToEms(contentTextWidth + additionalWidthForCard)}em) {
-    /* the window is too small to show note cards next to content even with sidebars closed */
-    display: none;
+    ${(props: {hasQuery: boolean}) => !!props.hasQuery && css`
+      display: none;
+    `}
   }
+
+  ${theme.breakpoints.mobile(css`
+    /* reset desktop breaks */
+    display: unset;
+
+    @media (max-width: ${remsToEms(contentTextWidth + additionalWidthForCard)}em) {
+      /* the window is too small to show note cards next to content even without sidebars */
+      display: none;
+    }
+  `)}
 
 `;
 
-export default isOpenConnector(StyledCard);
+export default connect(
+  (state: AppState) => ({
+    hasQuery: !!selectSearch.query(state),
+    isOpen: contentSelect.tocOpen(state),
+  })
+)(StyledCard);
