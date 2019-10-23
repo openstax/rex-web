81dadf6667cdb32384f26e7c939bb35faed3df58
tom <tom.g.woodward@gmail.com>
2019-10-23 11:10:59 -0400

    use real colors

--- src/app/content/highlights/constants.ts
+++ src/app/content/highlights/constants.ts
@@ -7,7 +7,9 @@ export const cardFocusedContentMargin = 1;
 export const cardMinWindowMargin = 2;
 
 export const highlightStyles = [
-  {label: 'yellow', passive: '#cbcfff', focused: '#545ec8'},
+  {label: 'yellow', passive: '#ffff8a', focused: '#fed200'},
+  {label: 'green', passive: '#def99f', focused: '#92d101'},
   {label: 'blue', passive: '#c8f5ff', focused: '#00c3ed'},
+  {label: 'purple', passive: '#cbcfff', focused: '#545ec8'},
   {label: 'red', passive: '#ffc5e1', focused: '#de017e'},
 ];
