6d40d48532c74b10290963f8c448878032af0a2a
KarinaMendez2 <13000365@galileo.edum>
2019-10-23 20:17:23 -0600

    indent with 2 spaces

--- src/app/notifications/components/NotificationsWrapper.tsx
+++ src/app/notifications/components/NotificationsWrapper.tsx
@@ -8,24 +8,24 @@ const notificationWidth = 30;
 
 // tslint:disable-next-line:variable-name
 export const ChooseNotification = styled(Notifications)`
-    width: ${notificationWidth}rem;
-    margin-left: calc(100% - ${notificationWidth}rem);
-    overflow: visible;
-    position: -webkit-sticky;
-    position: fixed;
-    padding-top: 1px;
-    margin-top: -1px;
-    z-index: ${theme.zIndex.contentNotifications};
+  width: ${notificationWidth}rem;
+  margin-left: calc(100% - ${notificationWidth}rem);
+  overflow: visible;
+  position: -webkit-sticky;
+  position: fixed;
+  padding-top: 1px;
+  margin-top: -1px;
+  z-index: ${theme.zIndex.contentNotifications};
 
-    @media (max-width: ${inlineDisplayBreak}) {
-        box-shadow: inset 0 -0.2rem 0.2rem 0 rgba(0, 0, 0, 0.14);
-        background-color: ${theme.color.neutral.darker};
-        margin: 0;
-        height: auto;
-        width: 100%;
-        padding: 1rem;
-    }
+  @media (max-width: ${inlineDisplayBreak}) {
+    box-shadow: inset 0 -0.2rem 0.2rem 0 rgba(0, 0, 0, 0.14);
+    background-color: ${theme.color.neutral.darker};
+    margin: 0;
+    height: auto;
+    width: 100%;
+    padding: 1rem;
+  }
 `;
 
 // tslint:disable-next-line:variable-name
-export const NotificationsWrapper: React.SFC = () => <ChooseNotification/>;
+export const NotificationsWrapper: React.SFC = () => <ChooseNotification />;
