7d1a8ce4a0bbc64a09bdf8d3cafdd7e7ce9ac8ec
KarinaMendez2 <13000365@galileo.edum>
2019-10-25 10:43:28 -0600

    removing class

--- src/app/notifications/components/UpdatesAvailable.tsx
+++ src/app/notifications/components/UpdatesAvailable.tsx
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Button, { ButtonGroup } from '../../components/Button';
 import { Body, Group, Header, P } from './Card';
 
-export default class UpdatesAvailable extends Component<{className?: string}> {
-  public reload = () => {
-    if (typeof(window) !== 'undefined') {
-      window.location.reload(true);
-    }
-  };
+const reload = () => {
+  if (typeof(window) !== 'undefined') {
+    window.location.reload(true);
+  }
+};
 
   // tslint:disable-next-line:variable-name
-  public render = () => <Body className={this.props.className}>
-    <Group>
-      <FormattedMessage id='i18n:notification:update:header'>
-        {(txt) => (<Header>{txt}</Header>)}
-      </FormattedMessage>
-      <FormattedMessage id='i18n:notification:update:body'>
-        {(txt) => (<P>{txt}</P>)}
-      </FormattedMessage>
-    </Group>
-    <ButtonGroup>
-      <FormattedMessage id='i18n:notification:update:reload'>
-        {(txt) => (<Button variant='primary' onClick={this.reload}>{txt}</Button>)}
-      </FormattedMessage>
-    </ButtonGroup>
-  </Body>;
-}
+const UpdatesAvailable = ({className}: {className?: string}) => <Body className={className}>
+  <Group>
+    <FormattedMessage id='i18n:notification:update:header'>
+      {(txt) => (<Header>{txt}</Header>)}
+    </FormattedMessage>
+    <FormattedMessage id='i18n:notification:update:body'>
+      {(txt) => (<P>{txt}</P>)}
+    </FormattedMessage>
+  </Group>
+  <ButtonGroup>
+    <FormattedMessage id='i18n:notification:update:reload'>
+      {(txt) => (<Button variant='primary' onClick={reload}>{txt}</Button>)}
+    </FormattedMessage>
+  </ButtonGroup>
+</Body>;
+
+export default UpdatesAvailable;
