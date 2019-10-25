1e63e066d3ae8f39ab59988a6e6b73f1608cfa57
tom <tom.g.woodward@gmail.com>
2019-10-25 16:17:13 -0400

    fix specs

--- src/app/content/highlights/components/Note.spec.tsx
+++ src/app/content/highlights/components/Note.spec.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
 import renderer from 'react-test-renderer';
 import { renderToDom } from '../../../../test/reactutils';
+import MessageProvider from '../../../MessageProvider';
 import Note from './Note';
 
 describe('Note', () => {
   it('matches snapshot', () => {
-    const component = renderer.create(<Note />);
+    const component = renderer.create(<MessageProvider>
+      <Note note='' onChange={() => null} />
+    </MessageProvider>);
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('resizes on update when necessary', () => {
-    const {node} = renderToDom(<Note />);
+    const {node, root} = renderToDom(<MessageProvider>
+      <Note note='' onChange={() => null} />
+    </MessageProvider>);
 
     Object.defineProperty(node, 'scrollHeight', { value: 100 });
     Object.defineProperty(node, 'offsetHeight', { value: 50 });
 
-    ReactTestUtils.Simulate.change(node);
+    renderToDom(<MessageProvider>
+      <Note note='asdf' onChange={() => null} />
+    </MessageProvider>, root);
 
     expect(node.style.height).toEqual('105px');
   });
 
   it('doesn\'t resize on update when unneccessary', () => {
-    const {node} = renderToDom(<Note />);
+    const {node, root} = renderToDom(<MessageProvider>
+      <Note note='' onChange={() => null} />
+    </MessageProvider>);
 
     Object.defineProperty(node, 'scrollHeight', { value: 50 });
     Object.defineProperty(node, 'offsetHeight', { value: 50 });
 
-    ReactTestUtils.Simulate.change(node);
+    renderToDom(<MessageProvider>
+      <Note note='asdf' onChange={() => null} />
+    </MessageProvider>, root);
 
     expect(node.style.height).toEqual('');
   });
