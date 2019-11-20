e195bdf20b886a6a1f249194606ad1b045850764
tom <tom.g.woodward@gmail.com>
2019-11-20 16:23:26 -0500

    :100:s

---
+++ src/app/content/highlights/components/ColorIndicator.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { highlightStyles } from '../constants';
+import ColorIndicator from './ColorIndicator';
+
+describe('ColorIndicator', () => {
+  it('matches snapshot', () => {
+    const component = renderer.create(<ColorIndicator style={highlightStyles[0]} />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('matches snapshot (checked)', () => {
+    const component = renderer.create(<ColorIndicator checked style={highlightStyles[0]} />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/app/content/highlights/components/ColorPicker.spec.tsx
+++ src/app/content/highlights/components/ColorPicker.spec.tsx
@@ -20,6 +20,16 @@ describe('ColorPicker', () => {
     rest.forEach((input) => expect(input.props.checked).toEqual(false));
   });
 
+  it('checks selection (multiple)', () => {
+    const component = renderer.create(
+      <ColorPicker multiple selected={[highlightStyles[0].label]} onChange={jest.fn()} onRemove={jest.fn()} />
+    );
+
+    const [first, ...rest] = component.root.findAllByType('input');
+    expect(first.props.checked).toEqual(true);
+    rest.forEach((input) => expect(input.props.checked).toEqual(false));
+  });
+
   it('calls update when changing selection', () => {
     const onChange = jest.fn();
     const onRemove = jest.fn();
@@ -36,7 +46,23 @@ describe('ColorPicker', () => {
     expect(onChange).toHaveBeenCalledWith(highlightStyles[1].label);
   });
 
-  it('calls update when changing selection', () => {
+  it('calls update when changing selection (multiple)', () => {
+    const onChange = jest.fn();
+    const onRemove = jest.fn();
+
+    const component = renderer.create(
+      <ColorPicker multiple selected={[highlightStyles[0].label]} onChange={onChange} onRemove={onRemove} />
+    );
+
+    const [, second] = component.root.findAllByType('input');
+
+    second.props.onChange();
+
+    expect(onRemove).not.toHaveBeenCalled();
+    expect(onChange).toHaveBeenCalledWith([highlightStyles[0].label, highlightStyles[1].label]);
+  });
+
+  it('calls remove when changing selection', () => {
     const onChange = jest.fn();
     const onRemove = jest.fn();
 
@@ -50,5 +76,21 @@ describe('ColorPicker', () => {
 
     expect(onRemove).toHaveBeenCalled();
     expect(onChange).not.toHaveBeenCalled();
+  });
+
+  it('calls remove when changing selection (multiple)', () => {
+    const onChange = jest.fn();
+    const onRemove = jest.fn();
+
+    const component = renderer.create(
+      <ColorPicker multiple selected={[highlightStyles[0].label]} onChange={onChange} onRemove={onRemove} />
+    );
+
+    const [first] = component.root.findAllByType('input');
+
+    first.props.onChange();
+
+    expect(onRemove).not.toHaveBeenCalled();
+    expect(onChange).toHaveBeenCalledWith([]);
   });
 });
