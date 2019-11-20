4a5840093f08bad560fc56154dc7500e286fde75
tom <tom.g.woodward@gmail.com>
2019-11-20 13:54:47 -0500

    :camera:

--- src/app/components/Dropdown.spec.tsx
+++ src/app/components/Dropdown.spec.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import MessageProvider from '../MessageProvider';
-import Dropdown, { DropdownItem } from './Dropdown';
+import Dropdown, { DropdownItem, DropdownList } from './Dropdown';
 
 describe('Dropdown', () => {
   it('matches snapshot', () => {
     const component = renderer.create(<MessageProvider>
-      <Dropdown toggle='i18n:highlighting:card:show-more'>
-        <DropdownItem onClick={() => null} message='i18n:highlighting:dropdown:delete' />
-        <DropdownItem onClick={() => null} href='/wooo' message='i18n:highlighting:dropdown:edit' />
+      <Dropdown toggle={<button>show more</button>}>
+        <DropdownList>
+          <DropdownItem onClick={() => null} message='i18n:highlighting:dropdown:delete' />
+          <DropdownItem onClick={() => null} href='/wooo' message='i18n:highlighting:dropdown:edit' />
+        </DropdownList>
       </Dropdown>
     </MessageProvider>);
 
