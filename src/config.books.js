51b8ae53b4884afa3407074a80ab985f7bebf247
KarinaMendez2 <13000365@galileo.edum>
2019-12-05 10:33:42 -0600

    adding algebra and trigonometry to books

--- src/config.books.js
+++ src/config.books.js
@@ -1,4 +1,5 @@
 const books = {
+  /* Algebra and Trigonometry */ '13ac107a-f15f-49d2-97e8-60ab2e3b519c': {defaultVersion: '17.12'},
   /* Am Gov 2e */ '9d8df601-4f12-4ac1-8224-b450bf739e5f': {defaultVersion: '5.9'},
   /* Anatomy & Physiology */ '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22': {defaultVersion: '16.7'},
   /* Astronomy */ '2e737be8-ea65-48c3-aa0a-9f35b4c6a966': {defaultVersion: '21.9'},
