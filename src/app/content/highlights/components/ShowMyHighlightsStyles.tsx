23c26710276171463d5d06be79d5891dac36cff8
Karina Méndez <13000365@galileo.edu>
2019-12-06 10:13:20 -0600

    ft: myhighlights popup - show highlights (#382)
    * initial styles for showing highlights in popup
    
    * removed console log
    
    * styles and functionality for
    go to top button and loader
    
    * Add loading flag
    
    * re orginized styles, moved
    loader, added timer to highlights by page promise
    
    * added timers for tests
    and loader to work around
    highlights, created tests for code coverage in
    back to top button
    
    * added test for unmounting
    without refs and for loader
    
    * screenshot updated
    
    * added test for onscroll to
    not render without children,
    separated highlights pop up
    tests from show my highlights test, renamed some functions.
    
    * lint error fix
    
    * revert snap
    
    * fix spec
    
    * removed store and providers from on scroll test
    added style to center pop up on large screens
    changed some elements in
    show my highlights test
    to simplify them
    
    * switched a test location
    
    * ordered imports

---
+++ src/app/content/highlights/components/ShowMyHighlightsStyles.tsx
@@ -0,0 +1,129 @@
+import styled, { css } from 'styled-components';
+import { AngleUp } from 'styled-icons/fa-solid/AngleUp';
+import { labelStyle, textRegularStyle } from '../../../components/Typography';
+import { h4Style } from '../../../components/Typography/headings';
+import theme from '../../../theme';
+import { highlightStyles } from '../constants';
+import { PopupBody, popupBodyPadding, popupPadding } from './HighlightStyles';
+
+// tslint:disable-next-line:variable-name
+export const HighlightsChapter = styled.div`
+  ${h4Style}
+  height: 5.6rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  background: ${theme.color.neutral.darker};
+  padding: 0 ${popupPadding}rem;
+  width: 100%;
+  ${theme.breakpoints.mobile(css`
+    padding: 0 ${popupPadding}rem;
+  `)}
+`;
+
+// tslint:disable-next-line:variable-name
+export const ShowMyHighlightsBody = styled(PopupBody)`
+  padding: 0;
+  ${theme.breakpoints.mobile(css`
+    text-align: left;
+    padding: 0;
+  `)}
+`;
+
+// tslint:disable-next-line:variable-name
+export const HighlightWrapper = styled.div`
+  margin: 0 ${popupPadding}rem 1.6rem;
+  border: solid 0.1rem ${theme.color.neutral.darkest};
+`;
+
+// tslint:disable-next-line:variable-name
+export const HighlightSection = styled.div`
+  ${labelStyle}
+  padding: 0 ${popupBodyPadding}rem 0 ${popupPadding}rem;
+  background: ${theme.color.neutral.darkest};
+  height: 3.2rem;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+`;
+
+// tslint:disable-next-line:variable-name
+export const HighlightOuterWrapper = styled.div`
+  :not(:last-child) {
+    border-bottom: solid 0.2rem ${theme.color.neutral.darker};
+  }
+`;
+
+// tslint:disable-next-line:variable-name
+export const HighlightContent = styled.span`
+  ${textRegularStyle}
+  line-height: unset;
+`;
+
+// tslint:disable-next-line:variable-name
+export const HighlightNote = styled.div`
+  ${textRegularStyle}
+  padding-top: 1.2rem;
+  display: flex;
+
+  span {
+    margin: 0 0.8rem 0 0;
+    overflow: visible;
+  }
+`;
+
+// tslint:disable-next-line:variable-name
+export const HighlightContentWrapper = styled.div`
+  padding: 1.2rem ${popupBodyPadding}rem;
+  ${(props: {color: string}) => {
+    const style = highlightStyles.find((search) => search.label === props.color);
+
+    if (!style) {
+      return null;
+    }
+
+    return css`
+      border-left: solid 0.8rem ${style.focused};
+
+      ${HighlightContent} {
+        background-color: ${style.passive};
+      }
+
+      ${HighlightNote} > span {
+        color: ${style.focused};
+      }
+    `;
+  }}
+`;
+
+// tslint:disable-next-line:variable-name
+export const GoToTopWrapper = styled.div`
+  width: 4.8rem;
+  height: 4.8rem;
+  position: absolute;
+  z-index: 1;
+  bottom: calc(4.8rem + ${popupBodyPadding}rem);
+  right: calc(4.8rem + ${popupBodyPadding}rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+// tslint:disable-next-line:variable-name
+export const GoToTop = styled.div`
+  width: 2.4rem;
+  height: 2.4rem;
+  background: #959595;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+`;
+
+// tslint:disable-next-line:variable-name
+export const GoToTopIcon = styled(AngleUp)`
+  width: 1.6rem;
+  height: 1.6rem;
+`;
