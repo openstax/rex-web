46df01127ce9ed29a605fba0105b45df5fc5a7d6
tom <tom.g.woodward@gmail.com>
2019-12-10 14:06:50 -0500

    fix build

--- script/doctor-workbox.bash
+++ script/doctor-workbox.bash
@@ -6,11 +6,13 @@ set -e
 echo "HACKING WORKBOX SCRIPT WITH STRING REPLACEMENT"
 echo "remove this when offical solution is merged https://github.com/facebook/create-react-app/pull/5369"
 
+worker="${BASH_SOURCE%/*}/../build/service-worker.js"
+
 sed -Ei '' \
   -e '1h;2,$H;$!d;g' \
   -e "s/\"\/index\.html.*}\);//" \
-  "build/service-worker.js"
+  "$worker"
 
 echo -n "\"/index.html\"), {
   blacklist: [/^\/accounts/,/^\/_/,/\/[^\/]+\.[^\/]+$/],
-});" >> "build/service-worker.js"
+});" >> "$worker"
