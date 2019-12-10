25272655918e803175b96cde85c0816f3923792a
tom <tom.g.woodward@gmail.com>
2019-12-10 15:32:37 -0500

    absolute path for worker

--- script/doctor-workbox.bash
+++ script/doctor-workbox.bash
@@ -6,7 +6,11 @@ set -e
 echo "HACKING WORKBOX SCRIPT WITH STRING REPLACEMENT"
 echo "remove this when offical solution is merged https://github.com/facebook/create-react-app/pull/5369"
 
-worker="${BASH_SOURCE%/*}/../build/service-worker.js"
+function abs_path {
+  (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
+}
+
+worker=$(abs_path "${BASH_SOURCE%/*}/../build")/service-worker.js
 
 echo "$worker"
 
