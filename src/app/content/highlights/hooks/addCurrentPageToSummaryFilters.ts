1c4c19ead9d623f20224cc588998ab75bf04ec6e
PiotrKozlowski <piotr@obrandowani.pl>
2019-12-18 16:15:53 +0100

    remove console logs

--- src/app/content/highlights/hooks/addCurrentPageToSummaryFilters.ts
+++ src/app/content/highlights/hooks/addCurrentPageToSummaryFilters.ts
@@ -8,23 +8,18 @@ import * as select from '../selectors';
 export const addCurrentPageToSummaryFilters = ({
   dispatch, getState,
 }: MiddlewareAPI & AppServices) => {
-  console.log('hello')
   const state = getState();
   const page = pageSelector(state);
   const sections = bookSections(state);
   const filters = select.summaryFilters(state);
-  console.log('sections.size ', sections.size, page )
   if (!sections.size || !page || typeof(window) === 'undefined') {
     return;
   }
-  console.log('hello2')
 
   const chapterToAdd = getCurrentChapter(sections, page);
   const idToAdd = chapterToAdd ? stripIdVersion(chapterToAdd.id) : null;
-  console.log('hello3', idToAdd)
 
   if (idToAdd && !filters.chapters.includes(idToAdd)) {
-    console.log('hello4')
     dispatch(setSummaryFilters({
       ...filters,
       chapters: [...filters.chapters, idToAdd],
