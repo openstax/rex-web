d95363b60a8eb6ba0bcf5ab66d0ba466e2efbdac
tom <tom.g.woodward@gmail.com>
2020-01-07 10:49:41 -0500

    no pager in script

--- concourse/check-pr-size/script.bash
+++ concourse/check-pr-size/script.bash
@@ -4,13 +4,7 @@ set -e
 
 cd rex-web-pull-request
 
-git status
-
-git diff master --numstat ':(exclude)*.snap' ':(exclude)yarn.lock'
-
-git diff master --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}'
-
-diffcount=$(git diff master --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}')
+diffcount=$(git --no-pager diff master --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}')
 if [ "$diffcount" -gt "200" ]
 then
   exit 1
