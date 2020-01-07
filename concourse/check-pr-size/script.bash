9300a5a41fc2ba606cfee89a9313502b85f65cfe
tom <tom.g.woodward@gmail.com>
2020-01-07 10:44:19 -0500

    Debug code

--- concourse/check-pr-size/script.bash
+++ concourse/check-pr-size/script.bash
@@ -4,6 +4,12 @@ set -e
 
 cd rex-web-pull-request
 
+git status
+
+git diff master --numstat ':(exclude)*.snap' ':(exclude)yarn.lock'
+
+git diff master --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}'
+
 diffcount=$(git diff master --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}')
 if [ "$diffcount" -gt "200" ]
 then
