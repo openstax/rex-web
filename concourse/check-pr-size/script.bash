e683bf80042fc7f5f60709f74304d57f6ce0a841
tom <tom.g.woodward@gmail.com>
2020-01-07 11:27:38 -0500

    removing debug

--- concourse/check-pr-size/script.bash
+++ concourse/check-pr-size/script.bash
@@ -1,25 +1,10 @@
 #!/usr/bin/env bash
 
-set -exv
+set -ex
 
 cd rex-web-pull-request
 
 base_sha=$(<.git/resource/base_sha)
-head_sha=$(<.git/resource/head_sha)
-
-echo "$head_sha"
-
-git rev-parse HEAD
-
-git checkout "$head_sha"
-
-ls
-
-git status
-
-git --no-pager diff "$base_sha"
-
-git --no-pager diff "$base_sha" --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}'
 
 diffcount=$(git --no-pager diff "$base_sha" --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}')
 
