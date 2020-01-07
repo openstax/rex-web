87ec0a75fcd0a9b44bc64b88b27048ba27a99174
tom <tom.g.woodward@gmail.com>
2020-01-07 11:35:02 -0500

    remove more debug

--- concourse/check-pr-size/script.bash
+++ concourse/check-pr-size/script.bash
@@ -8,8 +8,6 @@ base_sha=$(<.git/resource/base_sha)
 
 diffcount=$(git --no-pager diff "$base_sha" --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}')
 
-echo "$diffcount"
-
 if [ "$diffcount" -gt "200" ]
 then
   exit 1
