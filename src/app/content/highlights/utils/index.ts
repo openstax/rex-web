7f346ac1abf597d87f40e5134e34a20aa0c97fb9
tom <tom.g.woodward@gmail.com>
2020-01-21 15:39:52 -0500

    Remove unused util

--- src/app/content/highlights/utils/index.ts
+++ src/app/content/highlights/utils/index.ts
@@ -2,7 +2,6 @@ export { default as addCurrentPageToSummaryFilters } from './addCurrentPageToSum
 export { default as getHighlightLocationFilterForPage } from './getHighlightLocationFilterForPage';
 export { default as getHighlightLocationFilters } from './getHighlightLocationFilters';
 export { default as getHighlightLocationFiltersWithContent } from './getHighlightLocationFiltersWithContent';
-export { default as getHighlightColorFiltersWithContent } from './getHighlightColorFiltersWithContent';
 export {
   addToTotalCounts,
   updateInTotalCounts,
@@ -12,4 +11,3 @@ export {
   updateSummaryHighlight,
   updateSummaryHighlightsDependOnFilters,
 } from './summaryHighlightsUtils';
-export { default as reduceColorCounts } from './reduceColorCounts';
