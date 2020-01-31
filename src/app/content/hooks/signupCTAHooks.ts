ba0aff7289472dd3895892219b15a69113eee8bf
Piotr Kozłowski <piotr@obrandowani.pl>
2020-01-31 21:00:06 +0100

    Analytics for cta popup (#469)
    * Add analytics tracking
    
    * rejigger some stuff and add missing close event
    
    * fix tests
    
    * remove duplicate event
    
    * revert test changes
    
    * Remove unused event type
    
    Co-authored-by: Thomas Woodward <tom.g.woodward@gmail.com>

--- src/app/content/hooks/signupCTAHooks.ts
+++ src/app/content/hooks/signupCTAHooks.ts
@@ -9,26 +9,33 @@ import { showCTAPopup } from '../selectors';
 const pageLoadedAt = new Date().getTime();
 const showPopupAfter = 5000;
 
-export const receiveLoggedOutHookBody: ActionHookBody<typeof receiveLoggedOut> = (services) => async() => {
-  const state = services.getState();
-  const showCTA = showCTAPopup(state);
+export const receiveLoggedOutHookBody: ActionHookBody<typeof receiveLoggedOut> =
+  ({analytics, getState, dispatch}) => async() => {
+    const state = getState();
+    const showCTA = showCTAPopup(state);
+    const trackShow = analytics.signupCTA.bind(state);
 
-  if (showCTA === null && !assertWindow().matchMedia(theme.breakpoints.mobileQuery).matches) {
-    const now = new Date().getTime();
-    const timeFromPageLoaded = now - pageLoadedAt;
-    setTimeout(() => {
-      services.dispatch(openCallToActionPopup());
-    }, Math.max(showPopupAfter - timeFromPageLoaded, 0));
-  }
-};
+    if (showCTA === null && !assertWindow().matchMedia(theme.breakpoints.mobileQuery).matches) {
+      const now = new Date().getTime();
+      const timeFromPageLoaded = now - pageLoadedAt;
+      setTimeout(() => {
+        trackShow('open');
+        dispatch(openCallToActionPopup());
+      }, Math.max(showPopupAfter - timeFromPageLoaded, 0));
+    }
+  };
 
-export const locationChangeHookBody: ActionHookBody<typeof locationChange> = (services) => async() => {
-  const showCTA = showCTAPopup(services.getState());
+export const locationChangeHookBody: ActionHookBody<typeof locationChange> =
+  ({analytics, getState, dispatch}) => async() => {
+    const state = getState();
+    const showCTA = showCTAPopup(state);
+    const trackClose = analytics.signupCTA.bind(state);
 
-  if (showCTA === true) {
-    services.dispatch(closeCallToActionPopup());
-  }
-};
+    if (showCTA === true) {
+      trackClose('close by navigating');
+      dispatch(closeCallToActionPopup());
+    }
+  };
 
 export const showCTAPopupHook = actionHook(receiveLoggedOut, receiveLoggedOutHookBody);
 export const closeCTAPopupHook = actionHook(locationChange, locationChangeHookBody);
