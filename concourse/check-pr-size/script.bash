278bcc22315afdce81358b0bf3ac3920eb398f44
tom <tom.g.woodward@gmail.com>
2020-02-13 12:20:27 -0500

    pr size check was too aggressive

--- concourse/check-pr-size/script.bash
+++ concourse/check-pr-size/script.bash
@@ -8,7 +8,7 @@ base_sha=$(<.git/resource/base_sha)
 
 diffcount=$(git --no-pager diff "$base_sha" --numstat ':(exclude)*.snap' ':(exclude)yarn.lock' | cut -f1,2 | awk '{s+=$1+$2}END{print s}')
 
-if [ "$diffcount" -gt "200" ]
+if [ "$diffcount" -gt "250" ]
 then
   exit 1
 fi
