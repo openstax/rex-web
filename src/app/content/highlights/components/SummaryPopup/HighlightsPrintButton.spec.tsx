2b6de2002176af85dd96a05db37861dea58694b4
janKozie1 <jankkoziel@gmail.com>
2020-02-20 14:04:44 +0100

    removed unnecessary import

--- src/app/content/highlights/components/SummaryPopup/HighlightsPrintButton.spec.tsx
+++ src/app/content/highlights/components/SummaryPopup/HighlightsPrintButton.spec.tsx
@@ -1,6 +1,5 @@
 import noop from 'lodash/fp/noop';
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import createTestStore from '../../../../../test/createTestStore';
