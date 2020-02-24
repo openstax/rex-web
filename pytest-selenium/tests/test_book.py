174205b00df123da914ad987eeeae12b31c7c302
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2020-02-24 15:23:26 -0600

    test for buy book link

--- pytest-selenium/tests/test_book.py
+++ pytest-selenium/tests/test_book.py
@@ -22,3 +22,20 @@ def test_book_title_links_to_books_detail_page(selenium, base_url, book_slug, pa
     expected_page_url = base_url + "/details/books/" + book_slug
 
     assert expected_page_url == osweb.current_url
+
+
+@markers.test_case("C583482")
+@markers.parametrize("page_slug", ["preface"])
+@markers.nondestructive
+def test_buy_book_link(selenium, base_url, book_slug, page_slug):
+
+    # GIVEN: A page is loaded
+    content = Content(selenium, base_url, book_slug=book_slug, page_slug=page_slug).open()
+
+    # WHEN: Click on 'Buy Book' link in toolbar
+    content.buy_book.click()
+
+    # THEN: The Amazon link should be opened in a new tab
+
+
+# https://www.amazon.com/s?me=A1540JPBBI3F06&qid=1517336719
