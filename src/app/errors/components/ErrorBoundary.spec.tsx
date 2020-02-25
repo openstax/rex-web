446940b7edc4a0a2056e2fdf1738d0596c8fef2e
janKozie1 <jankkoziel@gmail.com>
2020-02-25 20:48:54 +0100

    Fixed existing tests

--- src/app/errors/components/ErrorBoundary.spec.tsx
+++ src/app/errors/components/ErrorBoundary.spec.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import Sentry from '../../../helpers/Sentry';
-import createTestStore from '../../../test/createTestStore';
 import MessageProvider from '../../MessageProvider';
-import { Store } from '../../types';
 import ErrorBoundary from './ErrorBoundary';
 
 jest.mock('../../../helpers/Sentry', () => ({
-  captureException: jest.fn().mockReturnValue('randomErrorId'),
+  captureException: jest.fn(),
 }));
 
 // tslint:disable-next-line:variable-name
@@ -18,27 +15,19 @@ const Buggy: React.SFC = () => {
 
 describe('ErrorBoundary', () => {
   let consoleError: jest.SpyInstance;
-  let store: Store;
-
   beforeEach(() => {
-    store = createTestStore();
     consoleError = jest
       .spyOn(console, 'error')
       .mockImplementation((msg) => msg);
   });
+
   afterEach(() => {
     consoleError.mockRestore();
   });
 
   it('matches snapshot', () => {
     const tree = renderer
-      .create(<Provider store={store}>
-        <MessageProvider>
-          <ErrorBoundary>
-            <Buggy />
-          </ErrorBoundary>
-        </MessageProvider>
-      </Provider>)
+      .create(<MessageProvider><ErrorBoundary><Buggy /></ErrorBoundary></MessageProvider>)
       .toJSON();
     expect(tree).toMatchSnapshot();
     expect(consoleError).toHaveBeenCalledWith(
