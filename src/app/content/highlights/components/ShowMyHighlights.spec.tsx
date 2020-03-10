778582d8f3b6f3114ff9da8f79da47e28414663a
janKozie1 <jankkoziel@gmail.com>
2020-03-10 14:41:58 +0100

    fixed and expanded some existing tests

--- src/app/content/highlights/components/ShowMyHighlights.spec.tsx
+++ src/app/content/highlights/components/ShowMyHighlights.spec.tsx
@@ -108,9 +108,11 @@ describe('Show my highlights', () => {
     const dispatch = jest.spyOn(store, 'dispatch');
 
     const {root} = renderToDom(<Provider store={store}>
-      <MessageProvider>
-        <ShowMyHighlights/>
-      </MessageProvider>
+      <Services.Provider value={createTestServices()} >
+        <MessageProvider>
+          <ShowMyHighlights/>
+        </MessageProvider>
+      </Services.Provider>
     </Provider>);
     const target = root.querySelector('[data-testid="show-myhighlights-body"]');
     if (!target) {
@@ -267,9 +269,11 @@ describe('Show my highlights', () => {
     });
 
     const {root} = renderToDom(<Provider store={store}>
-      <MessageProvider>
-        <ShowMyHighlights/>
-      </MessageProvider>
+      <Services.Provider value={createTestServices()} >
+        <MessageProvider>
+          <ShowMyHighlights/>
+        </MessageProvider>
+      </Services.Provider>
     </Provider>);
 
     const target = root.querySelector('[data-testid="show-myhighlights-body"]');
@@ -322,9 +326,11 @@ describe('Show my highlights', () => {
     });
 
     const {root} = renderToDom(<Provider store={store}>
-      <MessageProvider>
-        <ShowMyHighlights/>
-      </MessageProvider>
+      <Services.Provider value={createTestServices()} >
+        <MessageProvider>
+          <ShowMyHighlights/>
+        </MessageProvider>
+      </Services.Provider>
     </Provider>);
 
     const target = root.querySelector('[data-testid="show-myhighlights-body"]');
