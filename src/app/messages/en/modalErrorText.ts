18a9e6fcd75d8fd20b4841b675a6869d5316077b
Jan Kozieł <48291123+janKozie1@users.noreply.github.com>
2020-03-30 17:06:13 +0200

    updated error message (#559)
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>

--- src/app/messages/en/modalErrorText.ts
+++ src/app/messages/en/modalErrorText.ts
@@ -1,4 +1,4 @@
 export const bodyWithLink = `
-  We're not quite sure what went wrong. You can try refreshing the page, or contact
-  our <a href="{supportCenterLink}" target="_blank">Support Center</a> for help.
+  We're not quite sure what went wrong. Refresh the page. If this doesn't solve
+  the problem, visit our <a href="{supportCenterLink}" target="_blank">Support Center</a>.
 `;
