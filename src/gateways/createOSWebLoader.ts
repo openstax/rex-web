af6e6c8b6261b0b7ff67fd76790404f97f676997
PiotrKozlowski <piotr@obrandowani.pl>
2020-04-03 17:05:46 +0200

    fix - do not modify symlink file

--- src/gateways/createOSWebLoader.ts
+++ src/gateways/createOSWebLoader.ts
@@ -24,7 +24,7 @@ interface OSWebResponse {
   items: OSWebBook[];
 }
 
-export const fields = 'cnx_id,authors,publish_date,cover_color';
+export const fields = 'cnx_id,authors,publish_date,cover_color,amazon_link';
 
 export default (prefix: string) => {
   const baseUrl = `${prefix}/v2/pages`;
