729030d2c07d4055c384fdcd6896113097588fcf
tom <tom.g.woodward@gmail.com>
2020-04-20 13:57:01 -0400

    updates available popup works

--- src/app/notifications/components/UpdatesAvailable.tsx
+++ src/app/notifications/components/UpdatesAvailable.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import { ServiceWorker, ServiceWorkerRegistration } from '@openstax/types/lib.dom';
+import React, { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+import Sentry from '../../../helpers/Sentry';
 import Button, { ButtonGroup } from '../../components/Button';
+import { useServices } from '../../context/Services';
 import { Body, Group, Header, P } from './Card';
 
 const reload = () => {
@@ -9,21 +12,97 @@ const reload = () => {
   }
 };
 
+const handleEventOnce = (sw: ServiceWorkerRegistration | ServiceWorker, event: string, handler: () => void) => {
+  const deregisteringHandler = () => {
+    sw.removeEventListener(event, deregisteringHandler);
+    handler();
+  };
+  sw.addEventListener(event, deregisteringHandler);
+};
+
+const handleStateChangeOnce = (sw: ServiceWorker, state: ServiceWorker['state'], handler: () => void) => {
+  const filteringHandler = () => {
+    if (sw.state === state) {
+      handler();
+    }
+  };
+  handleEventOnce(sw, 'statechange', filteringHandler);
+};
+
+const workerNeedsUpdate = (sw: ServiceWorkerRegistration | undefined): sw is ServiceWorkerRegistration => {
+  if (!sw) {
+    return false;
+  }
+  if (sw.waiting || sw.installing) {
+    return false;
+  }
+
+  return true;
+};
+
+const awaitInstalled = (sw: ServiceWorkerRegistration, callback: () => void) => () => {
+  const {installing} = sw;
+  if (installing) {
+    handleStateChangeOnce(installing, 'installed', callback);
+  } else {
+    callback();
+  }
+};
+
+/*
+ * the service worker will update on its own after a refresh, but if we're going to
+ * prompt people we want to make sure its ready before the refresh
+ */
+const useUpdateServiceWorker = (sw: ServiceWorkerRegistration | undefined, callback: () => void) => useEffect(() => {
+  if (workerNeedsUpdate(sw)) {
+    const handler = awaitInstalled(sw, callback);
+    handleEventOnce(sw, 'updatefound', handler);
+
+    sw.update().catch((e) => {
+      Sentry.captureException(e);
+      callback();
+    });
+  }
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+
+const doReload = (sw: ServiceWorkerRegistration | undefined) => () => {
+  const {waiting} = sw || {waiting:  undefined};
+
+  if (waiting) {
+    handleStateChangeOnce(waiting, 'activating', reload);
+    waiting.postMessage({type:  'SKIP_WAITING'});
+  } else {
+    reload();
+  }
+};
+
   // tslint:disable-next-line:variable-name
-const UpdatesAvailable = ({className}: {className?: string}) => <Body className={className}>
-  <Group>
-    <FormattedMessage id='i18n:notification:update:header'>
-      {(txt) => (<Header>{txt}</Header>)}
-    </FormattedMessage>
-    <FormattedMessage id='i18n:notification:update:body'>
-      {(txt) => (<P>{txt}</P>)}
-    </FormattedMessage>
-  </Group>
-  <ButtonGroup>
-    <FormattedMessage id='i18n:notification:update:reload'>
-      {(txt) => (<Button variant='primary' onClick={reload}>{txt}</Button>)}
-    </FormattedMessage>
-  </ButtonGroup>
-</Body>;
+const UpdatesAvailable = ({className}: {className ?: string}) => {
+  const sw = useServices().serviceWorker;
+  const [readyToReload, setReadyToReload] = useState<boolean>(!workerNeedsUpdate(sw));
+
+  useUpdateServiceWorker(sw, () => setReadyToReload(true));
+
+  if (!readyToReload) {
+    return null;
+  }
+
+  return <Body className={className}>
+    <Group>
+      <FormattedMessage id='i18n:notification:update:header'>
+        {(txt) => (<Header>{txt}</Header>)}
+      </FormattedMessage>
+      <FormattedMessage id='i18n:notification:update:body'>
+        {(txt) => (<P>{txt}</P>)}
+      </FormattedMessage>
+    </Group>
+    <ButtonGroup>
+      <FormattedMessage id='i18n:notification:update:reload'>
+        {(txt) => (<Button variant='primary' onClick={doReload(sw)}>{txt}</Button>)}
+      </FormattedMessage>
+    </ButtonGroup>
+  </Body>;
+};
 
 export default UpdatesAvailable;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Loadable from 'react-loadable';
 import createApp from './app';
 import { onPageFocusChange } from './app/domUtils';
+import { updateAvailable } from './app/notifications/actions';
 import { assertDefined, assertWindow } from './app/utils';
 import config from './config';
 import './content.css';
@@ -16,6 +17,7 @@ import { registerGlobalAnalytics } from './helpers/analytics';
 import loadFont from './helpers/loadFont';
 import { startMathJax } from './helpers/mathjax';
 import pollUpdates from './helpers/pollUpdates';
+import Sentry from './helpers/Sentry';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
@@ -86,10 +88,21 @@ window.onfocus = onPageFocusChange(true, app);
 registerGlobalAnalytics(window, app.store);
 
 // start long running processes
-pollUpdates(app.store);
 startMathJax();
+pollUpdates(app.store);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register()
+  .then((registration) => {
+    app.services.serviceWorker = registration;
+
+    console.log(
+      registration, registration.waiting, registration.waiting && registration.waiting.state === 'installed'
+    );
+    if (registration && registration.waiting && registration.waiting.state === 'installed') {
+      app.store.dispatch(updateAvailable());
+    }
+  })
+  .catch((e) => {
+    Sentry.captureException(e);
+  });
