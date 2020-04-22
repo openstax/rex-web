ecc93541b6aed54c7856e1b1f51e45e7e827cceb
PiotrKozlowski <piotr@obrandowani.pl>
2020-04-22 14:26:20 +0200

    Fix tests for CardWrapper

--- src/app/content/highlights/components/CardWrapper.spec.tsx
+++ src/app/content/highlights/components/CardWrapper.spec.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import createTestStore from '../../../../test/createTestStore';
 import createMockHighlight from '../../../../test/mocks/highlight';
 import Card from './Card';
 import CardWrapper from './CardWrapper';
 
-jest.mock('./Card', () => () => null);
+jest.mock('./Card', () => () => <span data-mock-card />);
+
+jest.mock('./cardUtils', () => ({
+  ...jest.requireActual('./cardUtils'),
+  getHighlightTopOffset: jest.fn(() => 100),
+}));
 
 describe('CardWrapper', () => {
-  it('matches snapshot', () => {
+  const store = createTestStore();
+
+  it('matches snapshot', async() => {
     const component = renderer.create(<CardWrapper
       highlights={[createMockHighlight()]}
+      store={store}
     />);
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('renders cards', () => {
+  it('renders cards', async() => {
     const component = renderer.create(<CardWrapper
       highlights={[createMockHighlight(), createMockHighlight()]}
+      store={store}
     />);
 
     expect(component.root.findAllByType(Card).length).toBe(2);
