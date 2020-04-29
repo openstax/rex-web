60aefe2a5c46b31630fe8cefbdbc83c33464b4e5
PiotrKozlowski <piotr@obrandowani.pl>
2020-04-29 15:59:26 +0200

    fix losing focus when clicking another card

--- src/app/content/highlights/components/utils/onClickOutside.ts
+++ src/app/content/highlights/components/utils/onClickOutside.ts
@@ -1,9 +1,14 @@
-import { HTMLElement, MouseEvent } from '@openstax/types/lib.dom';
+import { EventListenerOptions, HTMLElement, MouseEvent } from '@openstax/types/lib.dom';
 import React from 'react';
 import { elementDescendantOf } from '../../../../domUtils';
 import { assertDocument, assertWindow } from '../../../../utils';
 
-const onClickOutside = (element: React.RefObject<HTMLElement>, isFocused: boolean, cb: () => void) => () => {
+const onClickOutside = (
+  element: React.RefObject<HTMLElement>,
+  isFocused: boolean,
+  cb: () => void,
+  eventOptions?: EventListenerOptions
+) => () => {
   if (typeof document === 'undefined') {
     return () => null;
   }
@@ -19,14 +24,19 @@ const onClickOutside = (element: React.RefObject<HTMLElement>, isFocused: boolea
   };
 
   if (isFocused) {
-    document.addEventListener('click', ifOutside);
+    document.addEventListener('click', ifOutside, eventOptions);
   }
 
-  return () => assertDocument().removeEventListener('click', ifOutside);
+  return () => assertDocument().removeEventListener('click', ifOutside, eventOptions);
 };
 
-export const useOnClickOutside = (element: React.RefObject<HTMLElement>, isEnabled: boolean, cb: () => void) => {
-  React.useEffect(onClickOutside(element, isEnabled, cb), [isEnabled]);
+export const useOnClickOutside = (
+  element: React.RefObject<HTMLElement>,
+  isEnabled: boolean,
+  cb: () => void,
+  eventOptions?: EventListenerOptions
+) => {
+  React.useEffect(onClickOutside(element, isEnabled, cb, eventOptions), [isEnabled]);
 };
 
 export default onClickOutside;
