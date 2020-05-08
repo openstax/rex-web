3a4c4b80f315eec23228cb327c00133db4461dc0
KarinaMendez2 <13000365@galileo.edum>
2020-05-08 09:01:15 -0600

    deleted unused file, simplified animation, updated snap

--- src/app/content/components/TableOfContents/styled/wrapper.tsx
+++ src/app/content/components/TableOfContents/styled/wrapper.tsx
@@ -18,7 +18,6 @@ import { CloseSidebarControl, ToCButtonText } from '../../SidebarControl';
 import { toolbarIconStyles } from '../../Toolbar/iconStyles';
 import { disablePrint } from '../../utils/disablePrint';
 import { styleWhenSidebarClosed } from '../../utils/sidebar';
-import { keyFrameExampleOne } from './keyframeExamples';
 
 const sidebarPadding = 1.8;
 
@@ -43,20 +42,10 @@ export const SidebarBody = styled.div<{isOpen: State['tocOpen']}>`
   overflow-y: auto;
   height: calc(100vh - ${navDesktopHeight + bookBannerDesktopMiniHeight}rem);
   max-height: calc(100vh - ${bookBannerDesktopMiniHeight}rem);
-  /*transition:
-    transform ${sidebarTransitionTime}ms ease-in-out,
-    box-shadow ${sidebarTransitionTime}ms ease-in-out,
-    background-color ${sidebarTransitionTime}ms ease-in-out;*/
-
-  animation-name: ${keyFrameExampleOne};
-  animation-duration: 1s;
-  animation-timing-function: ease;
-  animation-delay: 0s;
-  animation-iteration-count: 1;
+  animation: 2s ease 0s 1;
   animation-direction: normal;
   animation-fill-mode: forwards;
   animation-play-state: running;
-
   background-color: ${theme.color.neutral.darker};
   z-index: ${theme.zIndex.sidebar};
   margin-left: -50vw;
