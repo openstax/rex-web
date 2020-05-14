29505dbf3d200f91559f4b0891e9a255e59826f5
PiotrKozlowski <piotr@obrandowani.pl>
2020-05-14 15:40:05 +0200

    Add console log for debugging

--- src/app/content/components/utils/attachHighlight.ts
+++ src/app/content/components/utils/attachHighlight.ts
@@ -5,9 +5,11 @@ const attachHighlight = (
   highlight: Highlight | SerializedHighlight,
   highlighter: Highlighter
 ) => {
+  console.log('Trying to attach', highlight.id, highlight)
   highlighter.highlight(highlight);
   const result = highlighter.getHighlight(highlight.id);
   if (!result || !result.isAttached()) {
+    console.log('error while attaching')
     Sentry.captureException(new Error(`Highlight with id: ${highlight.id} has not been attached.`));
   }
   return result;
