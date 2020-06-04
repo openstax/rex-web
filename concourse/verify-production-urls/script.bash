c583e36b38d7d0c95dc6e5fa0af2ff353d61fc64
janKozie1 <jankkoziel@gmail.com>
2020-06-04 17:04:22 +0200

    added exit 1 for missing books

--- concourse/verify-production-urls/script.bash
+++ concourse/verify-production-urls/script.bash
@@ -3,10 +3,12 @@ set -ex
 bucket=sandbox-unified-web-primary
 production_url=https://openstax.org
 
-release_id=$(curl "${production_url}/rex/release.json" | jq .id -r)
+work_dir=$(pwd)
 
+release_id=$(curl "${production_url}/rex/release.json" | jq .id -r)
 files=$(aws s3api list-objects --bucket "$bucket" --prefix "rex/releases/$release_id/books/" | jq -r '.Contents[] | .Key')
 
-while IFS= read -r line; do
-  $line
+while IFS= read -r full_path; do
+  book_path=$(grep -oP '/books.*' <<< "$full_path")
+  [ ! -f "$work_dir$book_path" ] && exit 1
 done <<< "$files"
