2b7ff8c7ee7eebef6ae21dce7ccd5762b6bdf81b
janKozie1 <jankkoziel@gmail.com>
2020-06-04 14:27:12 +0200

    looping over the values in correct bucket

--- concourse/verify-production-urls/script.bash
+++ concourse/verify-production-urls/script.bash
@@ -4,25 +4,35 @@ bucket=sandbox-unified-web-primary
 production_url=https://openstax.org
 stack_region=us-east-1
 
-release_id=$(curl "${production_url}/rex/release.json" | jq .id)
-release_id=master/5333be8
-env_name=$(aws cloudformation describe-stacks --region $stack_region --query \
-  "Stacks[?Parameters[?(ParameterKey=='RexReleaseId' && ParameterValue=='$release_id')]] \
-  |[0].Parameters[?ParameterKey=='EnvName'] \
-  |[0].ParameterValue")
-
-env_name=foo
+release_id=$(curl "${production_url}/rex/release.json" | jq .id -r)
+#release_id=master/5333be8
+# env_name=$(aws cloudformation describe-stacks --region $stack_region --query \
+#   "Stacks[?Parameters[?(ParameterKey=='RexReleaseId' && ParameterValue=='$release_id')]] \
+#   |[0].Parameters[?ParameterKey=='EnvName'] \
+#   |[0].ParameterValue")
 
 #echo "$stack"
 #aws s3api list-buckets --query "Buckets[].Name"
 #--query "Contents[?contains(Key, 'rex')]"
 #
-entries=$(aws s3api list-objects --bucket "$bucket" --prefix "rex/releases/$env_name/books/")
+files=$(aws s3api list-objects --bucket "$bucket" --prefix "rex/releases/$release_id/books/" | jq -r '.Contents[] | .Key')
 
-for entry in $(jq '.Contents | keys | .[]' <<< "$entires"); do
-  name=$($entry | jq .Key)
-  echo "$name"
-done
+while IFS= read -r line; do
+  $line
+done <<< "$files"
+# entries |  | \ 
+#   while read key 
+#   do
+#     echo "$key"
+#     echo "keyssssssss---------"
+#   done
+
+
+
+# for entry in $(jq '.Contents | keys | .[]' <<< "$entires"); do
+#   name=$($entry | jq .Key)
+#   echo "$name"
+# done
 
 # for book in $books; do
 #   echo "$book"
