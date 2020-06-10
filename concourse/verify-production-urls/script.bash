b3233cf039c4b4234625daf4bdeb8c54b706842a
janKozie1 <jankkoziel@gmail.com>
2020-06-10 09:33:06 +0200

    applied requested changes

--- concourse/verify-production-urls/script.bash
+++ concourse/verify-production-urls/script.bash
@@ -4,15 +4,17 @@ set -ex
 production_url=https://openstax.org
 
 work_dir=$(pwd)
+failed=0
 
 release_id=$(curl "${production_url}/rex/environment.json" | jq .release_id -r)
 files=$(aws s3api list-objects --bucket "$PROD_UNIFIED_S3_BUCKET" --prefix "rex/releases/$release_id/books/" | jq -r '.Contents[] | .Key')
 
-missing_files=""
-
 for full_path in $files; do
   book_path="/release/$(grep -oP 'books.*' <<< "$full_path")"
-  [ ! -f "$work_dir$book_path" ] && missing_files="$missing_files$book_path'\n'"
+  if [ ! -f "$work_dir$book_path" ]; then
+    echo "$book_path";
+    failed=1;
+  fi;
 done
 
-echo "$missing_files"
+exit "$failed"
