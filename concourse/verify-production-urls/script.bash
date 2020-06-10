9d7345498d900291b14a1e9506275cdda242958b
janKozie1 <jankkoziel@gmail.com>
2020-06-10 09:41:09 +0200

    renamed to accurate variable name

--- concourse/verify-production-urls/script.bash
+++ concourse/verify-production-urls/script.bash
@@ -10,9 +10,9 @@ release_id=$(curl "${production_url}/rex/environment.json" | jq .release_id -r)
 files=$(aws s3api list-objects --bucket "$PROD_UNIFIED_S3_BUCKET" --prefix "rex/releases/$release_id/books/" | jq -r '.Contents[] | .Key')
 
 for full_path in $files; do
-  book_path="/release/$(grep -oP 'books.*' <<< "$full_path")"
-  if [ ! -f "$work_dir$book_path" ]; then
-    echo "$book_path";
+  file_path="/release/$(grep -oP 'books.*' <<< "$full_path")"
+  if [ ! -f "$work_dir$file_path" ]; then
+    echo "$file_path";
     failed=1;
   fi;
 done
