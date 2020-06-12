967510451d1b49866ca1486350da6dd96e03e8a6
janKozie1 <jankkoziel@gmail.com>
2020-06-12 14:42:22 +0200

    one more guard for 'books'

--- concourse/verify-production-urls/script.bash
+++ concourse/verify-production-urls/script.bash
@@ -5,12 +5,12 @@ production_url=https://openstax.org
 
 work_dir=$(pwd)
 failed=0
-
+PROD_UNIFIED_S3_BUCKET=sandbox-unified-web-primary
 release_id=$(curl "${production_url}/rex/environment.json" | jq .release_id -r)
 files=$(aws s3api list-objects --bucket "$PROD_UNIFIED_S3_BUCKET" --prefix "rex/releases/$release_id/books/" | jq -r '.Contents[] | .Key')
 
 for full_path in $files; do
-  file_path="/release/$(sed -e "s/.*\(books\/.*\)/\1/" <<< "$full_path")"
+  file_path="/release/$(sed -e "s/.*\/\(books\/.*\)/\1/" <<< "$full_path")"
   if [ ! -f "$work_dir$file_path" ]; then
     echo "$file_path";
     failed=1;
