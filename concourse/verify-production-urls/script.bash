f3324d38b82254ac61e59f1c207f65d97eec688d
tom <tom.g.woodward@gmail.com>
2020-06-17 14:09:46 -0400

    Reduce noise in url checker script output

--- concourse/verify-production-urls/script.bash
+++ concourse/verify-production-urls/script.bash
@@ -6,13 +6,16 @@ production_url=https://openstax.org
 work_dir=$(pwd)
 failed=0
 
-release_id=$(curl "${production_url}/rex/environment.json" | jq .release_id -r)
-files=$(aws s3api list-objects --bucket "$PROD_UNIFIED_S3_BUCKET" --prefix "rex/releases/$release_id/books/" | jq -r '.Contents[] | .Key')
+release_id=$(curl -s "${production_url}/rex/environment.json" | jq .release_id -r)
+files=$(aws --profile openstax s3api list-objects --bucket "$PROD_UNIFIED_S3_BUCKET" --prefix "rex/releases/$release_id/books/" | jq -r '.Contents[] | .Key')
+number=$(wc -l <<< "$files" | awk '{$1=$1};1')
+
+echo "testing $number urls..."
 
 for full_path in $files; do
   file_path="/release/$(sed -e "s/.*\/\(books\/.*\)/\1/" <<< "$full_path")"
   if [ ! -f "$work_dir$file_path" ]; then
-    echo "$file_path";
+    echo "MISSING PATH: $file_path";
     failed=1;
   fi;
 done
