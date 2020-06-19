0a5f47229bce75c0dd1225562c4df08de6d2b393
PiotrKozlowski <piotr@obrandowani.pl>
2020-06-19 15:34:06 +0200

    Show nudge only every 4 weeks up to 3 times

--- src/app/content/components/NudgeStudyTools/constants.ts
+++ src/app/content/components/NudgeStudyTools/constants.ts
@@ -9,3 +9,10 @@ export const closeButtonMobileMargin = 3;
 export const contentMarginTop = 1;
 
 export const spotlightPadding = 1;
+
+export const cookieNudgeStudyGuidesDate = 'nudge_study_guides_date';
+export const cookieNudgeStudyGuidesCounter = 'nudge_study_guides_counter';
+
+// 4 weeks: 4 * 7 days * 24 hours * 60 minutes * 60 seconds * 1000 ms
+export const timeIntervalBetweenShowingNudgeInMs = 4 * 7 * 24 * 60 * 60 * 1000;
+export const nudgeStudyToolsShowLimit = 3;
--- src/app/content/components/NudgeStudyTools/index.tsx
+++ src/app/content/components/NudgeStudyTools/index.tsx
@@ -1,12 +1,20 @@
+import * as Cookies from 'js-cookie';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useDebouncedMatchMobileQuery } from '../../../reactUtils';
 import { assertDocument, assertNotNull } from '../../../utils';
-import { closeNudgeStudyTools } from '../../actions';
+import { closeNudgeStudyTools, openNudgeStudyTools } from '../../actions';
 import { showNudgeStudyTools } from '../../selectors';
+import { studyGuidesSummaryIsNotEmpty } from '../../studyGuides/selectors';
 import arrowDesktop from './assets/arrowDesktop.svg';
 import arrowMobile from './assets/arrowMobile.svg';
+import {
+  cookieNudgeStudyGuidesCounter,
+  cookieNudgeStudyGuidesDate,
+  nudgeStudyToolsShowLimit,
+  timeIntervalBetweenShowingNudgeInMs,
+} from './constants';
 import {
   NudgeArrow,
   NudgeBackground,
@@ -28,7 +36,28 @@ const NudgeStudyTools = () => {
   const show = useSelector(showNudgeStudyTools);
   const target = useGetStudyToolsTarget();
   const positions = usePositions(target, isMobile);
+  const hasStudyGuides = useSelector(studyGuidesSummaryIsNotEmpty);
   const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (show === null && hasStudyGuides) {
+      const counter = Number(Cookies.get(cookieNudgeStudyGuidesCounter) || 0);
+      const lastShownDate = Cookies.get(cookieNudgeStudyGuidesDate);
+      const now = new Date();
+      const passedAllowedInterval = Boolean(lastShownDate
+        && (now.getTime() - new Date(lastShownDate).getTime()) > timeIntervalBetweenShowingNudgeInMs);
+
+      if (
+        counter < nudgeStudyToolsShowLimit
+        && (!lastShownDate || passedAllowedInterval)
+      ) {
+        Cookies.set(cookieNudgeStudyGuidesCounter, (counter + 1).toString());
+        Cookies.set(cookieNudgeStudyGuidesDate, now.toString());
+        // TODO: Track opens
+        dispatch(openNudgeStudyTools());
+      }
+    }
+  }, [show, hasStudyGuides, dispatch]);
 
   const close = () => {
     dispatch(closeNudgeStudyTools());
