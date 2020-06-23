12cc0125a9ecbd32b6c70acbbd0315cae25759ba
PiotrKozlowski <piotr@obrandowani.pl>
2020-06-23 14:59:48 +0200

    100% test coverage

--- src/app/content/components/NudgeStudyTools/index.spec.tsx
+++ src/app/content/components/NudgeStudyTools/index.spec.tsx
@@ -5,12 +5,15 @@ import createTestServices from '../../../../test/createTestServices';
 import createTestStore from '../../../../test/createTestStore';
 import * as Services from '../../../context/Services';
 import MessageProvider from '../../../MessageProvider';
+import * as reactUtils from '../../../reactUtils';
 import { AppServices, Store } from '../../../types';
 import { assertDocument } from '../../../utils';
-import { openNudgeStudyTools } from '../../actions';
+import { closeNudgeStudyTools, openNudgeStudyTools } from '../../actions';
 import * as contentSelect from '../../selectors';
 import * as studyGuidesSelect from '../../studyGuides/selectors';
 import NudgeStudyTools from './';
+import arrowMobile from './assets/arrowMobile.svg';
+import { NudgeArrow, NudgeCloseButton } from './styles';
 import * as utils from './utils';
 
 jest.mock('react-dom', () => ({
@@ -109,5 +112,48 @@ describe('NudgeStudyTools', () => {
     </Provider>);
 
     expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('dispatches action on clicking close button and tests if body has overflow style set to hidden', async() => {
+    jest.spyOn(contentSelect, 'showNudgeStudyTools')
+      .mockReturnValue(true);
+
+    jest.spyOn(reactUtils, 'useDebouncedMatchMobileQuery')
+      .mockReturnValue(true);
+
+    jest.spyOn(utils, 'usePositions')
+      .mockReturnValue({
+        arrowLeft: 1200,
+        arrowTopOffset: 245,
+        closeButtonLeft: 1500,
+        closeButtonTopOffset: 345,
+        contentWrapperRight: -486,
+        contentWrapperTopOffset: 385,
+        spotlightHeight: 45,
+        spotlightLeftOffset: 1190,
+        spotlightTopOffset: 190,
+        spotlightWidth: 300,
+      });
+
+    const component = renderer.create(<Provider store={store}>
+      <Services.Provider value={services}>
+        <MessageProvider>
+          <NudgeStudyTools/>
+        </MessageProvider>
+      </Services.Provider>
+    </Provider>);
+
+    renderer.act(() => {
+      expect(assertDocument().body.style.overflow).toEqual('hidden');
+    });
+
+    renderer.act(() => {
+      const arrow = component.root.findByType(NudgeArrow);
+      expect(arrow.props.src).toEqual(arrowMobile);
+      const button = component.root.findByType(NudgeCloseButton);
+      button.props.onClick();
+    });
+
+    expect(dispatch).toHaveBeenCalledWith(closeNudgeStudyTools());
   });
 });
