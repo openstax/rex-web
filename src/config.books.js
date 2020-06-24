cad5187dd3ccd6a8552c75f255f3d3a22eb06e71
devops@openstax.org <devops@openstax.org>
2020-06-24 05:11:00 +0000

    update content

--- src/config.books.js
+++ src/config.books.js
@@ -33,7 +33,7 @@ const books = {
   /* Precalculus */ 'fd53eae1-fa23-47c7-bb1b-972349835c3c': {defaultVersion: '13.30'},
   /* Principles of Accounting Vol 1 */ '9ab4ba6d-1e48-486d-a2de-38ae1617ca84': {defaultVersion: '5.2'},
   /* Principles of Accounting Vol 2 */ '920d1c8a-606c-4888-bfd4-d1ee27ce1795': {defaultVersion: '15.1'},
-  /* Principles of MicroEcon for AP courses 2e */ '636cbfd9-4e37-4575-83ab-9dec9029ca4e': {defaultVersion: '9.3'},
+  /* Principles of MicroEcon for AP courses 2e */ '636cbfd9-4e37-4575-83ab-9dec9029ca4e': {defaultVersion: '9.6'},
   /* Principles of MacroEcon for AP courses 2e */ '9117cf8c-a8a3-4875-8361-9cb0f1fc9362': {defaultVersion: '9.1'},
   /* Principles of Economics 2e */ 'bc498e1f-efe9-43a0-8dea-d3569ad09a82': {defaultVersion: '8.7'},
   /* Principles of Macroeconomics 2e */ '27f59064-990e-48f1-b604-5188b9086c29': {defaultVersion: '10.7'},
