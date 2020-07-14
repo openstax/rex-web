83e42866a280a099b04876bfcbe1170727d0951e
tom <tom.g.woodward@gmail.com>
2020-07-14 12:05:45 -0400

    such rejigger

--- concourse/check-modified-books/script.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd rex-web-pull-request || exit 1
-git remote add origin "https://${GITHUB_ACCESS_TOKEN}@github.com/openstax/rex-web.git"
-yarn
-
-./script/scan-modified-books.bash
--- script/get-modified-books.bash
+++ script/get-modified-books.bash
@@ -4,18 +4,20 @@ set -ex
 git fetch origin master
 git show origin/master:src/config.books.js > src/config.books.old.js
 
-book_ids=$(node -e "$(cat <<script
+book_json=$(node -e "$(cat <<script
   const oldBooks = require('./src/config.books.old.js');
   const newBooks = require('./src/config.books.js');
 
-  Object.keys(newBooks).forEach((key) => {
+  const modified = Object.keys(newBooks).map((key) => {
     if (oldBooks[key] === undefined || newBooks[key].defaultVersion !== oldBooks[key].defaultVersion) {
-      console.log(key);
+      return {book: key, version: newBooks[key].defaultVersion};
     }
-  });
+  }).filter(record => !!record);
+
+  console.log(JSON.stringify(modified));
 script
 )")
 
 rm src/config.books.old.js
 
-echo "$book_ids"
+echo "$book_json"
