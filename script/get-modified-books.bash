4cba8b69de5601153f48fabfe6f0235fd3e73113
tom <tom.g.woodward@gmail.com>
2020-07-14 15:06:38 -0400

    hopefully nicer check title

--- script/get-modified-books.bash
+++ script/get-modified-books.bash
@@ -10,7 +10,7 @@ book_json=$(node -e "$(cat <<script
 
   const modified = Object.keys(newBooks).map((key) => {
     if (oldBooks[key] === undefined || newBooks[key].defaultVersion !== oldBooks[key].defaultVersion) {
-      return {book_id: key, book_version: newBooks[key].defaultVersion};
+      return {title: '', book_version: newBooks[key].defaultVersion, book_id: key};
     }
   }).filter(record => !!record);
 
