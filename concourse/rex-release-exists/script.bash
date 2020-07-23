86b55af9b9fd75fadff41af2a0c2c28df71e97fa
tom <tom.g.woodward@gmail.com>
2020-07-23 16:52:28 -0400

    better dev nulling

--- concourse/rex-release-exists/script.bash
+++ concourse/rex-release-exists/script.bash
@@ -1,1 +1,1 @@
-aws s3api get-object --bucket "$PROD_UNIFIED_S3_BUCKET" --key "rex/releases/$RELEASE_ID/rex/release.json" tmp 2> /dev/null
+aws s3api get-object --bucket "$PROD_UNIFIED_S3_BUCKET" --key "rex/releases/$RELEASE_ID/rex/release.json" /dev/null > /dev/null 2>&1
