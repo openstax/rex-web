c6876f3972ae13152309f066a1a908150a419519
tom <tom.g.woodward@gmail.com>
2020-07-24 16:22:14 -0400

    run saucelabs against release issues

--- concourse/execute-test-runs/script.bash
+++ concourse/execute-test-runs/script.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e -u
 
 base_dir=$(pwd)
@@ -6,29 +6,26 @@ failed=0
 
 cd rex-web
 
-for milestone_number_dir in "$base_dir"/unified-issues-milestones/*
+release_number=$(< release-issue/number.txt)
+env_name="release-$release_number"
+export BASE_URL="https://$env_name.sandbox.openstax.org"
+
+echo "checking for runs using $BASE_URL"
+
+for tr_run_dir in "$base_dir"/test-plans/*
 do
-  milestone_number=$(basename "$milestone_number_dir")
-  env_name=release-$milestone_number
-  export BASE_URL=https://$env_name.sandbox.openstax.org
+  tr_run_id=$(basename "$tr_run_dir")
+  base_url=$(< "$tr_run_dir/base_url.txt")
 
-  echo "checking for runs using $BASE_URL"
+  if [[ "$BASE_URL" == "$base_url" ]]; then
+    echo "found test plan $tr_run_id"
 
-  for tr_run_dir in "$base_dir"/test-plans/*
-  do
-    tr_run_id=$(basename "$tr_run_dir")
-    base_url=$(< "$tr_run_dir/base_url.txt")
-    
-    if [[ "$BASE_URL" == "$base_url" ]]; then
-      echo "found test plan $tr_run_id"
+    browser=$(< "$tr_run_dir/browser.txt")
+    export TR_RUN_ID=$tr_run_id
+    export BROWSER=$browser
 
-      browser=$(< "$tr_run_dir/browser.txt")
-      export TR_RUN_ID=$tr_run_id
-      export BROWSER=$browser
-
-      make test-rail-sauce || failed=1
-    fi
-  done
+    make test-rail-sauce || failed=1
+  fi
 done
 
 exit "$failed"
