8aaf7bdb7c00223f4a39379d812099f073df7e73
janKozie1 <jankkoziel@gmail.com>
2020-09-08 18:58:29 +0200

    update tests

--- src/app/content/highlights/hooks/updateHighlight.spec.ts
+++ src/app/content/highlights/hooks/updateHighlight.spec.ts
@@ -1,15 +1,27 @@
-import { UpdateHighlightRequest } from '@openstax/highlighter/dist/api';
+import { NewHighlight, UpdateHighlightRequest } from '@openstax/highlighter/dist/api';
+import Sentry from '../../../../helpers/Sentry';
 import createTestServices from '../../../../test/createTestServices';
 import createTestStore from '../../../../test/createTestStore';
-import { resetModules } from '../../../../test/utils';
 import { MiddlewareAPI, Store } from '../../../types';
-import { updateHighlight } from '../actions';
+import { createHighlight, updateHighlight } from '../actions';
+import { addToast } from '../../../notifications/actions';
+import { toastNotifications } from '../../../notifications/selectors';
 
-const updateMockHighlight = () => {
+jest.mock('../../../../helpers/Sentry');
+
+const mockHighlight = () => {
   const id = Math.random().toString(36).substring(7);
 
   return {
-    highlight: {color: 'red' as string, annotation: 'asdf'},
+    annotation: 'asdf',
+    color: 'red' as string,
+    id,
+  } as NewHighlight & {id: string};
+};
+
+const highlightUpdatePayload = (id: string, toChange: {color: string, annotation: string}) => {
+  return {
+    highlight: { color: toChange.color, annotation: toChange.annotation },
     id,
   } as UpdateHighlightRequest;
 };
@@ -18,10 +30,15 @@ describe('updateHighlight', () => {
   let store: Store;
   let helpers: ReturnType<typeof createTestServices> & MiddlewareAPI;
   let hook: ReturnType<typeof import ('./updateHighlight').hookBody>;
+  let highlight: ReturnType<typeof mockHighlight>;
+  let dispatch: jest.SpyInstance;
+
+  const meta = {locationFilterId: 'id', pageId: 'id'};
 
   beforeEach(() => {
-    resetModules();
     store = createTestStore();
+
+    dispatch = jest.spyOn(store, 'dispatch');
 
     helpers = {
       ...createTestServices(),
@@ -29,15 +46,60 @@ describe('updateHighlight', () => {
       getState: store.getState,
     };
 
+    highlight = mockHighlight();
+    store.dispatch(createHighlight(highlight, meta));
+
     hook = (require('./updateHighlight').hookBody)(helpers);
   });
 
   it('updates highlight', async() => {
-    const updateHighlightClient = jest.spyOn(helpers.highlightClient, 'updateHighlight');
-    const mock = updateMockHighlight();
+    const updateHighlightClient = jest.spyOn(helpers.highlightClient, 'updateHighlight')
+      .mockResolvedValue({} as any);
+    const updatePayload = highlightUpdatePayload(highlight.id, {color: 'red', annotation: 'new'});
 
-    await hook(updateHighlight(mock, {locationFilterId: 'id', pageId: 'id'}));
+    hook(updateHighlight(updatePayload, meta));
 
-    expect(updateHighlightClient).toHaveBeenCalledWith(mock);
+    await Promise.resolve();
+
+    expect(updateHighlightClient).toHaveBeenCalledWith(updatePayload);
+  });
+
+  it('doesn\'t call highlightClient when reverting updates', async() => {
+    const updateHighlightClient = jest.spyOn(helpers.highlightClient, 'updateHighlight')
+      .mockResolvedValue({} as any);
+
+    hook(updateHighlight(
+      highlightUpdatePayload(highlight.id, {color: 'red', annotation: 'new'}),
+      {...meta, failedToSave: true}
+    ));
+
+    await Promise.resolve();
+
+    expect(updateHighlightClient).not.toHaveBeenCalled();
+  });
+
+  it('reverts an update if it failed', async() => {
+    const error = {} as any;
+
+    const updateHighlightClient = jest.spyOn(helpers.highlightClient, 'updateHighlight')
+      .mockRejectedValue(error);
+    const updatePayload = highlightUpdatePayload(highlight.id, {color: 'red', annotation: 'new'});
+
+    hook(updateHighlight(updatePayload, meta));
+
+    await Promise.resolve();
+
+    expect(updateHighlightClient).toHaveBeenCalledWith(updatePayload);
+    expect(Sentry.captureException).toHaveBeenCalledWith(error);
+
+    expect(dispatch).toHaveBeenCalledWith(updateHighlight(
+      expect.objectContaining({highlight}),
+      expect.objectContaining({failedToSave: true}))
+    );
+
+    const hasErrorToast = toastNotifications(store.getState())
+      .some((notification) => notification.message === 'i18n:notification:toast:highlights:failure');
+
+    expect(hasErrorToast).toBe(true);
   });
 });
