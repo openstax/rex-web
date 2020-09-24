4564c89dd0a2d64ddf3d0e021e592e2eede137de
PiotrKozlowski <piotr@obrandowani.pl>
2020-09-24 16:13:26 +0200

    Clear the url if deleted highlight was in it as a scrollTarget

--- src/app/navigation/reducer.ts
+++ src/app/navigation/reducer.ts
@@ -2,9 +2,13 @@ import { Location } from 'history';
 import queryString from 'query-string';
 import { Reducer } from 'redux';
 import { getType } from 'typesafe-actions';
+import { deleteHighlight } from '../content/highlights/actions';
+import { isHighlightScrollTarget } from '../content/highlights/guards';
 import { AnyAction } from '../types';
+import { assertWindow } from '../utils';
 import * as actions from './actions';
 import { State } from './types';
+import { getScrollTargetFromQuery } from './utils';
 
 const addQuery = (location: Location) => ({
   ...location,
@@ -15,6 +19,15 @@ export default (location: Location): Reducer<State, AnyAction> => (state = addQu
   switch (action.type) {
     case getType(actions.locationChange):
       return addQuery(action.payload.location);
+    case getType(deleteHighlight): {
+      const scrollTarget = getScrollTargetFromQuery(state.query, state.hash);
+      if (scrollTarget && isHighlightScrollTarget(scrollTarget) && scrollTarget.id === action.payload) {
+        const window = assertWindow();
+        window.history.replaceState(null, '', window.location.origin + window.location.pathname);
+        return {...state, search: '', hash: '', query: {}};
+      }
+      return state;
+    }
     default:
       return state;
   }
