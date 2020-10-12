da2daa617eea818b99b647769bb5e1bc9c796649
PiotrKozlowski <piotr@obrandowani.pl>
2020-10-12 17:23:33 +0200

    Fix Safari issue

--- src/app/content/highlights/components/SummaryPopup/utils.ts
+++ src/app/content/highlights/components/SummaryPopup/utils.ts
@@ -1,5 +1,6 @@
 import { Highlight } from '@openstax/highlighter/dist/api';
 import queryString from 'querystring';
+import { assertWindow } from '../../../../utils';
 import { Book } from '../../../types';
 import { findArchiveTreeNodeById } from '../../../utils/archiveTreeUtils';
 import { getBookPageUrlAndParams } from '../../../utils/urlUtils';
@@ -8,5 +9,7 @@ export const createHighlightLink = ({ sourceId, id, anchor: elementId }: Highlig
   const page = book ? findArchiveTreeNodeById(book.tree, sourceId) : undefined;
   const data = { target: JSON.stringify({ id, type: 'highlight' }) };
   const target = queryString.stringify(data) + `#${elementId}`;
-  return `${page && book ? getBookPageUrlAndParams(book, page).url : ''}?${target}`;
+  // We need assertWindow().origin here because Safari
+  // does not want to open links in a new window if they does not have it
+  return `${assertWindow().origin}${page && book ? getBookPageUrlAndParams(book, page).url : ''}?${target}`;
 };
