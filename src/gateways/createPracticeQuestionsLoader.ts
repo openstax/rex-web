7979df6a8429a880ebaf7ae459f8229005fcc222
PiotrKozlowski <piotr@obrandowani.pl>
2020-10-26 16:14:35 +0100

    Move practice summary data to the other location

--- src/gateways/createPracticeQuestionsLoader.ts
+++ src/gateways/createPracticeQuestionsLoader.ts
@@ -1,15 +1,14 @@
 import { PracticeQuestionsSummary } from '../app/content/practiceQuestions/types';
-import mockPhysics from '../test/fixtures/practice/cce64fde-f448-43b8-ae88-27705cceb0da.json';
 
 export default () => {
   return {
-    getPracticeQuestionsBookSummary: (bookId: string): Promise<PracticeQuestionsSummary | undefined> => {
-      return new Promise((resolve) => {
-        if (bookId === 'cce64fde-f448-43b8-ae88-27705cceb0da') {
-          return resolve(mockPhysics as PracticeQuestionsSummary);
-        }
-        return resolve(undefined);
-      });
+    getPracticeQuestionsBookSummary: async(bookId: string): Promise<PracticeQuestionsSummary | undefined> => {
+      try {
+        const data = await import(`../../data/practice/summary/${bookId}.json`);
+        return data.default as PracticeQuestionsSummary;
+      } catch {
+        return undefined;
+      }
     },
   };
 };
