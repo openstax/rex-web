86ba28f9f968ed166b14682c713b2e2f7dd66f11
Piotr Kozłowski <piotr@obrandowani.pl>
2020-10-27 13:34:33 +0100

    Issue 1328 (#907)
    * Add Practice button
    
    * Remove print button
    
    * changes to practice button
    
    * fix some styles
    
    * update icon
    
    * use render() to create component in tests
    
    * Remove print button only if practice questions feature flag is enabled
    
    * Initial work
    
    * some tests
    
    * more tests
    
    * fixes
    
    * requested changes
    
    * omit unneccessary variables
    
    * cleaner way of finding section
    
    * Update summary to match test data
    
    * We don not need practice url
    
    * Move practice summary data to the other location
    
    * Remove practice data from fixtures
    
    * unused var
    
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>

--- src/app/content/practiceQuestions/types.ts
+++ src/app/content/practiceQuestions/types.ts
@@ -1,4 +1,11 @@
 export interface State {
   isEnabled: boolean;
+  summary: PracticeQuestionsSummary | null;
   open: boolean;
+}
+
+export interface PracticeQuestionsSummary {
+  countsPerSource: {
+    [key: string]: number;
+  };
 }
