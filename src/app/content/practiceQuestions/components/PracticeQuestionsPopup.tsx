64cfde2e9815745c8ec22ea1bffd8c2e27d91242
Piotr Kozłowski <piotr@obrandowani.pl>
2020-11-18 14:05:34 +0100

    Add warning prompt to the the PQ modal (#944)
    * Add warning prompt to the the PQ modal
    
    * Adjust MH and SG popups to use updated version of useOnEsc

--- src/app/content/practiceQuestions/components/PracticeQuestionsPopup.tsx
+++ src/app/content/practiceQuestions/components/PracticeQuestionsPopup.tsx
@@ -1,15 +1,16 @@
 import { HTMLElement } from '@openstax/types/lib.dom';
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { useAnalyticsEvent } from '../../../../helpers/analytics';
 import ScrollLock from '../../../components/ScrollLock';
 import { useOnEsc } from '../../../reactUtils';
 import theme from '../../../theme';
+import { assertWindow } from '../../../utils';
 import { bookTheme as bookThemeSelector } from '../../selectors';
 import { CloseIcon, CloseIconWrapper, Header, Modal, PopupWrapper } from '../../styles/PopupStyles';
 import { closePracticeQuestions } from '../actions';
-import { practiceQuestionsOpen } from '../selectors';
+import * as pqSelectors from '../selectors';
 import ShowPracticeQuestions from './ShowPracticeQuestions';
 
 // tslint:disable-next-line: variable-name
@@ -17,19 +18,21 @@ const PracticeQuestionsPopup = () => {
   const dispatch = useDispatch();
   const popUpRef = React.useRef<HTMLElement>(null);
   const trackOpenClosePQ = useAnalyticsEvent('openClosePracticeQuestions');
-  const isPracticeQuestionsOpen = useSelector(practiceQuestionsOpen);
+  const isPracticeQuestionsOpen = useSelector(pqSelectors.practiceQuestionsOpen);
+  const currentQuestionIndex = useSelector(pqSelectors.currentQuestionIndex);
   const bookTheme = useSelector(bookThemeSelector);
+  const intl = useIntl();
 
-  const closeAndTrack = () => {
+  const closeAndTrack = React.useCallback((method: string) => () => {
+    if (currentQuestionIndex !== null) {
+      const message = intl.formatMessage({ id: 'i18n:practice-questions:popup:warning-before-close' });
+      if (!assertWindow().confirm(message)) { return; }
+    }
     dispatch(closePracticeQuestions());
-    trackOpenClosePQ('esc');
-  };
+    trackOpenClosePQ(method);
+  }, [dispatch, currentQuestionIndex, trackOpenClosePQ, intl]);
 
-  const closePracticeQuestionsPopUp = () => {
-    dispatch(closePracticeQuestions());
-  };
-
-  useOnEsc(popUpRef, isPracticeQuestionsOpen, closeAndTrack);
+  useOnEsc(popUpRef, isPracticeQuestionsOpen, closeAndTrack('esc'));
 
   React.useEffect(() => {
     const popUp = popUpRef.current;
@@ -45,10 +48,7 @@ const PracticeQuestionsPopup = () => {
         overlay={true}
         mobileOnly={false}
         zIndex={theme.zIndex.highlightSummaryPopup}
-        onClick={() => {
-          closePracticeQuestionsPopUp();
-          trackOpenClosePQ('overlay');
-        }}
+        onClick={closeAndTrack('overlay')}
       />
       <Modal
         ref={popUpRef}
@@ -64,10 +64,7 @@ const PracticeQuestionsPopup = () => {
               <CloseIconWrapper
                 data-testid='close-practice-questions-popup'
                 aria-label={msg}
-                onClick={() => {
-                  closePracticeQuestionsPopUp();
-                  trackOpenClosePQ('button');
-                }}
+                onClick={closeAndTrack('button')}
               >
                 <CloseIcon colorSchema={bookTheme} />
               </CloseIconWrapper>
