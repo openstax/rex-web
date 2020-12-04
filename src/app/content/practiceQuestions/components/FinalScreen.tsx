74f9809ff89bbaac73194f3076da880b5cb20bef
jarosik10 <bartosz.jaros96@gmail.com>
2020-12-04 16:52:01 +0100

    Add FinalScreen message to FinalScreen component

--- src/app/content/practiceQuestions/components/FinalScreen.tsx
+++ src/app/content/practiceQuestions/components/FinalScreen.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components/macro';
 
+// tslint:disable-next-line: variable-name
+const StyledText = styled.span`
+  margin-bottom: 3rem;
+`;
 const FinalScreen = () => {
+    return (
+        <StyledText>
+            <FormattedMessage id='i18n:practice-questions:popup:final'>
+                {(msg: string) => msg}
+            </FormattedMessage>
+        </StyledText>
+    );
 
 }
 
