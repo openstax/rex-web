103a993ab96d0816166b2c544fa21d60922ff8c1
jarosik10 <bartosz.jaros96@gmail.com>
2020-12-09 16:39:57 +0100

    Remove unused import

--- src/app/content/practiceQuestions/selectors.ts
+++ src/app/content/practiceQuestions/selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 import * as parentSelectors from '../selectors';
-import { getPracticeQuestionsLocationFilters, pageHasPracticeQuestions } from './utils';
+import { getPracticeQuestionsLocationFilters } from './utils';
 
 export const localState = createSelector(
   parentSelectors.localState,
