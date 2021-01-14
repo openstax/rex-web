f63ecd39ba961a67d841364443acb74dc31dac1e
Karina Mendez <karinamendez@Karinas-MacBook-Pro.local>
2021-01-14 12:14:29 -0600

    code coverage

--- src/app/content/utils.spec.ts
+++ src/app/content/utils.spec.ts
@@ -63,6 +63,21 @@ describe('getContentPageReferences', () => {
       },
     ]);
   });
+
+  it('picks up a rap link', () => {
+    expect(
+      getContentPageReferences(`
+      asdfa <a href="./13ac107a-f15f-49d2-97e8-60ab2e3b519c@29.7:99d38770-49c7-49d3-b567-88f393ffb4fe.xhtml"></a> sdf
+    `)
+    ).toEqual([
+      {
+        bookId: '13ac107a-f15f-49d2-97e8-60ab2e3b519c',
+        bookVersion: '29.7',
+        match: './13ac107a-f15f-49d2-97e8-60ab2e3b519c@29.7:99d38770-49c7-49d3-b567-88f393ffb4fe.xhtml',
+        pageUid: '99d38770-49c7-49d3-b567-88f393ffb4fe',
+      },
+    ]);
+  });
 });
 
 describe('getUrlParamForPageId', () => {
