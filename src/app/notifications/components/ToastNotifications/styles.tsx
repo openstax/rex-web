5eac7bc909fd8551498abf97f2a218ba040018b8
PiotrKozlowski <piotr@obrandowani.pl>
2021-01-27 17:07:09 +0100

    fix styles and removing toasts

--- src/app/notifications/components/ToastNotifications/styles.tsx
+++ src/app/notifications/components/ToastNotifications/styles.tsx
@@ -35,7 +35,7 @@ export const ToastsContainer = styled.div`
 const setTransitionStyles = ({positionProps, isFadingIn}: BannerProps) => css`
   transition: transform 0.6s;
   z-index: ${positionProps.totalToastCount - positionProps.index};
-  transform: translateY(${isFadingIn ? (positionProps.index) * 100 : -100}%);
+  transform: translateY(${isFadingIn ? 0 : -100}%);
 `;
 
 interface BannerProps {
@@ -49,7 +49,6 @@ export const BannerBodyWrapper = styled.div`
   width: 100%;
   margin: 0;
   overflow: visible;
-  position: absolute;
   ${setTransitionStyles};
   ${(props: BannerProps) => props.isFadingOut && css`
     animation: ${fadeOut} ${fadeOutDuration / 1000}s forwards;
@@ -69,7 +68,6 @@ export const BannerBody = styled.div`
   border: 1px solid ${errorBorderColor};
 
   ${Header} {
-    width: 90%;
     background: inherit;
     color: ${errorTextColor};
     font-weight: normal;
--- src/app/notifications/utils.ts
+++ src/app/notifications/utils.ts
@@ -31,3 +31,9 @@ export const groupToasts = (toasts: ToastNotification[]) => toasts.reduce((group
   }
   return grouped;
 }, {} as {[key in ToastNotification['destination']]?: ToastNotification[]});
+
+export const compareToasts = (toast: ToastNotification, toast2: ToastNotification) => {
+  return toast.messageKey === toast2.messageKey
+    && toast.destination === toast2.destination
+    && toast.errorId === toast2.errorId;
+};
