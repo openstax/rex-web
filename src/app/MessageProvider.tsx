a71b91a6198f598eafd769c476c85ceb29a28f59
tom <tom.g.woodward@gmail.com>
2021-01-27 10:21:24 -0500

    upgrade intl-pluralrules polyfill

--- src/app/MessageProvider.tsx
+++ src/app/MessageProvider.tsx
@@ -1,11 +1,27 @@
+import { shouldPolyfill } from '@formatjs/intl-pluralrules/should-polyfill';
 import React from 'react';
 import { IntlProvider } from 'react-intl';
 import enMessages from './messages/en';
 
-if (!Intl.PluralRules) {
-  require('@formatjs/intl-pluralrules/polyfill'); // tslint:disable-line:no-var-requires
-  require('@formatjs/intl-pluralrules/dist/locale-data/en'); // tslint:disable-line:no-var-requires
+// https://formatjs.io/docs/polyfills/intl-pluralrules/#dynamic-import--capability-detection
+async function polyfill(locale: 'en') {
+  if (shouldPolyfill()) {
+    await import('@formatjs/intl-pluralrules/polyfill');
+  }
+
+  // boolean added by the polyfill
+  if ((Intl.PluralRules as (typeof Intl.PluralRules & {polyfilled?: boolean})).polyfilled) {
+    switch (locale) {
+      case 'en':
+        await import('@formatjs/intl-pluralrules/locale-data/en');
+        break;
+      default:
+        throw new Error('unsupported locale in messageProvider');
+    }
+  }
 }
+
+polyfill('en');
 
 // tslint:disable-next-line:variable-name
 const MessageProvider: React.SFC<{onError?: () => void}> = (props) =>
