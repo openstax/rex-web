4ad99ad615e20b50ed319aa83142066712468753
PiotrKozlowski <piotr@obrandowani.pl>
2021-02-01 17:00:44 +0100

    requested change

--- script/prerender/createRedirects.ts
+++ script/prerender/createRedirects.ts
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import { Redirects } from '../../data/redirects/types';
 import { content } from '../../src/app/content/routes';
 import { makeUnifiedBookLoader } from '../../src/app/content/utils';
 import { findArchiveTreeNodeById } from '../../src/app/content/utils/archiveTreeUtils';
 import { AppServices } from '../../src/app/types';
-import { Redirects } from '../../src/redirects/types';
 import { writeAssetFile } from './fileUtils';
 
 const redirectsPath = path.resolve(__dirname, '../../src/redirects/');
@@ -21,22 +21,20 @@ const createRedirects = async(archiveLoader: AppServices['archiveLoader'], osWeb
     const bookId = bookName.replace('.json', '');
     const { tree, slug: bookSlug } = await bookLoader(bookId);
 
-    redirects.push(
-      ...bookRedirects.map(({ pageId, pathname }) => {
-        const page = findArchiveTreeNodeById(tree, pageId);
+    for (const { pageId, pathname } of bookRedirects) {
+      const page = findArchiveTreeNodeById(tree, pageId);
 
-        if (!page) {
-          // tslint:disable-next-line: no-console
-          console.log(`Couldn't find page ${pageId} in book ${bookId}`);
-          return null;
-        }
+      if (!page) {
+        // tslint:disable-next-line: no-console
+        console.log(`Couldn't find page ${pageId} in book ${bookId}`);
+        continue;
+      }
 
-        return {
-          from: pathname,
-          to: content.getUrl({ book: { slug: bookSlug }, page: { slug: page.slug } }),
-        };
-      }).filter((data) => Boolean(data)) as Array<{ from: string, to: string }>
-    );
+      redirects.push({
+        from: pathname,
+        to: content.getUrl({ book: { slug: bookSlug }, page: { slug: page.slug } }),
+      });
+    }
   }
 
   writeAssetFile('/rex/redirects.json', JSON.stringify(redirects, undefined, 2));
