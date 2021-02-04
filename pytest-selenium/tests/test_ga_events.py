1c6caa0000c0d436074953492d8cdd3b52a0d22b
Greg Fitch <greg.fitch@rice.edu>
2021-02-04 11:08:20 -0600

    add release 50 tests

--- pytest-selenium/tests/test_ga_events.py
+++ pytest-selenium/tests/test_ga_events.py
@@ -3,15 +3,99 @@
 import random
 from time import sleep
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
 from pages.accounts import Signup
 from pages.content import Content
 from tests import markers
 from utils.utility import Highlight, Utilities
 
 
+@markers.test_case("C597671")
+@markers.parametrize(
+    "book_slug, page_slug",
+    [("physics", "1-introduction")])
+def test_select_text_ga_event(selenium, base_url, book_slug, page_slug):
+    """The page submits the correct GA event when content is selected."""
+    # SETUP:
+    event_action = "show create"
+    event_category = "REX highlighting - show create"
+    event_label = f"/books/{book_slug}/pages/{page_slug}"
+
+    # GIVEN: a logged in user viewing a book page
+    book = user_setup(selenium, base_url, book_slug, page_slug)
+
+    # WHEN:  they select some text
+    while not book.content.highlight_boxes:
+        paragraph = random.choice(book.content.paragraphs)
+        book.content.highlight(
+            target=paragraph,
+            offset=Highlight.ENTIRE,
+            color=None,
+            close_box=False
+        )
+
+    # THEN:  the create highlight box is opened
+    # AND:   the correct Google Analytics event is queued
+    #        { eventAction: "show create",
+    #          eventCategory: "REX highlighting - show create",
+    #          eventLabel: "/books/{book_slug}/pages/{page_slug}" }
+    assert(book.content.highlight_boxes), 'No highlight box found'
+
+    last_event = Utilities.get_analytics_queue(selenium, -1)
+    assert(last_event['eventAction'] == event_action)
+    assert(last_event['eventCategory'] == event_category)
+    assert(last_event['eventLabel'] == event_label)
+
+
+@markers.test_case("C597672")
+@markers.parametrize(
+    "book_slug, page_slug",
+    [("physics", "1-introduction")])
+def test_inline_highlighting_login_nudge_ga_event(
+        selenium, base_url, book_slug, page_slug):
+    """The page submits the correct GA event when the log in nudge is shown."""
+    # SETUP:
+    event_action = "show login"
+    event_category = "REX highlighting - show login"
+    event_label = f"/books/{book_slug}/pages/{page_slug}"
+
+    # GIVEN: a non-logged in user viewing a book page
+    book = Content(selenium, base_url,
+                   book_slug=book_slug, page_slug=page_slug).open()
+    while book.notification_present:
+        book.notification.got_it()
+
+    # WHEN:  they select some text
+    while not book.content.highlight_boxes:
+        paragraph = random.choice(book.content.paragraphs)
+        book.content.highlight(
+            target=paragraph,
+            offset=Highlight.ENTIRE,
+            color=None,
+            close_box=False
+        )
+
+    # THEN:  the inline log in nudge is opened
+    # AND:   the correct Google Analytics event is queued
+    #        { eventAction: "show login",
+    #          eventCategory: "REX highlighting - show login",
+    #          eventLabel: "/books/{book_slug}/pages/{page_slug}" }
+    assert(book.content.highlight_box.login_overlay_present), "Log in not seen"
+
+    last_event = Utilities.get_analytics_queue(selenium, -1)
+    assert(last_event['eventAction'] == event_action)
+    assert(last_event['eventCategory'] == event_category)
+    assert(last_event['eventLabel'] == event_label)
+
+
 @markers.test_case("C615600")
 @markers.desktop_only
-@markers.parametrize("page_slug", ["preface"])
+@markers.parametrize(
+    "book_slug, page_slug",
+    [("physics", "1-introduction")])
 def test_go_to_highlight_ga_event(selenium, base_url, book_slug, page_slug):
     """The page submits the correct GA event for the 'Go to highlight' button.
 
@@ -19,6 +103,7 @@ def test_go_to_highlight_ga_event(selenium, base_url, book_slug, page_slug):
 
     """
     # SETUP:
+    event_category = "REX Link (MH gotohighlight)"
     event_label = f"/books/{book_slug}/pages/{page_slug}"
     event_action = f"{event_label}?target="
 
@@ -26,19 +111,11 @@ def test_go_to_highlight_ga_event(selenium, base_url, book_slug, page_slug):
     # AND:   one or more highlights are present on the page
     # AND:   the 'My Highlights and Notes' window is open
     # AND:   the context menu is open for a highlight
-    book = Content(selenium, base_url,
-                   book_slug=book_slug, page_slug=page_slug).open()
-    while book.notification_present:
-        book.notification.got_it()
-    book.navbar.click_login()
-    Signup(selenium).register()
-    book.wait_for_page_to_load()
-    while book.notification_present:
-        book.notification.got_it()
-    book.content.show_solutions()
+    book = user_setup(selenium, base_url, book_slug, page_slug)
 
-    paragraph = random.choice(book.content.paragraphs)
-    book.content.highlight(paragraph, Highlight.ENTIRE)
+    while not book.content.highlight_count:
+        paragraph = random.choice(book.content.paragraphs)
+        book.content.highlight(paragraph, Highlight.ENTIRE)
 
     my_highlights = book.toolbar.my_highlights()
 
@@ -55,8 +132,23 @@ def test_go_to_highlight_ga_event(selenium, base_url, book_slug, page_slug):
     #          eventLabel: "/books/{book_slug}/pages/{page_slug}" }
     last_event = Utilities.get_analytics_queue(selenium, -1)
     assert(event_action in last_event['eventAction'])
-    assert(last_event['eventCategory'] == "REX Link (MH gotohighlight)")
+    assert(last_event['eventCategory'] == event_category)
     assert(last_event['eventLabel'] == event_label)
+
+
+def user_setup(driver, base_url, book_slug, page_slug):
+    """Setup a new user for use in highlighting event tests."""
+    book = Content(driver, base_url,
+                   book_slug=book_slug, page_slug=page_slug).open()
+    while book.notification_present:
+        book.notification.got_it()
+    book.navbar.click_login()
+    Signup(driver).register()
+    book.wait_for_page_to_load()
+    while book.notification_present:
+        book.notification.got_it()
+    book.content.show_solutions()
+    return book
 
 
 def switch_tab(driver):
