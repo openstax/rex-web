8c7ff52a9444762fd2afd60a40d7f6ea97e39264
PiotrKozlowski <piotr@obrandowani.pl>
2021-02-05 16:52:01 +0100

    Use bookId from the json object instead of the file

--- script/prerender/createRedirects.ts
+++ script/prerender/createRedirects.ts
@@ -19,18 +19,18 @@ const createRedirects = async(archiveLoader: AppServices['archiveLoader'], osWeb
 
   for (const fileName of books) {
     const bookRedirects: Redirects = await import(`${redirectsPath}/${fileName}`);
-    const bookId = fileName.replace('.json', '');
-    const configForBook: { defaultVersion: string } | undefined = config[bookId];
+    const bookIdFromFilename = fileName.replace('.json', '');
+    const configForBook: { defaultVersion: string } | undefined = config[bookIdFromFilename];
 
     if (!configForBook) {
       // tslint:disable-next-line: no-console
-      console.log(`Couldn't find version for book: ${bookId}`);
+      console.log(`Couldn't find version for book: ${bookIdFromFilename}`);
       continue;
     }
 
-    const { tree, slug: bookSlug } = await bookLoader(bookId, configForBook.defaultVersion);
+    const { tree, slug: bookSlug } = await bookLoader(bookIdFromFilename, configForBook.defaultVersion);
 
-    for (const { pageId, pathname } of bookRedirects) {
+    for (const { bookId, pageId, pathname } of bookRedirects) {
       const page = findArchiveTreeNodeById(tree, pageId);
 
       if (!page) {
