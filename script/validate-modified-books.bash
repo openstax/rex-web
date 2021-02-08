287138f5484a54687a7563e71e23658fbacf69b9
jarosik10 <bartosz.jaros96@gmail.com>
2021-02-08 16:58:20 +0100

    Add catching exit status

--- script/validate-modified-books.bash
+++ script/validate-modified-books.bash
@@ -10,10 +10,16 @@ for row in $(jq -c '.[]' <<< "$modified_books"); do
     --bookVersion="$book_version" \
     --rootUrl="$BASE_URL" \
     --queryString="validateLinks"
+  if [ $? != 0 ]; then                   
+    echo "Error $? in domVisitor script for book $book_id" && exit 1
+  fi
 
   node script/entry.js urlChecker \
     --rootUrl="https://openstax.org" \
     --bookId="$book_id" \
     --bookVersion="$book_version" \
     --useUnversionedUrls
+  if [ $? != 0 ]; then                   
+   echo "Error $? in urlChecker script for book $book_id" && exit 1
+  fi
 done
