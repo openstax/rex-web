ce8657517fb7eca798f1efef18be15e1119df98e
jarosik10 <bartosz.jaros96@gmail.com>
2021-02-11 17:35:06 +0100

    Change script exit code check

--- script/validate-modified-books.bash
+++ script/validate-modified-books.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 modified_books=$(./script/get-modified-books.bash)
+code=0
 
 for row in $(jq -c '.[]' <<< "$modified_books"); do
   book_id=$(jq -r '.book_id' <<< "$row")
@@ -9,17 +10,16 @@ for row in $(jq -c '.[]' <<< "$modified_books"); do
     --bookId="$book_id" \
     --bookVersion="$book_version" \
     --rootUrl="$BASE_URL" \
-    --queryString="validateLinks"
-  if [ $? != 0 ]; then                   
-    echo "Error $? in domVisitor script for book $book_id" && exit 1
-  fi
+    --queryString="validateLinks" \
+    || code=1
 
   node script/entry.js urlChecker \
     --rootUrl="https://openstax.org" \
     --bookId="$book_id" \
     --bookVersion="$book_version" \
-    --useUnversionedUrls
-  if [ $? != 0 ]; then                   
-   echo "Error $? in urlChecker script for book $book_id" && exit 1
-  fi
+    --useUnversionedUrls \
+    || code=1
+
 done
+
+exit $code
