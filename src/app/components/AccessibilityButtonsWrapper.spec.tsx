88375cb05e74a00955cc1163428c016572b1cc41
Piotr Kozłowski <piotr@obrandowani.pl>
2021-03-03 22:34:07 +0100

    Issue 1496 (#1109)
    * Add hidden link to accessibility help page
    
    * Update snaps
    
    * Provide correct link
    
    * Update link text
    
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>

--- src/app/components/AccessibilityButtonsWrapper.spec.tsx
+++ src/app/components/AccessibilityButtonsWrapper.spec.tsx
@@ -3,32 +3,33 @@ import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
 import { expectError, renderToDom } from '../../test/reactutils';
 import MessageProvider from '../MessageProvider';
+import AccessibilityButtonsWrapper from './AccessibilityButtonsWrapper';
 import MainContent from './MainContent';
-import SkipToContentWrapper from './SkipToContentWrapper';
 
-describe('SkipToContentWrapper', () => {
+describe('AccessibilityButtonsWrapper', () => {
 
     it('errors when no main content is provided', () => {
         const {node} = renderToDom(<MessageProvider>
-            <SkipToContentWrapper/>
+            <AccessibilityButtonsWrapper/>
         </MessageProvider>);
         const breaks = () => ReactTestUtils.Simulate.click(node);
-        expectError('BUG: Expected mainComponent to be defined. Does SkipToContentWrapper contain a MainContent?',
+        expectError(
+            'BUG: Expected mainComponent to be defined. Does AccessibilityButtonsWrapper contain a MainContent?',
             breaks);
     });
 
-    it('fails when main is not wrapped in a SkipToContentWrapper', () => {
+    it('fails when main is not wrapped in a AccessibilityButtonsWrapper', () => {
         const breaks = () => renderToDom(<MessageProvider>
             <MainContent/>
         </MessageProvider>);
-        expectError('BUG: MainContent must be inside SkipToContentWrapper', breaks);
+        expectError('BUG: MainContent must be inside AccessibilityButtonsWrapper', breaks);
     });
 
     it('succeeds when main content is provided', () => {
         const {node} = renderToDom(<MessageProvider>
-            <SkipToContentWrapper>
+            <AccessibilityButtonsWrapper>
                 <MainContent/>
-            </SkipToContentWrapper>
+            </AccessibilityButtonsWrapper>
         </MessageProvider>);
 
         const works = () => ReactTestUtils.Simulate.click(node);
@@ -37,12 +38,12 @@ describe('SkipToContentWrapper', () => {
 
     it('scrolls and moves focus to mainContent when clicked (a11y)', () => {
         const {node, tree} = renderToDom(<MessageProvider>
-            <SkipToContentWrapper>
+            <AccessibilityButtonsWrapper>
                 <MainContent/>
-            </SkipToContentWrapper>
+            </AccessibilityButtonsWrapper>
         </MessageProvider>);
 
-        const wrapper = ReactTestUtils.findRenderedComponentWithType(tree, SkipToContentWrapper);
+        const wrapper = ReactTestUtils.findRenderedComponentWithType(tree, AccessibilityButtonsWrapper);
 
         if (!window) {
             expect(window).toBeTruthy();
