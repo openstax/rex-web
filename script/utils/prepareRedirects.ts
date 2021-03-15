f95969a65b68b9bfee318a5204c439d78c67916d
PiotrKozlowski <piotr@obrandowani.pl>
2021-03-15 16:47:08 +0100

    remove whole for loop from preprareRedirects

--- script/utils/prepareRedirects.ts
+++ script/utils/prepareRedirects.ts
@@ -1,47 +1,16 @@
-import { RedirectsData } from '../../data/redirects/types';
 import { makeUnifiedBookLoader } from '../../src/app/content/utils';
-import { findArchiveTreeNodeById } from '../../src/app/content/utils/archiveTreeUtils';
 import { AppServices } from '../../src/app/types';
-import config from '../../src/config.books';
 
 const prepareRedirects = async(
   archiveLoader: AppServices['archiveLoader'],
   osWebLoader: AppServices['osWebLoader']
 ) => {
   const bookLoader = makeUnifiedBookLoader(archiveLoader, osWebLoader);
-
+  console.log('bookLoader', bookLoader)
   const books: string[] = [];
+  console.log('books', books)
 
   const redirects: Array<{ from: string, to: string }> = [];
-
-  for (const fileName of books) {
-    console.log('fileName', fileName)
-    const bookRedirects: RedirectsData = [];
-
-    for (const { bookId, pageId, pathname } of bookRedirects) {
-      const configForBook: { defaultVersion: string } | undefined = config[bookId];
-
-      if (!configForBook) {
-        // tslint:disable-next-line: no-console
-        console.log(`Couldn't find version for book: ${bookId}`);
-        continue;
-      }
-
-      const { tree, slug: bookSlug } = await bookLoader(bookId, configForBook.defaultVersion);
-      const page = findArchiveTreeNodeById(tree, pageId);
-
-      if (!page) {
-        // tslint:disable-next-line: no-console
-        console.log(`Couldn't find page ${pageId} in book ${bookId}`);
-        continue;
-      }
-
-      redirects.push({
-        from: pathname,
-        to: bookSlug, // content.getUrl({ book: { slug: bookSlug }, page: { slug: page.slug } }),
-      });
-    }
-  }
 
   return redirects;
 };
