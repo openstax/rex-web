5ec98082bb79dc3b78a9a9cc1accd5c9b0a196d7
piotr@obrandowani.pl <piotr@obrandowani.pl>
2021-04-02 15:49:24 +0200

    this should work

--- concourse/update-content/script.bash
+++ concourse/update-content/script.bash
@@ -9,78 +9,47 @@ if [ "$GITHUB_USERNAME" != "" ] && [ "$GITHUB_PASSWORD" != "" ]; then
 fi
 
 # this is here so the creds don't get pasted to the output
-# set -ex
+set -ex
 
-# yarn
+yarn
 
 approved_books_default_branch=$(curl -s https://api.github.com/repos/openstax/content-manager-approved-books | jq -r .default_branch)
 rex_default_branch=$(curl -s https://api.github.com/repos/openstax/rex-web | jq -r .default_branch)
 
 data=$(curl -sL "https://github.com/openstax/content-manager-approved-books/raw/$approved_books_default_branch/approved-book-list.json")
-approved_books=$(echo $data | jq '.approved_books | map(select(.tutor_only == false)) | map(select(.server == "cnx.org"))')
-approved_versions=$(echo $data | jq '.approved_versions')
 
-repo_books=$(echo $approved_books | jq '[.[] | select(.repo)]')
-repo_versions=$(echo $approved_versions | jq '[.[] | select(.repo)]')
-collection_books=$(echo $approved_books | jq '[.[] | select(.collection_id)]')
-collection_versions=$(echo $approved_versions | jq '[.[] | select(.collection_id)]')
+# script will return a JSON object with book ids and new versions only for books that doesn't mach current config
+book_ids_and_versions=$(node script/entry.js transform-approved-books-data --data "$data")
+book_ids=$(echo "$book_ids_and_versions" | jq -r 'keys | .[]')
 
-collection_ids=$(jq -r 'map(.collection_id) | unique | .[]' <<< "$collection_books")
-repos=$(jq -r 'map(.repo) | unique | .[]' <<< "$repo_books")
+git remote set-branches origin 'update-content-*'
+git remote set-branches origin --add "$rex_default_branch"
 
-# git remote set-branches origin 'update-content-*'
-# git remote set-branches origin --add "$rex_default_branch"
+for book_id in $book_ids; do
+  new_version=$(echo "$book_ids_and_versions" | jq -r --arg uuid "$book_id" 'to_entries | map(select(.key == $uuid))[0].value')
 
-# for repo in $repos; do
+  branch="update-content-$book_id"
+  git fetch
+  git checkout "$rex_default_branch"
+  git checkout src/config.books.json
+  (git checkout "$branch" && git merge "origin/$rex_default_branch" --no-edit -X theirs) || git checkout -b "$branch"
 
-# done
+  node script/entry.js update-content-versions-and-check-for-archived-slugs --bookId "$book_id" --versionNumber "$new_version"
 
-for collection_id in $collection_ids; do
-  book_ids=$(echo $approved_books | jq --arg collection_id "$collection_id" '[.[] | select(.collection_id == $collection_id)][].books | [.[] | .uuid]')
+  if [[ -z $(git status --porcelain) ]]; then
+    continue
+  fi
 
-  echo $book_ids
+  git add src/config.books.json
+  git commit -m "update content" || true
+  git push --set-upstream origin "$branch"
 
-  for book_id in book_ids; do
-    # branch="update-content-$book_id"
-    # git fetch
-    # git checkout "$rex_default_branch"
-    # git checkout src/config.books.json
-    # (git checkout "$branch" && git merge "origin/$rex_default_branch" --no-edit -X theirs) || git checkout -b "$branch"
-
-    # there may be multiple approved versions for supporting different products, historical support, etc
-    available_versions=$(jq --arg collection_id "$collection_id" 'map(select(.collection_id == $collection_id).content_version) | .[]' <<< "$collection_versions")
-    echo "available_versions" $available_versions
-    echo $available_versions >> temp
-
-    # in general we want the most recent one, in the future we'll have to filter this by which ones
-    # are available for the recipe code version REX supports
-    desired_version=$(echo $available_versions | sort --version-sort -r | head -n 1)
-    echo "desired_version sorted" $desired_version
-
-    # approved book format has a "1." on the front of every version
-    desired_version=${desired_version:2}
-
-    echo "desired_version" $desired_version
-
-    exit
-
-  #   node script/entry.js update-content-versions-and-check-for-archived-slugs --bookId "$book_id" --versionNumber "$desired_version"
-
-  #   if [[ -z $(git status --porcelain) ]]; then
-  #     continue
-  #   fi
-
-  #   git add src/config.books.json
-  #   git commit -m "update content" || true
-  #   git push --set-upstream origin "$branch"
-
-  #   book_title=$(node script/entry.js book-info "$book_id" --field title)
-  #   curl -s -X POST -H "Authorization: token $GITHUB_ACCESS_TOKEN" "https://api.github.com/repos/openstax/rex-web/pulls" --data-binary @- << JSON
-  #     {
-  #       "title": "$book_title updates",
-  #       "head": "$branch",
-  #       "base": "$rex_default_branch"
-  #     }
-  # JSON
-  done
+  book_title=$(node script/entry.js book-info "$book_id" --field title)
+  curl -s -X POST -H "Authorization: token $GITHUB_ACCESS_TOKEN" "https://api.github.com/repos/openstax/rex-web/pulls" --data-binary @- << JSON
+    {
+      "title": "$book_title updates",
+      "head": "$branch",
+      "base": "$rex_default_branch"
+    }
+JSON
 done
---
+++ script/transform-approved-books-data.ts
@@ -0,0 +1,160 @@
+import { argv } from 'yargs';
+import configuredBooks from '../src/config.books';
+
+interface BookData {
+  uuid: string;
+  slug: string;
+}
+
+const isBookData = (something: any): something is BookData => {
+  return typeof something.uuid === 'string' && typeof something.slug === 'string';
+};
+
+interface ApprovedBooksData {
+  style: string;
+  tutor_only: boolean;
+  books: BookData[];
+}
+
+const isApprovedBooksData = (something: any): something is ApprovedBooksData => {
+  return typeof something.style === 'string'
+    && typeof something.tutor_only === 'boolean'
+    && Array.isArray(something.books)
+    && something.books.every(isBookData);
+};
+
+interface ApprovedCollection extends ApprovedBooksData {
+  collection_id: string;
+  server: string;
+}
+
+const isApprovedCollection = (something: any): something is ApprovedCollection => {
+  return typeof something.collection_id === 'string'
+    && typeof something.server === 'string'
+    && isApprovedBooksData(something);
+};
+
+interface ApprovedRepo extends ApprovedBooksData {
+  repo: string;
+}
+
+const isApprovedRepo = (something: any): something is ApprovedRepo => {
+  return typeof something.repo === 'string' && isApprovedBooksData(something);
+};
+
+interface ApprovedVersion {
+  content_version: string;
+  min_code_version: string;
+}
+
+const isApprovedVersion = (something: any): something is ApprovedVersion => {
+  return typeof something.content_version === 'string' && typeof something.min_code_version === 'string';
+};
+
+interface ApprovedVersionCollection extends ApprovedVersion {
+  collection_id: string;
+}
+
+const isApprovedVersionCollection = (something: any): something is ApprovedVersionCollection => {
+  return typeof something.collection_id === 'string' && isApprovedVersion(something);
+};
+
+interface ApprovedVersionRepo extends ApprovedVersion {
+  repo: string;
+}
+
+const isApprovedVersionRepo = (something: any): something is ApprovedVersionRepo => {
+  return typeof something.repo === 'string' && isApprovedVersion(something);
+};
+
+interface ApprovedBooksAndVersions {
+  approved_books: Array<ApprovedCollection | ApprovedRepo>;
+  approved_versions: Array<ApprovedVersionCollection | ApprovedVersionRepo>;
+}
+
+const isApprovedBooksAndVersions = (something: any): something is ApprovedBooksAndVersions => {
+  return Array.isArray(something.approved_books)
+    && something.approved_books.every(
+      (element: any) => isApprovedCollection(element) || isApprovedRepo(element))
+    && something.approved_versions.every(
+      (element: any) => isApprovedVersionCollection(element) || isApprovedVersionRepo(element));
+};
+
+const matchRepoVersion = (repoData: ApprovedRepo) =>
+  (versionData: ApprovedVersionCollection | ApprovedVersionRepo): versionData is ApprovedVersionRepo => {
+  return isApprovedVersionRepo(versionData) && versionData.repo === repoData.repo;
+};
+
+const matchCollectionVersion = (collectionData: ApprovedCollection) =>
+  (versionData: ApprovedVersionCollection | ApprovedVersionRepo): versionData is ApprovedVersionCollection => {
+  return isApprovedVersionCollection(versionData) && versionData.collection_id === collectionData.collection_id;
+};
+
+const getDesiredVersion = (
+  approvedVersions: Array<ApprovedVersionCollection | ApprovedVersionRepo>,
+  colOrRepo: ApprovedRepo | ApprovedCollection
+): string | undefined => {
+  const isCollection = isApprovedCollection(colOrRepo);
+  const versions = approvedVersions
+    // only versions for current repo
+    .filter(isApprovedRepo(colOrRepo) ? matchRepoVersion(colOrRepo) : matchCollectionVersion(colOrRepo))
+    .map((versionData) => {
+      // books from collections have versions starting with 1.
+      return isCollection ? versionData.content_version.slice(2) : versionData.content_version;
+    })
+    // sort and revert so results are descending
+    .sort()
+    .reverse();
+
+  // in general we want the most recent one, in the future we'll have to filter this by which ones
+  // are available for the recipe code version REX supports
+  return versions[0];
+};
+
+const { data } = argv as { data?: string };
+
+const transformData = () => {
+  if (!data) {
+    throw new Error('data argument is missing');
+  }
+
+  let parsed: ApprovedBooksAndVersions;
+  try {
+    parsed = JSON.parse(data);
+    if (!isApprovedBooksAndVersions(parsed)) {
+      throw new Error('Data is valid JSON, but has wrong structure. See ApprovedBooksAndVersions interface.');
+    }
+  } catch (e) {
+    throw new Error(`Error while parsing data. Details: ${e}`);
+  }
+
+  const results: { [key: string]: string } = {};
+
+  const { approved_books, approved_versions } = parsed;
+
+  for (const collectionOrRepo of approved_books) {
+    const books = collectionOrRepo.books;
+    const desiredVersion = getDesiredVersion(approved_versions, collectionOrRepo);
+
+    if (!desiredVersion) {
+      throw new Error(`Couldn't find version for ${
+        isApprovedRepo(collectionOrRepo)
+          ? `repo ${collectionOrRepo.repo}`
+          : `collection ${collectionOrRepo.collection_id}`
+      }`);
+    }
+
+    for (const book of books) {
+      if (!configuredBooks[book.uuid] || configuredBooks[book.uuid].defaultVersion !== desiredVersion) {
+        results[book.uuid] = desiredVersion;
+      }
+    }
+  }
+
+  // this script is used in bash script so we write results to the terminal so we can assign them to a variable
+  // tslint:disable-next-line: no-console
+  console.log(JSON.stringify(results));
+  return results;
+};
+
+transformData();
