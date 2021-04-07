be988fe0e9f88a7a430ed48625857f5a623756b3
jarosik10 <bartosz.jaros96@gmail.com>
2021-04-07 16:54:32 +0200

    Fix lint

--- src/gateways/createBookConfigLoader.ts
+++ src/gateways/createBookConfigLoader.ts
@@ -9,24 +9,24 @@ interface BookConfig {
 }
 
 interface ReleaseJsonStructure {
-  books: BookConfig,
-  code: string,
-  id: string,
+  books: BookConfig;
+  code: string;
+  id: string;
 }
 
 export default (baseUrl: string) => {
-  const url = `${baseUrl}/rex/release.json`;
   const toJson = (response: any) => response.json() as Promise<ReleaseJsonStructure>;
 
   const loadBookRemoteBookConfig = (url: string): Promise<BookConfig | undefined> => {
     return fetch(url)
       .then(toJson)
-      .then(res => res.books)
-  }
+      .then((res) => res.books);
+  };
 
   return {
     getBookVersionFromUUID: (uuid: string): BookVersion | Promise<BookVersion | undefined> => {
-      return BOOKS[uuid] ? BOOKS[uuid] : loadBookRemoteBookConfig(url).then(books => books && books[uuid])
-    }
-  }
-}
+      const url = `${baseUrl}/rex/release.json`;
+      return BOOKS[uuid] ? BOOKS[uuid] : loadBookRemoteBookConfig(url).then((books) => books && books[uuid]);
+    },
+  };
+};
