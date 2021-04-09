8d55c2fb481927d35585d8eb3e20488492091f4d
Beth Shook <es66@c02dr05mml87.attlocal.net>
2021-04-09 11:30:34 -0500

    Add funcs for checking page type and getting first graph

--- src/app/content/hooks/receiveContent.ts
+++ src/app/content/hooks/receiveContent.ts
@@ -12,11 +12,22 @@ import getCleanContent from '../utils/getCleanContent';
 import { createTitle } from '../utils/seoUtils';
 
 const stripHtmlAndTrim = (str: string) => str
+  .replace(/(<span class="os-math-in-para">)(.*?)(<\/span>)/g, ' ... ')
   .replace(/<[^>]*>/g, ' ')
   .replace(/ +/g, ' ')
   .trim()
   .substring(0, 155)
   .trim();
+
+const getPageType = (str: string) => {
+  const dataTypes = str.match(/(data-type=")([\w\-]+)"/) || [];
+  return dataTypes.length ? dataTypes[0].replace(/(data-type=")/g, ' ').replace(/"/g, ' ').trim() : '';
+}
+
+const getFirstParagraph = (str: string) => {
+  const paragraphs = str.match(/<p (.*?)>(.*?)<\/p>/g) || [];
+  return paragraphs[0];
+}
 
 const hookBody: ActionHookBody<typeof receivePage> = ({
   getState,
@@ -44,8 +55,11 @@ const hookBody: ActionHookBody<typeof receivePage> = ({
   const title = createTitle(page, book);
 
   // the abstract could be '<div/>'.
-  const abstract = stripHtmlAndTrim(page.abstract ? page.abstract : '');
-  const description = abstract || stripHtmlAndTrim(getCleanContent(book, page, archiveLoader));
+  // const abstract = stripHtmlAndTrim(page.abstract ? page.abstract : '');
+  const cleanContent = getCleanContent(book, page, archiveLoader);
+  const pageType = getPageType(cleanContent);
+  const firstParagraph = getFirstParagraph(cleanContent);
+  const description = pageType === "page" ? stripHtmlAndTrim(firstParagraph): `On this page you will discover the ${page.title} for Chapter X of OpenStax's ${book.title} free college textbook.`
   const canonical = await getCanonicalUrlParams(archiveLoader, osWebLoader, book, page.id, book.version);
   const canonicalUrl = canonical && contentRoute.getUrl(canonical);
   const bookTheme = theme.color.primary[hasOSWebData(book) ? book.theme : defaultTheme].base;
