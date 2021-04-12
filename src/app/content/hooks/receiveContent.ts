5923d9b0501b359b1571f7b8d977963aca4050dd
Beth Shook <es66@c02dr05mml87.attlocal.net>
2021-04-12 13:49:50 -0500

    Move logic out of receiveContent hook to SEO utils

--- src/app/content/hooks/receiveContent.ts
+++ src/app/content/hooks/receiveContent.ts
@@ -9,20 +9,19 @@ import { content as contentRoute } from '../routes';
 import * as select from '../selectors';
 import { getCanonicalUrlParams } from '../utils/canonicalUrl';
 import getCleanContent from '../utils/getCleanContent';
-import { createTitle, getDescriptionPhrase } from '../utils/seoUtils';
+import { createTitle, createDescription } from '../utils/seoUtils';
 import { findArchiveTreeNodeById } from '../utils/archiveTreeUtils';
 import { assertDefined } from '../../utils';
 
-const domParser = new DOMParser();
 
-const stripHtmlAndTrim = (str: string) => str
-  .replace(/(<span class='os-math-in-para'>)(.*?)(<\/span>)/g, ' ... ')
-  .replace(/<[^>]*>/g, ' ')
-  .replace(/ +/g, ' ')
-  .replace(/ ,/, ',')
-  .trim()
-  .substring(0, 155)
-  .trim();
+// const stripHtmlAndTrim = (str: string) => str
+//   .replace(/(<span class='os-math-in-para'>)(.*?)(<\/span>)/g, ' ... ')
+//   .replace(/<[^>]*>/g, ' ')
+//   .replace(/ +/g, ' ')
+//   .replace(/ ,/, ',')
+//   .trim()
+//   .substring(0, 155)
+//   .trim();
 
 const hookBody: ActionHookBody<typeof receivePage> = ({
   getState,
@@ -49,18 +48,17 @@ const hookBody: ActionHookBody<typeof receivePage> = ({
 
   const title = createTitle(page, book);
   const cleanContent = getCleanContent(book, page, archiveLoader);
-  const doc = domParser.parseFromString(cleanContent, 'text/html');
-  const contentNode = doc.body.children[0];
-  const pageType = contentNode.classList.contains('appendix') ? 'appendix' : contentNode.getAttribute('data-type');
-  const firstParagraph = contentNode.querySelector('p')?.outerHTML || '';
+
   const node = assertDefined(
     findArchiveTreeNodeById(book.tree, page.id),
     `couldn't find node for a page id: ${page.id}`
   );
-  const descriptionPhrase = getDescriptionPhrase(node);
-  const description = pageType === 'page'
-    ? stripHtmlAndTrim(firstParagraph)
-    : `On this page you will discover ${descriptionPhrase} OpenStax's ${book.title} free college textbook.`;
+
+  const description = createDescription(cleanContent, node);
+
+  // const description = pageType === 'page'
+  //   ? stripHtmlAndTrim(firstParagraph)
+  //   : `On this page you will discover ${descriptionPhrase} OpenStax's ${book.title} free college textbook.`;
   const canonical = await getCanonicalUrlParams(archiveLoader, osWebLoader, book, page.id, book.version);
   const canonicalUrl = canonical && contentRoute.getUrl(canonical);
   const bookTheme = theme.color.primary[hasOSWebData(book) ? book.theme : defaultTheme].base;
--- src/app/content/utils/seoUtils.ts
+++ src/app/content/utils/seoUtils.ts
@@ -1,3 +1,4 @@
+import { Node } from 'styled-icons/fa-brands';
 import { assertDefined } from '../../utils';
 import { Book, LinkedArchiveTreeNode, Page } from '../types';
 import {
@@ -8,6 +9,8 @@ import {
   getArchiveTreeSectionTitle,
   splitTitleParts,
 } from './archiveTreeUtils';
+
+const domParser = new DOMParser();
 
 const getParentPrefix = (node: LinkedArchiveTreeNode | undefined): string => {
   if (!node) {
@@ -25,17 +28,42 @@ const getParentPrefix = (node: LinkedArchiveTreeNode | undefined): string => {
 
 };
 
-export const getDescriptionPhrase = (node: LinkedArchiveTreeNode | undefined): string => {
+// need to find out correct type here
+const getPageType = (node: any) => {
   if (!node) {
     return '';
   }
+  
+  if (node.classList.contains('appendix')) {
+    return 'appendix';
+  } else {
+    return node.getAttribute('data-type');
+  }
+}
+
+export const createDescription = (pageContent: string, node: LinkedArchiveTreeNode | undefined): string => {
+  if (!node) {
+    return '';
+  }
+
+  const doc = domParser.parseFromString(pageContent, 'text/html');
+  const contentNode = doc.body.children[0];
+  const pageType = getPageType(contentNode);
 
   const prefix = getParentPrefix(node.parent).trim();
   const sectionTitle = getArchiveTreeSectionTitle(node);
   const chapterFromSlug = node.slug.match(/\d*(?=-)/) || [];
   const isAnswerKey = prefix === 'Answer Key';
 
-  if (isAnswerKey) {
+  if (pageType === "page") {
+    const firstParagraph = contentNode.querySelector('p');
+    const mathSpans = firstParagraph.querySelectorAll('.os-math-in-para');
+    mathSpans.forEach((el) => {
+      el.outerHTML = "...";
+    })
+    console.log(firstParagraph.textContent.substring(0, 155))
+    return contentNode.querySelector('p')?.outerHTML || '';
+  } else if (isAnswerKey) {
     return `the Answer Key of ${sectionTitle}`;
   } else {
     return chapterFromSlug[0] ? `${sectionTitle} for Chapter ${chapterFromSlug[0]} of` : `${sectionTitle} for`;
