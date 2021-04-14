f331f0c0cb979e31b2e83d48fc04ff7ee454e551
piotr@obrandowani.pl <piotr@obrandowani.pl>
2021-04-14 15:47:05 +0200

    remove unused variable

--- script/get-modified-books.bash
+++ script/get-modified-books.bash
@@ -23,7 +23,6 @@ rm src/config.books.old.json
 working_set=$(jq "map({(.book_id): .}) | add // {}" <<< "$book_json")
 
 for book_id in $(jq -r "keys[]" <<< "$working_set"); do
-  book_version=$(jq -r --arg book_id "$book_id" '.[$book_id].book_version' <<< "$working_set")
   title=$(node script/entry.js book-info --field=title "$book_id")
   working_set=$(jq -r \
     --arg title "$title" \
