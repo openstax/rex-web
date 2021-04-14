2b5f5dd1e0ab36aca33ab1d4efb795ca48ad7e0b
Piotr Kozłowski <piotr@obrandowani.pl>
2021-04-14 16:15:59 +0200

    Issue 1518 - add support for new web pipeline with archive code versions (#1133)
    * Update REACT_APP_ARCHIVE_URL
    
    * Update logic to work with new web pipeline
    
    * Initial work on tests
    
    * keep working on tests
    
    * fix all unit tests
    
    * Fix test:build/browser
    
    * update paths to fixtures
    
    * update get-modified books
    
    * update concourse/update-content/script.bash
    
    * lint
    
    * requested changes
    
    * move getContentPageReferences to utils
    
    * Update REACT_APP_ARCHIVE_URL in config.test.js
    
    * noop quickly if book version wasnt specified and adjust tests
    
    * use version specified in book params
    
    * book-info script will use book version from config
    
    * remove unused variable
    
    * add removed test
    
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>

--- script/book-info.ts
+++ script/book-info.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import { argv } from 'yargs';
-import { ARCHIVE_URL, REACT_APP_ARCHIVE_URL } from '../src/config';
+import { ARCHIVE_URL, BOOKS, REACT_APP_ARCHIVE_URL } from '../src/config';
 import createArchiveLoader from '../src/gateways/createArchiveLoader';
 
 const {
@@ -14,7 +14,8 @@ const {
 const archiveLoader = createArchiveLoader(`${ARCHIVE_URL}${REACT_APP_ARCHIVE_URL}`);
 
 const bookId = argv._[1];
-archiveLoader.book(bookId).load().then((book: any) => {
+const bookVersion = BOOKS[bookId].defaultVersion;
+archiveLoader.book(bookId, bookVersion).load().then((book: any) => {
   if (field) {
     // tslint:disable-next-line:no-console
     console.log(book[field]);
