8c7b29d2f7b9e3f676a0d3bc0d5aad1255f2d622
Beth Shook <es66@rice.edu>
2021-04-16 12:01:43 -0500

    linting

--- src/app/content/utils/seoUtils.constants.ts
+++ src/app/content/utils/seoUtils.constants.ts
@@ -1,107 +1,107 @@
 // tslint:disable: object-literal-sort-keys max-line-length
 export const mockBook = {
-    title: 'JavaScript Testing',
-    id: '',
-    license: {
-        name: '',
-        url: '',
-        version: '',
-    },
+  title: 'JavaScript Testing',
+  id: '',
+  license: {
+    name: '',
+    url: '',
     version: '',
-    revised: '',
-    tree: {
-        id: '123',
-        title: 'JavaScript Testing',
-        slug: 'javascript-testing-slug',
+  },
+  version: '',
+  revised: '',
+  tree: {
+    id: '123',
+    title: 'JavaScript Testing',
+    slug: 'javascript-testing-slug',
+    contents: [
+      {
+        id: '1',
+        title: '<span data-type="" itemprop="" class="os-text">Preface</span>',
+        slug: 'preface-slug',
+        contents: [],
+      },
+      {
+        id: '2',
+        title: '<span class="os-number"><span class="os-part-text">Chapter </span>1</span><span class="os-divider"> </span><span data-type="" itemprop="" class="os-text">What is a test?</span>',
+        slug: '2-what-is-a-test-slug',
+        contents: [],
+      },
+      {
+        id: '3',
+        title: '<span class="os-number"><span class="os-part-text">Chapter </span>2</span><span class="os-divider"> </span><span data-type="" itemprop="" class="os-text">Unit Testing</span>',
+        slug: '3-unit-testing',
         contents: [
-            {
-                id: '1',
-                title: '<span data-type="" itemprop="" class="os-text">Preface</span>',
-                slug: 'preface-slug',
-                contents: [],
-            },
-            {
-                id: '2',
-                title: '<span class="os-number"><span class="os-part-text">Chapter </span>1</span><span class="os-divider"> </span><span data-type="" itemprop="" class="os-text">What is a test?</span>',
-                slug: '2-what-is-a-test-slug',
-                contents: [],
-            },
-            {
-                id: '3',
-                title: '<span class="os-number"><span class="os-part-text">Chapter </span>2</span><span class="os-divider"> </span><span data-type="" itemprop="" class="os-text">Unit Testing</span>',
-                slug: '3-unit-testing',
-                contents: [
-                    {
-                        id: '4',
-                        title: '<span data-type="" itemprop="" class="os-text">Chapter Review</span>',
-                        slug: 'chapter-review-slug',
-                        contents: [],
-                    },
-                ],
-            },
+          {
+            id: '4',
+            title: '<span data-type="" itemprop="" class="os-text">Chapter Review</span>',
+            slug: 'chapter-review-slug',
+            contents: [],
+          },
         ],
-    },
+      },
+    ],
+  },
 };
 
 export const contentPage = {
-    title: 'Introduction',
-    content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" class="introduction" data-cnxml-to-html-ver="2.1.0"><p>For example, take a look at the image above. This image is of the Andromeda Galaxy, which contains billions of individual stars, huge clouds of gas, and dust. Two smaller galaxies are also visible as bright blue spots in the background. At a staggering 2.5 million light years from the Earth, this galaxy is the nearest one to our own galaxy (which is called the Milky Way). The stars and planets that make up Andromeda might seem to be the furthest thing from most people’s regular, everyday lives. But Andromeda is a great starting point to think about the forces that hold together the universe.</p><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
-    abstract: '',
-    id: '5',
-    version: '0',
-    revised: '',
+  title: 'Introduction',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" class="introduction" data-cnxml-to-html-ver="2.1.0"><p>For example, take a look at the image above. This image is of the Andromeda Galaxy, which contains billions of individual stars, huge clouds of gas, and dust. Two smaller galaxies are also visible as bright blue spots in the background. At a staggering 2.5 million light years from the Earth, this galaxy is the nearest one to our own galaxy (which is called the Milky Way). The stars and planets that make up Andromeda might seem to be the furthest thing from most people’s regular, everyday lives. But Andromeda is a great starting point to think about the forces that hold together the universe.</p><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
+  abstract: '',
+  id: '5',
+  version: '0',
+  revised: '',
 };
 
 export const contentPageShort = {
-    title: 'Introduction',
-    content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>Two smaller galaxies are also visible as bright blue spots in the background.</p></div></body></html>',
-    abstract: '',
-    id: '6',
-    version: '0',
-    revised: '',
+  title: 'Introduction',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>Two smaller galaxies are also visible as bright blue spots in the background.</p></div></body></html>',
+  abstract: '',
+  id: '6',
+  version: '0',
+  revised: '',
 };
 
 export const contentPageWithObjectives = {
-    title: 'How to Write a Test',
-    content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><section class="learning-objectives"><h3 data-type="title">Learning Objectives</h3><p>By the end of this section, you will be able to:</p><ul><li>Explain the importance of citizen engagement in a democracy</li></ul></section><p>This is the paragraph that comes after the learning objectives section. It does not have any special classes applied.</p></div></body></html>',
-    abstract: '',
-    id: '7',
-    version: '0',
-    revised: '',
+  title: 'How to Write a Test',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><section class="learning-objectives"><h3 data-type="title">Learning Objectives</h3><p>By the end of this section, you will be able to:</p><ul><li>Explain the importance of citizen engagement in a democracy</li></ul></section><p>This is the paragraph that comes after the learning objectives section. It does not have any special classes applied.</p></div></body></html>',
+  abstract: '',
+  id: '7',
+  version: '0',
+  revised: '',
 };
 
 export const answerKeyPage = {
-    title: 'Chapter 3',
-    content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0" class="os-solutions-container"><div data-type="solution" id="fs-id1171474484001-solution"><a class="os-number" href="1-review-questions#fs-id1171474484001">1</a><span class="os-divider">. </span><div class="os-solution-container "><p id="fs-id1171474202764">B</p></div></div></div></body></html>',
-    abstract: '',
-    id: '8',
-    version: '0',
-    revised: '',
+  title: 'Chapter 3',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0" class="os-solutions-container"><div data-type="solution" id="fs-id1171474484001-solution"><a class="os-number" href="1-review-questions#fs-id1171474484001">1</a><span class="os-divider">. </span><div class="os-solution-container "><p id="fs-id1171474202764">B</p></div></div></div></body></html>',
+  abstract: '',
+  id: '8',
+  version: '0',
+  revised: '',
 };
 
 export const eocPage = {
-    title: 'Review Questions',
-    content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
-    abstract: '',
-    id: '9',
-    version: '0',
-    revised: '',
+  title: 'Review Questions',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
+  abstract: '',
+  id: '9',
+  version: '0',
+  revised: '',
 };
 
 export const eobPage = {
-    title: 'References',
-    content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
-    abstract: '',
-    id: '10',
-    version: '0',
-    revised: '',
+  title: 'References',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
+  abstract: '',
+  id: '10',
+  version: '0',
+  revised: '',
 };
 
 export const eocSubPage = {
-    title: 'Key Concepts',
-    content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
-    abstract: '',
-    id: '11',
-    version: '0',
-    revised: '',
+  title: 'Key Concepts',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="1d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
+  abstract: '',
+  id: '11',
+  version: '0',
+  revised: '',
 };
