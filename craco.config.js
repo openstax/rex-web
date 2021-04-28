13ab6adfd4fe27c6ab3fa6fe27aec2bf296b4858
piotr@obrandowani.pl <piotr@obrandowani.pl>
2021-04-28 11:59:58 +0200

    Add craco and set url: false in css-loader

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    style: {
+        css: {
+            loaderOptions: {
+                // https://github.com/openstax/unified/issues/1469
+                url: false,
+            },
+        },
+    },
+};
