daaaea8b96c2b59e813e646c95f6053a45c95a44
Beth Shook <es66@rice.edu>
2021-04-30 15:33:54 -0500

    Add i18n message for metadata desc

--- src/app/content/utils/seoUtils.ts
+++ src/app/content/utils/seoUtils.ts
@@ -1,4 +1,5 @@
 import { Element, HTMLElement } from '@openstax/types/lib.dom';
+import { useIntl } from 'react-intl';
 import { AppServices } from '../../types';
 import { assertDefined } from '../../utils';
 import { Book, LinkedArchiveTreeNode, Page } from '../types';
@@ -87,7 +88,7 @@ export const getPageDescription = (loader: AppServices['archiveLoader'], book: B
   const contentDescription: string | null = getPageDescriptionFromContent(doc.body.firstElementChild);
 
   // tslint:disable-next-line: max-line-length
-  return contentDescription || 'OpenStax is a non-profit organization committed to improving student access to quality learning materials. Our free textbooks are developed and peer-reviewed by educators to ensure they are readable and accurate.';
+  return contentDescription || useIntl().formatMessage({id: 'i18n:metadata:description'});
 };
 
 export const createTitle = (page: Page, book: Book): string => {
