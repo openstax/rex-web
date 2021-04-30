a19609ce6dc66b943b980841aecf035b8e33fe8a
Beth Shook <es66@rice.edu>
2021-04-30 12:54:45 -0500

    Remove template logic from seoUtils

--- src/app/content/utils/seoUtils.ts
+++ src/app/content/utils/seoUtils.ts
@@ -1,4 +1,4 @@
-import { Element, HTMLElement } from '@openstax/types/lib.dom';
+import { Element } from '@openstax/types/lib.dom';
 import { AppServices } from '../../types';
 import { assertDefined } from '../../utils';
 import { Book, LinkedArchiveTreeNode, Page } from '../types';
@@ -11,22 +11,6 @@ import {
   getArchiveTreeSectionTitle,
   splitTitleParts,
 } from './archiveTreeUtils';
-
-type Services = {
-  intl: AppServices['intl'];
-  loader: AppServices['archiveLoader'];
-};
-
-type PageTypes = 'page' | 'answer-key' | 'subpage' | 'eoc-page' | 'eob-page' | 'appendix' | 'index';
-
-type TreeNodeTypes = 'chapter' | 'book' | 'other';
-
-interface DescriptionTemplateValues {
-  parentTitle: string;
-  pageTitle: string;
-  bookTitle: string;
-  parentPrefix: string;
-}
 
 const domParser = new DOMParser();
 
@@ -79,31 +63,6 @@ export const generateExcerpt = (str: string) => {
   return str.replace(/\n+/g, ' ').replace(/\s+/g, ' ').trim().substring(0, 152) + '...';
 };
 
-// tslint:disable-next-line: max-line-length
-const getPageType = (node: HTMLElement, parentType: TreeNodeTypes, parentPrefix: string, pageTitle: string ): PageTypes => {
-  const nodeClasses = node.classList;
-  const nodeType = node.getAttribute('data-type');
-  const answerKeyClasses = ['os-solution-container', 'os-solutions-container', 'os-end-of-book-solutions-container'];
-
-  if (nodeType === 'page' && !nodeClasses.contains('appendix')) {
-    return 'page';
-  } else if (nodeClasses.contains('appendix')) {
-    return 'appendix';
-  } else if (nodeClasses.contains('os-index-container')) {
-    return 'index';
-  } else if (pageTitle === parentPrefix) {
-    return 'eob-page';
-  } else if (answerKeyClasses.some((className) => nodeClasses.contains(className))) {
-    return 'answer-key';
-  } else if (parentType !== 'chapter' && parentType !== 'book') {
-    return 'subpage';
-  } else if (pageTitle !== parentPrefix) {
-    return 'eoc-page';
-  } else {
-    throw new Error('no page type detected');
-  }
-};
-
 const getPageDescriptionFromContent = (node: Element): string | null => {
   const page = node.firstElementChild;
   if (!page) {
@@ -120,46 +79,16 @@ const getPageDescriptionFromContent = (node: Element): string | null => {
   return foundByLength && foundByLength.textContent ? generateExcerpt(foundByLength.textContent) : null;
 };
 
-const getTemplateVars = (book: Book, node: LinkedArchiveTreeNode) => {
-  const parentTitle = node.parent ? getTextContent(node.parent.title) : null;
-  const parentPrefix = getParentPrefix(node, true).replace('Ch.', 'Chapter').trim();
-  const pageTitle = getArchiveTreeSectionTitle(node);
-  const values: DescriptionTemplateValues = {
-    bookTitle: book.title,
-    pageTitle,
-    parentPrefix,
-    parentTitle,
-  };
-
-  return values;
-};
-
-const getParentType = (node: LinkedArchiveTreeNode): TreeNodeTypes =>
-  node && node.parent && archiveTreeSectionIsChapter(node.parent)
-    ? 'chapter'
-    : (node && node.parent && archiveTreeSectionIsBook(node.parent)
-      ? 'book'
-      : 'other');
-
-export const getPageDescription = (services: Services, book: Book, page: Page) => {
-  const intl = services.intl;
-  const cleanContent = getCleanContent(book, page, services.loader);
+export const getPageDescription = (loader: AppServices['archiveLoader'], book: Book, page: Page) => {
+  const cleanContent = getCleanContent(book, page, loader);
   const doc = domParser.parseFromString(cleanContent, 'text/html');
-  const node = doc.body.children[0];
   const treeNode = findArchiveTreeNodeById(book.tree, page.id);
   if (!treeNode) {
     return '';
   }
-  const values = getTemplateVars(book, treeNode);
-  const parentType = getParentType(treeNode);
-  const { parentTitle, pageTitle, parentPrefix, bookTitle } = values;
-  const pageType = getPageType(node, parentType, parentPrefix, pageTitle);
-  const contentDescription: string | null = pageType === 'page'
-    ? getPageDescriptionFromContent(doc.body)
-    : null;
+  const contentDescription: string | null = getPageDescriptionFromContent(doc.body);
 
-  return contentDescription
-  || intl.formatMessage({ id: `i18n:metadata:${pageType}` }, { parentTitle, pageTitle, parentPrefix, bookTitle });
+  return contentDescription || 'OpenStax is ...';
 };
 
 export const createTitle = (page: Page, book: Book): string => {
