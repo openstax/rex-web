94da447da0fc40b622dad876d713f588e03b95ea
Beth Shook <es66@rice.edu>
2021-05-10 17:01:30 -0500

    Add helper for loading google optimize

---
+++ src/helpers/loadOptimize.ts
@@ -0,0 +1,16 @@
+export default (containerId: string) => new Promise((resolve) => {
+    if (typeof(document) === 'undefined' || !document.head) {
+      throw new Error('Google Optimize can only be loaded in the browser');
+    }
+
+    if (document.head.querySelector(`script[src=https://www.googleoptimize.com/optimize.js?id=${containerId}]`)) {
+      resolve();
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', `https://www.googleoptimize.com/optimize.js?id=${containerId}`);
+    script.onload = resolve;
+    document.head.appendChild(script);
+  });
