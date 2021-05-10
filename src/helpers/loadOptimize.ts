9e3648bf37a4eb549426543d44a5c8707288ffe3
Beth Shook <es66@rice.edu>
2021-05-10 18:55:48 -0500

    Continue trying to trigger GO callback

--- src/helpers/loadOptimize.ts
+++ src/helpers/loadOptimize.ts
@@ -1,3 +1,5 @@
+import { assertWindow } from '../app/utils';
+
 export default (containerId: string) => new Promise((resolve) => {
     if (typeof(document) === 'undefined' || !document.head) {
       throw new Error('Google Optimize can only be loaded in the browser');
@@ -8,4 +10,15 @@ export default (containerId: string) => new Promise((resolve) => {
     script.setAttribute('src', `https://www.googleoptimize.com/optimize.js?id=${containerId}`);
     script.onload = resolve;
     document.head.appendChild(script);
+
+    const window = assertWindow('Browser entrypoint must be used in the browser');
+
+    function myCallback() {
+        console.log('my callback: ', arguments);
+    }
+
+    window.dataLayer.push('event', 'optimize.callback', {
+        callback: myCallback,
+    });
+    window.dataLayer.push({event: 'optimize.activate'});
   });
