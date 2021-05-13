4a6f9a17f23a32d0b555042d4f7107ad8fa6392e
Beth Shook <es66@rice.edu>
2021-05-13 09:39:39 -0500

    Comment sentry enabling in dev config

--- src/config.development.js
+++ src/config.development.js
@@ -29,7 +29,7 @@ module.exports = {
   ACCOUNTS_URL: process.env.ACCOUNTS_URL || 'https://accounts-dev.openstax.org',
   OS_WEB_URL: process.env.OS_WEB_URL || 'https://cms-dev.openstax.org',
   HIGHLIGHTS_URL: 'https://highlights-hl-bbb4bb5.sandbox.openstax.org',
-  SENTRY_ENABLED: true,
+  // SENTRY_ENABLED: true,
 
   SKIP_OS_WEB_PROXY: process.env.SKIP_OS_WEB_PROXY !== undefined,
   FIXTURES: false,
