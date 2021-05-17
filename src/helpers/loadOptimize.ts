11eb60ee11796debd4582a8a5a104071d37b6fd9
Beth Shook <es66@rice.edu>
2021-05-17 15:32:52 -0500

    import assertDocument helper

--- src/helpers/loadOptimize.ts
+++ src/helpers/loadOptimize.ts
@@ -1,15 +1,13 @@
+import { assertDocument } from '../app/utils';
+
 export default (containerId: string | null) => new Promise((resolve) => {
     if (!containerId) {
         return;
     }
 
-    if (typeof(document) === 'undefined' || !document.head) {
-      throw new Error('Google Optimize can only be loaded in the browser');
-    }
-
-    const script = document.createElement('script');
+    const script = assertDocument().createElement('script');
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('src', `https://www.googleoptimize.com/optimize.js?id=${containerId}`);
     script.onload = resolve;
-    document.head.appendChild(script);
+    assertDocument().head.appendChild(script);
   });
