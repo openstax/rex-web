5b33acf6ce84fc614a9104991459b069a79d1a45
Beth Shook <es66@rice.edu>
2021-05-21 13:09:59 -0500

    Revert change to auth init

--- src/app/auth/init/establishState.ts
+++ src/app/auth/init/establishState.ts
@@ -2,13 +2,12 @@ import { Initializer } from '../../types';
 import { receiveLoggedOut, receiveUser } from '../actions';
 import { formatUser } from '../utils';
 
-const initializer: Initializer = async({dispatch}) => {
+const initializer: Initializer = async({dispatch, userLoader}) => {
   if (typeof(document) === 'undefined') {
     return;
   }
 
-  // const user = await userLoader.getCurrentUser();
-  const user = null;
+  const user = await userLoader.getCurrentUser();
 
   if (user) {
     dispatch(receiveUser(formatUser(user)));
