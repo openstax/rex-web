67e2876d803c9af91836f9158e0484edee851cc9
Beth Shook <es66@rice.edu>
2021-06-07 14:26:00 -0500

    Mock appending of optimize scripts

--- src/helpers/loadOptimize.spec.ts
+++ src/helpers/loadOptimize.spec.ts
@@ -31,35 +31,47 @@ describe('loadOptimize', () => {
 
   afterEach(() => {
     document.head.innerHTML = '';
-    window.dataLayer = [];
   });
 
   it('injects correct <script> into head if in production', async() => {
     config.DEPLOYED_ENV = 'openstax.org';
     window.dataLayer.push = jest.fn();
+    window.gtag = jest.fn();
 
+    jest.spyOn(document.head, 'appendChild');
     await loadOptimize(window, store);
-    const script = document.head.querySelector('script');
 
-    expect(script!.src).toEqual(
-      'https://www.googleoptimize.com/optimize.js?id=OPT-NFHSM4B'
-    );
+    const script = document.head.querySelector('script');
+    expect(script).toMatchInlineSnapshot(`
+    <script
+      src="https://www.googleoptimize.com/optimize.js?id=OPT-NFHSM4B"
+      type="text/javascript"
+    />
+  `);
+
   });
 
   it('injects correct <script> into head if in development', async() => {
     config.DEPLOYED_ENV = 'foo';
     window.dataLayer.push = jest.fn();
+    window.gtag = jest.fn();
 
+    jest.spyOn(document.head, 'appendChild');
     await loadOptimize(window, store);
-    const script = document.head.querySelector('script');
 
-    expect(script!.src).toEqual(
-      'https://www.googleoptimize.com/optimize.js?id=OPT-W65B3CP'
-    );
+    const script = document.head.querySelector('script');
+    expect(script).toMatchInlineSnapshot(`
+    <script
+      src="https://www.googleoptimize.com/optimize.js?id=OPT-W65B3CP"
+      type="text/javascript"
+    />
+  `);
   });
 
   it('registers optimize callback and correctly dispatches action', async() => {
     config.DEPLOYED_ENV = 'foo';
+
+    jest.spyOn(document.head, 'appendChild');
     await loadOptimize(window, store);
 
     ((window.dataLayer[0] as any)[2] as any).callback('1', 'OCCkMMCZSwW87szzpniCow');
