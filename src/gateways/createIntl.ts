b9cc9226b510f7edde6bd862ef7d84f71e0c7c32
Beth Shook <es66@rice.edu>
2021-06-09 17:38:51 -0500

    Dynamically load translations

--- src/gateways/createIntl.ts
+++ src/gateways/createIntl.ts
@@ -1,15 +1,13 @@
 import { createIntl, createIntlCache } from 'react-intl';
-import enMessages from '../app/messages/en';
-import plMessages from '../app/messages/pl';
 
 export default () => {
   return {
-    getIntlObject: (locale: string = 'en') => {
+    getIntlObject: async(locale: string = 'en') => {
         const cache = createIntlCache();
 
         const intl = createIntl({
           locale,
-          messages: locale === 'pl' ? plMessages : enMessages,
+          messages: await import(`../app/messages/${locale}`),
         }, cache);
 
         return intl;
