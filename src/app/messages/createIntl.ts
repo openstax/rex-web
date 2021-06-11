9aa5c8fc597f14243b921b5ac1f1ed5e5a6e8ba9
Beth Shook <es66@rice.edu>
2021-06-11 11:02:16 -0500

    Fix typo causing compile error

--- src/app/messages/createIntl.ts
+++ src/app/messages/createIntl.ts
@@ -2,9 +2,9 @@ import { createIntl, createIntlCache } from 'react-intl';
 
 export default () => {
   return {
-    getIntlObject: async(locale: string = 'en') => {
+    getIntlObject: async(locale: string) => {
         const cache = createIntlCache();
-        const messages = await require(`../${locale}`).default;
+        const messages = await require(`./${locale}`).default;
 
         const intl = createIntl({
           locale,
