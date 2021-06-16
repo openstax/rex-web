91a55abc0296fc4fdc5f1a1d31d1b3e39b6fd21d
Beth Shook <es66@rice.edu>
2021-06-16 11:33:24 -0500

    Update message provider tests

--- src/app/messages/MessageProvider.spec.tsx
+++ src/app/messages/MessageProvider.spec.tsx
@@ -1,25 +1,57 @@
-import { resetModules } from '../../test/utils';
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import createTestServices from '../../test/createTestServices';
+import createTestStore from '../../test/createTestStore';
+import { book as archiveBook } from '../../test/mocks/archiveLoader';
+import { mockCmsBook } from '../../test/mocks/osWebLoader';
+import { resetModules, runHooksAsync } from '../../test/utils';
+import { receiveBook } from '../content/actions';
+import { formatBookData } from '../content/utils';
+import * as Services from '../context/Services';
+import MessageProvider from '../messages/MessageProvider';
+import { Store } from '../types';
+
+const book = formatBookData(archiveBook, mockCmsBook);
 
 describe('MessageProvider', () => {
+  let store: Store;
+  let services: ReturnType<typeof createTestServices>;
 
   beforeEach(() => {
+    store = createTestStore();
+    services = createTestServices();
+    store.dispatch(receiveBook(book));
     resetModules();
   });
 
-  it('doesn\'t polyfill when api exists', () => {
+  it('doesn\'t polyfill when api exists', async() => {
     let loaded = false;
 
     jest.doMock('@formatjs/intl-pluralrules/polyfill', () => {
       loaded = true;
     });
-    require('./MessageProvider');
+
+    renderer.create(
+      <Provider store={store}>
+        <Services.Provider value={services}>
+          <MessageProvider />
+        </Services.Provider>
+      </Provider>
+    );
+
+    await runHooksAsync();
 
     expect(loaded).toBe(false);
   });
 
   describe('when api is not there', () => {
+
     beforeEach(() => {
+      store = createTestStore();
+      services = createTestServices();
       (Intl as any).PluralRules.polyfilled = true;
+      store.dispatch(receiveBook(book));
     });
 
     afterEach(() => {
@@ -32,12 +64,20 @@ describe('MessageProvider', () => {
       jest.doMock('@formatjs/intl-pluralrules/should-polyfill', () => ({
         shouldPolyfill: () => true,
       }));
+
       jest.doMock('@formatjs/intl-pluralrules/polyfill', () => {
         loaded = true;
       });
-      require('./MessageProvider');
 
-      await new Promise((resolve) => setImmediate(resolve));
+      renderer.create(
+        <Provider store={store}>
+          <Services.Provider value={services}>
+            <MessageProvider />
+          </Services.Provider>
+        </Provider>
+      );
+
+      await runHooksAsync();
 
       expect(loaded).toBe(true);
     });
@@ -51,9 +91,16 @@ describe('MessageProvider', () => {
       jest.doMock('@formatjs/intl-pluralrules/locale-data/en', () => {
         loaded = true;
       });
-      require('./MessageProvider');
 
-      await new Promise((resolve) => setImmediate(resolve));
+      renderer.create(
+        <Provider store={store}>
+          <Services.Provider value={services}>
+            <MessageProvider />
+          </Services.Provider>
+        </Provider>
+      );
+
+      await runHooksAsync();
 
       expect(loaded).toBe(true);
     });
