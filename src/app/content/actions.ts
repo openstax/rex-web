d5084dcff27c805bec4d9368787a2d9edbeb02c4
jarosik10 <bartosz.jaros96@gmail.com>
2021-06-18 16:20:42 +0200

    fixup! Change confirmationModal action types

--- src/app/content/actions.ts
+++ src/app/content/actions.ts
@@ -28,7 +28,7 @@ export const receivePageNotFoundId = createStandardAction('Content/receivePageNo
 export const openNudgeStudyTools = createStandardAction('Content/openNudgeStudyTools')();
 export const closeNudgeStudyTools = createStandardAction('Content/closeNudgeStudyTools')();
 
-export const showConfirmationModal = createStandardAction('Content/showDiscardChangesConfirmation')<{
+export const showConfirmationModal = createStandardAction('Content/showConfirmationModal')<{
   options: {
     callback: (confimred: boolean) => void,
     headingi18nKey: string,
@@ -37,4 +37,4 @@ export const showConfirmationModal = createStandardAction('Content/showDiscardCh
     cancelButtoni18nKey: string,
   };
 }>();
-export const closeConfirmationModal = createStandardAction('Content/closeConfirmation')<void>();
+export const closeConfirmationModal = createStandardAction('Content/closeConfirmationModal')<void>();
