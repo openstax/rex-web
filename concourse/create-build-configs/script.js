225d9e78279843843fc02944d6630dc8cd7b1765
tom <tom.g.woodward@gmail.com>
2021-06-24 10:54:18 -0400

    add books to build arguments, might be helpful later

--- concourse/create-build-configs/script.js
+++ concourse/create-build-configs/script.js
@@ -2,10 +2,12 @@
 
 const fs = require('fs');
 const versionFile = 'rex-web/.git/ref';
+const booksFile = 'rex-web/src/config.books.json';
 const envFile = 'build-configs/config.env';
 const argFile = 'build-configs/config.json';
 const commitFile = 'build-configs/commit.txt';
 const releaseFile = 'build-configs/release-id.txt';
+const booksConfigFile = 'build-configs/books.json';
 
 const handleErr = err => {
   if (!err) return;
@@ -13,7 +15,12 @@ const handleErr = err => {
   process.exit(1);
 };
 
-fs.readFile(versionFile, 'utf8', function(err, commit) {
+const readFile = (path) => new Promise(resolve => fs.readFile(path, 'utf8', (err, contents) => err ? handleErr(err) : resolve(contents)));
+
+Promise.all([
+  readFile(versionFile),
+  readFile(booksFile)
+]).then(([commit, books]) => {
   // the v3 gives an easy way to detect very old releases, this
   // is the third time i've changed the release id format:
   //  - initially `master/${commit}` became wrong when we started building from non-master places
@@ -25,7 +32,8 @@ fs.readFile(versionFile, 'utf8', function(err, commit) {
     PUBLIC_URL: `/rex/releases/${releaseId}`,
     REACT_APP_CODE_VERSION: commit,
     REACT_APP_RELEASE_ID: releaseId,
-    REACT_APP_ENV: 'production'
+    REACT_APP_ENV: 'production',
+    BOOKS: JSON.stringify(books)
   };
 
   console.log(JSON.stringify(args, null, 2));
@@ -44,4 +52,7 @@ fs.readFile(versionFile, 'utf8', function(err, commit) {
 
   console.log(`Generating release id file with: ${releaseId}`);
   fs.writeFile(releaseFile, releaseId, handleErr);
+
+  console.log(`Generating books file...`);
+  fs.writeFile(booksConfigFile, JSON.stringify(books), handleErr);
 });
