fd742efad3e09686020d7fbf81d8eb00e23a5f46
Beth Shook <es66@rice.edu>
2021-06-25 09:42:23 -0500

    Fix translation typo

--- src/app/messages/pl/modalErrorText.ts
+++ src/app/messages/pl/modalErrorText.ts
@@ -1,4 +1,4 @@
 // tslint:disable: max-line-length
 export const bodyWithLink = `
-  Coś poszło nie tak. Odśwież stronę. Jeśli to nie pomoże, przejdź do<a href="{supportCenterLink}" target="_blank">Centrum wsparcia</a>.
+  Coś poszło nie tak. Odśwież stronę. Jeśli to nie pomoże, przejdź do <a href="{supportCenterLink}" target="_blank">Centrum wsparcia</a>.
 `;
