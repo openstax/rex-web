8b557b3c0cb3ef4ba7de32195f266c238f3c84ec
Beth Shook <es66@rice.edu>
2021-07-06 09:42:30 -0500

    Fix increaseSize func to round to nearest integer in px

--- src/app/utils/increaseSize.ts
+++ src/app/utils/increaseSize.ts
@@ -1,3 +1,4 @@
-const increaseSize = (size: number) => (size * 8 / 7).toFixed(1);
+// rem * 10px * 8/7, then convert back to rem
+const increaseSize = (size: number) => Math.round(size * 10 * 8 / 7) / 10;
 
 export default increaseSize;
