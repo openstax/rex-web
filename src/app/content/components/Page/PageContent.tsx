da9aa6c63ef3bcc377946ae73e0660e29580606a
jarosik10 <bartosz.jaros96@gmail.com>
2021-07-15 15:06:52 +0200

    Revert "changes related to note indicator"
    This reverts commit 43c4ac9bdc332eaed5851c28831d7a7352c781d6.

--- src/app/content/components/Page/PageContent.tsx
+++ src/app/content/components/Page/PageContent.tsx
@@ -94,7 +94,7 @@ export default styled(MainContent)`
         }
       }
 
-      &.first.text.has-note:before {
+      &.first.text.has-note:after {
         position: absolute;
         top: 0;
         left: 0;
@@ -119,8 +119,8 @@ export default styled(MainContent)`
             background-color: ${style.focused};
           }
 
-          &.first.text.has-note:before {
-            content: none;
+          &.first.text.has-note:after {
+            display: none;
           }
         }
       }
