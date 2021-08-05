926fec83ca6d8620121bdfd73651f9dbfbbc49cf
tom <tom.g.woodward@gmail.com>
2021-08-05 14:46:30 -0400

    cheat, basically

--- src/gateways/eventCaptureClient.ts
+++ src/gateways/eventCaptureClient.ts
@@ -1,11 +1,12 @@
 import { createCaptureContext } from '@openstax/event-capture-client/capture';
 import { trackingIsDisabled } from '../helpers/analytics';
 import sentry from '../helpers/Sentry';
+import { not } from "../app/fpUtils";
 
 const {capture, configure, queue} = createCaptureContext({
   initialized: false,
   reportError: sentry.captureException,
-  sendingEnabled: () => !trackingIsDisabled(),
+  sendingEnabled: not(trackingIsDisabled),
 });
 
 export const captureQueue = queue;
