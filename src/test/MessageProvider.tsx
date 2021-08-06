97cfa52a047b5e54f02882542aa0d5f9a706fdbc
Beth Shook <es66@rice.edu>
2021-08-06 11:13:46 -0500

    Pare down test MessageProvider

--- src/test/MessageProvider.tsx
+++ src/test/MessageProvider.tsx
@@ -1,4 +1,3 @@
-import { shouldPolyfill } from '@formatjs/intl-pluralrules/should-polyfill';
 import React from 'react';
 import { RawIntlProvider } from 'react-intl';
 import createIntl from './createIntl';
@@ -7,20 +6,6 @@ interface Props {
   locale?: string;
   messages?: Record<string, string>;
 }
-
-// https://formatjs.io/docs/polyfills/intl-pluralrules/#dynamic-import--capability-detection
-async function polyfill(locale: 'en') {
-  if (shouldPolyfill()) {
-    await import('@formatjs/intl-pluralrules/polyfill');
-  }
-
-  // boolean added by the polyfill
-  if ((Intl.PluralRules as (typeof Intl.PluralRules & {polyfilled?: boolean})).polyfilled) {
-    await import(`@formatjs/intl-pluralrules/locale-data/${locale}`);
-  }
-}
-
-polyfill('en');
 
 // tslint:disable-next-line:variable-name
 const MessageProvider = ({children, ...props}: React.PropsWithChildren<Props>) => {
