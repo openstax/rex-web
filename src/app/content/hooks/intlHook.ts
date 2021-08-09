3cf3c62b7bddd9ff9f6b8fc1c25afc285477c4e2
Beth Shook <es66@rice.edu>
2021-08-09 15:29:16 -0500

    Adjust tests for coverage

--- src/app/content/hooks/intlHook.ts
+++ src/app/content/hooks/intlHook.ts
@@ -5,11 +5,7 @@ export const hookBody = (services: MiddlewareAPI & AppServices) => async() => {
   const state = services.getState();
   const lang = state.content.book?.language;
 
-  if (!lang) {
-    return;
-  }
-
-  services.intl.current = await createIntl(lang);
+  services.intl.current = await createIntl(lang || '');
 };
 
 export default hookBody;
