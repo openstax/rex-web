8f267d88849dd6b11c8c2bfc6bcfb58cca979cfe
tom <tom.g.woodward@gmail.com>
2021-08-16 13:06:14 -0400

    rejigger some stuff

--- src/gateways/eventCaptureClient.ts
+++ src/gateways/eventCaptureClient.ts
@@ -1,7 +1,7 @@
 import { createCaptureContext } from '@openstax/event-capture-client/capture';
-import { trackingIsDisabled } from '../helpers/analytics';
+import { not } from '../app/fpUtils';
+import { trackingIsDisabled } from '../helpers/analytics/utils';
 import sentry from '../helpers/Sentry';
-import { not } from "../app/fpUtils";
 
 const {capture, configure, queue} = createCaptureContext({
   initialized: false,
