674cbbb227fb5ea517cff26a0595130ccca37d40
Beth Shook <es66@rice.edu>
2021-09-10 09:44:11 -0500

    Restore idUtils file

--- src/app/content/utils/idUtils.ts
+++ src/app/content/utils/idUtils.ts
@@ -1,4 +1,2 @@
-export const stripArchiveVersion = (id: string): string => id.split('__')[1];
-export const stripIdVersion = (id: string): string =>
-    id.includes('__') ? stripArchiveVersion(id).split('@')[0] : id.split('@')[0];
+export const stripIdVersion = (id: string): string => id.split('@')[0];
 export const getIdVersion = (id: string): string | undefined => id.split('@')[1];
