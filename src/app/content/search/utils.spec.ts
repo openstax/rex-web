5275ddc9eb9a9fc15efb5d200341c937785606e3
Beth Shook <es66@rice.edu>
2021-10-20 19:56:10 -0500

    Add test for filterTreeForHits util

--- src/app/content/search/utils.spec.ts
+++ src/app/content/search/utils.spec.ts
@@ -9,7 +9,13 @@ import { makeSearchResultHit, makeSearchResults } from '../../../test/searchResu
 import { treeWithoutUnits, treeWithUnits } from '../../../test/trees';
 import { assertDocument } from '../../utils';
 import { ArchivePage, LinkedArchiveTree } from '../types';
-import { generateKeyTermExcerpt, getFirstResult, getFormattedSearchResults, highlightResults } from './utils';
+import {
+  filterTreeForHits,
+  generateKeyTermExcerpt,
+  getFirstResult,
+  getFormattedSearchResults,
+  highlightResults
+} from './utils';
 
 jest.mock('@openstax/highlighter/dist/Highlight', () => ({
   default: class {
@@ -43,6 +49,25 @@ describe('getFirstResult', () => {
     }
 
     expect(result.result.source.pageId).toEqual(treeWithoutUnits.contents[1].contents![0].id);
+  });
+});
+
+describe('filterTreeForHits', () => {
+
+  it('works with treeWithoutUnits', () => {
+    const chapterHit = makeSearchResultHit({
+      book: {...mockArchive.book, tree: treeWithoutUnits},
+      page: treeWithoutUnits.contents[2] as unknown as ArchivePage,
+    });
+    const searchResults: SearchResult = makeSearchResults([
+      chapterHit,
+    ]);
+    expect(filterTreeForHits(treeWithoutUnits, searchResults)[0].source).toEqual({
+      elementId: 'fs-id1544727',
+      elementType: 'paragraph',
+      pageId: treeWithoutUnits.contents[2].id,
+      pagePosition: 60,
+    });
   });
 });
 
