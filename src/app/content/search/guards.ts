45da74664563f3767c91dbaf41926284ded51a13
Beth Shook <es66@rice.edu>
2021-10-25 17:14:33 -0500

    Remove unneeded guard

--- src/app/content/search/guards.ts
+++ src/app/content/search/guards.ts
@@ -1,14 +1,10 @@
 import { SearchResultHit, SearchResultHitSourceElementTypeEnum } from '@openstax/open-search-client';
 import { ScrollTarget } from '../../navigation/types';
 import { isArchiveTree } from '../guards';
-import { ArchiveTree, ArchiveTreeNode } from '../types';
 import { KeyTermHit, SearchResultChapter, SearchResultContainer, SearchResultPage, SearchScrollTarget } from './types';
 
 export const isSearchResultChapter = (container: SearchResultContainer): container is SearchResultChapter =>
   isArchiveTree(container);
-
-export const isSearchResultPage = (section: ArchiveTreeNode | ArchiveTree): section is SearchResultPage =>
-  (section as SearchResultPage).results !== undefined;
 
 export const isSearchScrollTarget = (target: ScrollTarget): target is SearchScrollTarget => {
   return target.type === 'search' && typeof target.index === 'number';
