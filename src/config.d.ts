1ae6c3a4e8e8e535d8e956146db59266a167d5b1
tom <tom.g.woodward@gmail.com>
2021-10-29 09:13:02 -0400

    necessary types

--- src/config.d.ts
+++ src/config.d.ts
@@ -3,6 +3,10 @@ interface Config {
   SENTRY_ENABLED: boolean;
   REACT_APP_ACCOUNTS_URL: string;
   ARCHIVE_URL: string;
+  ACCOUNTS_URL: string;
+  HIGHLIGHTS_URL: string;
+  SEARCH_URL: string;
+  OS_WEB_URL: string;
   UNLIMITED_CONTENT: boolean;
   REACT_APP_BUY_PRINT_CONFIG_URL: string;
   REACT_APP_ARCHIVE: string;
