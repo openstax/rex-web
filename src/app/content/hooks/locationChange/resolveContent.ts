26593c8ce3c8581dfb5c47ac0420cf1090138880
Beth Shook <es66@rice.edu>
2021-11-30 15:02:43 -0600

    Remove log

--- src/app/content/hooks/locationChange/resolveContent.ts
+++ src/app/content/hooks/locationChange/resolveContent.ts
@@ -250,7 +250,6 @@ export const loadContentReference = async(
 
 const loadContentReferences = (services: AppServices & MiddlewareAPI, book: Book) => async(page: ArchivePage) => {
   const contentReferences = getContentPageReferences(book, page);
-  console.log('refs: ', contentReferences);
   const references: Array<PageReferenceMap | PageReferenceError> = [];
   for (const reference of contentReferences) {
     references.push(await loadContentReference(services, book, page, reference));
