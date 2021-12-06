429c7a9bba66b89017647c11ea14ee60e02326d5
philschatz <253202+philschatz@users.noreply.github.com>
2021-12-06 17:59:00 -0600

    :shirt: lint

--- script/transform-approved-books-data.ts
+++ script/transform-approved-books-data.ts
@@ -19,180 +19,181 @@ const isBookData = (something: any): something is BookData => {
   return typeof something.uuid === 'string' && typeof something.slug === 'string';
 };
 
-interface ApprovedBooksData_v1 {
+interface ApprovedBooksDataVer1 {
   style: string;
   tutor_only: boolean;
   books: BookData[];
 }
 
-const isApprovedBooksData_v1 = (something: any): something is ApprovedBooksData_v1 => {
+const isApprovedBooksDataVer1 = (something: any): something is ApprovedBooksDataVer1 => {
   return typeof something.style === 'string'
     && typeof something.tutor_only === 'boolean'
     && Array.isArray(something.books)
     && something.books.every(isBookData);
 };
 
-interface ApprovedCollection_v1 extends ApprovedBooksData_v1 {
+interface ApprovedCollectionVer1 extends ApprovedBooksDataVer1 {
   collection_id: string;
   server: string;
 }
 
-const isApprovedCollection_v1 = (something: any): something is ApprovedCollection_v1 => {
+const isApprovedCollectionVer1 = (something: any): something is ApprovedCollectionVer1 => {
   return typeof something.collection_id === 'string'
     && typeof something.server === 'string'
-    && isApprovedBooksData_v1(something);
+    && isApprovedBooksDataVer1(something);
 };
 
-interface ApprovedRepo_v1 extends ApprovedBooksData_v1 {
+interface ApprovedRepoVer1 extends ApprovedBooksDataVer1 {
   repository_name: string;
 }
 
-const isApprovedRepo_v1 = (something: any): something is ApprovedRepo_v1 => {
-  return typeof something.repository_name === 'string' && isApprovedBooksData_v1(something);
+const isApprovedRepoVer1 = (something: any): something is ApprovedRepoVer1 => {
+  return typeof something.repository_name === 'string' && isApprovedBooksDataVer1(something);
 };
 
-interface ApprovedVersion_v1 {
+interface ApprovedVersionVer1 {
   content_version: string;
   min_code_version: string;
 }
 
-const isApprovedVersion_v1 = (something: any): something is ApprovedVersion_v1 => {
+const isApprovedVersionVer1 = (something: any): something is ApprovedVersionVer1 => {
   return typeof something.content_version === 'string' && typeof something.min_code_version === 'string';
 };
 
-interface ApprovedVersionCollection_v1 extends ApprovedVersion_v1 {
+interface ApprovedVersionCollectionVer1 extends ApprovedVersionVer1 {
   collection_id: string;
 }
 
-const isApprovedVersionCollection_v1 = (something: any): something is ApprovedVersionCollection_v1 => {
-  return typeof something.collection_id === 'string' && isApprovedVersion_v1(something);
+const isApprovedVersionCollectionVer1 = (something: any): something is ApprovedVersionCollectionVer1 => {
+  return typeof something.collection_id === 'string' && isApprovedVersionVer1(something);
 };
 
-interface ApprovedVersionRepo_v1 extends ApprovedVersion_v1 {
+interface ApprovedVersionRepoVer1 extends ApprovedVersionVer1 {
   repository_name: string;
 }
 
-const isApprovedVersionRepo_v1 = (something: any): something is ApprovedVersionRepo_v1 => {
-  return typeof something.repository_name === 'string' && isApprovedVersion_v1(something);
+const isApprovedVersionRepoVer1 = (something: any): something is ApprovedVersionRepoVer1 => {
+  return typeof something.repository_name === 'string' && isApprovedVersionVer1(something);
 };
 
-interface ApprovedBooksAndVersions_v1 {
-  approved_books: Array<ApprovedCollection_v1 | ApprovedRepo_v1>;
-  approved_versions: Array<ApprovedVersionCollection_v1 | ApprovedVersionRepo_v1>;
+interface ApprovedBooksAndVersionsVer1 {
+  approved_books: Array<ApprovedCollectionVer1 | ApprovedRepoVer1>;
+  approved_versions: Array<ApprovedVersionCollectionVer1 | ApprovedVersionRepoVer1>;
 }
 
-const isApprovedBooksAndVersions_v1 = (something: any): something is ApprovedBooksAndVersions_v1 => {
+const isApprovedBooksAndVersionsVer1 = (something: any): something is ApprovedBooksAndVersionsVer1 => {
   return Array.isArray(something.approved_books)
     && something.approved_books.every(
-      (element: any) => isApprovedCollection_v1(element) || isApprovedRepo_v1(element))
+      (element: any) => isApprovedCollectionVer1(element) || isApprovedRepoVer1(element))
     && something.approved_versions.every(
-      (element: any) => isApprovedVersionCollection_v1(element) || isApprovedVersionRepo_v1(element));
+      (element: any) => isApprovedVersionCollectionVer1(element) || isApprovedVersionRepoVer1(element));
 };
 
-enum ApprovedPlatformKind_v2 {
+enum ApprovedPlatformKindVer2 {
   REX = 'REX',
-  Tutor = 'TUTOR'
+  Tutor = 'TUTOR',
 }
 
-type ApprovedPlatform_v2 = ApprovedPlatformKind_v2.REX | ApprovedPlatformKind_v2.Tutor
+type ApprovedPlatformVer2 = ApprovedPlatformKindVer2.REX | ApprovedPlatformKindVer2.Tutor;
 
-const isApprovedPlatform_v2 = (str: string) => {
-  return str === ApprovedPlatformKind_v2.REX || str === ApprovedPlatformKind_v2.Tutor
-}
+const isApprovedPlatformVer2 = (str: string) => {
+  return str === ApprovedPlatformKindVer2.REX || str === ApprovedPlatformKindVer2.Tutor;
+};
 
-interface ApprovedRepo_v2 {
+interface ApprovedRepoVer2 {
   repository_name: string;
-  platforms: Array<ApprovedPlatform_v2>;
-  versions: Array<ApprovedRepoVersion_v2>;
+  platforms: ApprovedPlatformVer2[];
+  versions: ApprovedRepoVersionVer2[];
 }
 
-const isApprovedRepo_v2 = (something: any): something is ApprovedRepo_v2 => {
+const isApprovedRepoVer2 = (something: any): something is ApprovedRepoVer2 => {
   return typeof something.repository_name === 'string'
     && Array.isArray(something.platforms)
     && something.platforms.every(
-      (element: any) => isApprovedPlatform_v2(element)
+      (element: any) => isApprovedPlatformVer2(element)
     )
     && Array.isArray(something.versions)
     && something.versions.every(
-      (element: any) => isApprovedRepoVersion_v2(element)
-    )
-}
-interface ApprovedRepoVersion_v2 {
-  min_code_version: string, // TODO: Since everyone is treating this as a number should this just become a number? Or maybe v3?
-  edition: number,
-  commit_sha: string,
+      (element: any) => isApprovedRepoVersionVer2(element)
+    );
+};
+interface ApprovedRepoVersionVer2 {
+  min_code_version: string; // TODO: Since everyone is treating this as a number should this just become a number?
+  edition: number;
+  commit_sha: string;
   commit_metadata: {
     committed_at: string,
-    books: Array<ApprovedRepoVersionBook_v2>
-  }
+    books: ApprovedRepoVersionBookVer2[]
+  };
 }
 
-const isApprovedRepoVersion_v2 = (something: any): something is ApprovedRepoVersion_v2 => {
-  return typeof something.min_code_version === 'string' 
+const isApprovedRepoVersionVer2 = (something: any): something is ApprovedRepoVersionVer2 => {
+  return typeof something.min_code_version === 'string'
     && typeof something.edition === 'number'
     && typeof something.commit_sha === 'string'
     && typeof something.commit_metadata === 'object'
     && typeof something.commit_metadata.committed_at === 'string'
     && Array.isArray(something.commit_metadata.books)
     && something.commit_metadata.books.every(
-      (element: any) => isApprovedRepoVersionBook_v2(element)
-    )
-}
+      (element: any) => isApprovedRepoVersionBookVer2(element)
+    );
+};
 
-interface ApprovedRepoVersionBook_v2 {
+interface ApprovedRepoVersionBookVer2 {
   style: string;
   uuid: string;
   slug: string;
 }
 
-const isApprovedRepoVersionBook_v2 = (something: any): something is ApprovedRepoVersionBook_v2 => {
+const isApprovedRepoVersionBookVer2 = (something: any): something is ApprovedRepoVersionBookVer2 => {
   return typeof something.style === 'string'
     && typeof something.uuid === 'string'
-    && typeof something.slug === 'string'
-}
+    && typeof something.slug === 'string';
+};
 
-interface ApprovedBooksAndVersions_v2 {
+interface ApprovedBooksAndVersionsVer2 {
   version: 2;
-  approved_books: Array<ApprovedCollection_v1 | ApprovedRepo_v2>;
-  approved_versions: Array<ApprovedVersionCollection_v1>;
+  approved_books: Array<ApprovedCollectionVer1 | ApprovedRepoVer2>;
+  approved_versions: ApprovedVersionCollectionVer1[];
 }
 
-const isApprovedBooksAndVersions_v2 = (something: any): something is ApprovedBooksAndVersions_v2 => {
+const isApprovedBooksAndVersionsVer2 = (something: any): something is ApprovedBooksAndVersionsVer2 => {
   return something.version === 2
     && Array.isArray(something.approved_books)
     && something.approved_books.every(
-      (element: any) => isApprovedRepo_v2(element) || isApprovedCollection_v1(element)
+      (element: any) => isApprovedRepoVer2(element) || isApprovedCollectionVer1(element)
     )
     && Array.isArray(something.approved_versions)
     && something.approved_versions.every(
-      (element: any) => isApprovedVersion_v1(element)
-    )
-}
-
-const matchRepoVersion_v1 = (repoData: ApprovedRepo_v1, archiveVersion?: string) =>
-  (versionData: ApprovedVersionCollection_v1 | ApprovedVersionRepo_v1): versionData is ApprovedVersionRepo_v1 => {
-  if (archiveVersion && versionData.min_code_version > archiveVersion) { return false; }
-  return isApprovedVersionRepo_v1(versionData) && versionData.repository_name === repoData.repository_name;
+      (element: any) => isApprovedVersionVer1(element)
+    );
 };
 
-const matchCollectionVersion_v1 = (collectionData: ApprovedCollection_v1, archiveVersion?: string) =>
-  (versionData: ApprovedVersionCollection_v1 | ApprovedVersionRepo_v1): versionData is ApprovedVersionCollection_v1 => {
+const matchRepoVersionVer1 = (repoData: ApprovedRepoVer1) =>
+  (versionData: ApprovedVersionCollectionVer1 | ApprovedVersionRepoVer1): versionData is ApprovedVersionRepoVer1 => {
   if (archiveVersion && versionData.min_code_version > archiveVersion) { return false; }
-  return isApprovedVersionCollection_v1(versionData) && versionData.collection_id === collectionData.collection_id;
+  return isApprovedVersionRepoVer1(versionData) && versionData.repository_name === repoData.repository_name;
 };
 
-const getDesiredVersion_v1 = (
-  approvedVersions: Array<ApprovedVersionCollection_v1 | ApprovedVersionRepo_v1>,
-  colOrRepo: ApprovedRepo_v1 | ApprovedCollection_v1
+const matchCollectionVersionVer1 = (collectionData: ApprovedCollectionVer1) =>
+  (versionData: ApprovedVersionCollectionVer1 | ApprovedVersionRepoVer1):
+  versionData is ApprovedVersionCollectionVer1 => {
+  if (archiveVersion && versionData.min_code_version > archiveVersion) { return false; }
+  return isApprovedVersionCollectionVer1(versionData) && versionData.collection_id === collectionData.collection_id;
+};
+
+const getDesiredVersionVer1 = (
+  approvedVersions: Array<ApprovedVersionCollectionVer1 | ApprovedVersionRepoVer1>,
+  colOrRepo: ApprovedRepoVer1 | ApprovedCollectionVer1
 ): string | undefined => {
-  const [filter, transformVersion, sortFunction] = isApprovedCollection_v1(colOrRepo)
+  const [filter, transformVersion, sortFunction] = isApprovedCollectionVer1(colOrRepo)
     ? [
-      matchCollectionVersion_v1(colOrRepo, archiveVersion),
+      matchCollectionVersionVer1(colOrRepo),
       (version: string) => version.slice(2),
       semverSort.desc,
     ]
     : [
-      matchRepoVersion_v1(colOrRepo, archiveVersion),
+      matchRepoVersionVer1(colOrRepo),
       (version: string) => version,
       (array: string[]) => array.sort().reverse(),
     ];
@@ -212,7 +213,7 @@ const getDesiredVersion_v1 = (
   return transformed[0];
 };
 
-const transformData_v1 = (data: ApprovedBooksAndVersions_v1): { [key: string]: string } => {
+const transformDataVer1 = (data: ApprovedBooksAndVersionsVer1): { [key: string]: string } => {
   const results: { [key: string]: string } = {};
 
   const { approved_books, approved_versions } = data;
@@ -220,13 +221,13 @@ const transformData_v1 = (data: ApprovedBooksAndVersions_v1): { [key: string]: s
   for (const collectionOrRepo of approved_books) {
     if (
       collectionOrRepo.tutor_only
-      || (isApprovedCollection_v1(collectionOrRepo) && collectionOrRepo.server !== 'cnx.org')
+      || (isApprovedCollectionVer1(collectionOrRepo) && collectionOrRepo.server !== 'cnx.org')
     ) {
       continue;
     }
 
     const books = collectionOrRepo.books;
-    const desiredVersion = getDesiredVersion_v1(approved_versions, collectionOrRepo);
+    const desiredVersion = getDesiredVersionVer1(approved_versions, collectionOrRepo);
 
     if (!desiredVersion) {
       // Skip if we couldn't find the version matching currently used archive version
@@ -241,40 +242,43 @@ const transformData_v1 = (data: ApprovedBooksAndVersions_v1): { [key: string]: s
   }
 
   return results;
-}
+};
 
-const transformData_v2 = (data: ApprovedBooksAndVersions_v2) => {
+const transformDataVer2 = (data: ApprovedBooksAndVersionsVer2) => {
   // get all the v1 archive entries
-  const v1Data: ApprovedBooksAndVersions_v1 = {
-    approved_books: data.approved_books.filter(b => isApprovedCollection_v1(b)) as ApprovedCollection_v1[],
-    approved_versions: data.approved_versions
-  }
-  const results = transformData_v1(v1Data)
+  const v1Data: ApprovedBooksAndVersionsVer1 = {
+    approved_books: data.approved_books.filter((b) => isApprovedCollectionVer1(b)) as ApprovedCollectionVer1[],
+    approved_versions: data.approved_versions,
+  };
+  const results = transformDataVer1(v1Data);
 
   // Add the v2 git entries
-  const approvedRepos = data.approved_books.filter(b => isApprovedRepo_v2(b)) as ApprovedRepo_v2[]
+  const approvedRepos = data.approved_books.filter((b) => isApprovedRepoVer2(b)) as ApprovedRepoVer2[];
   for (const repo of approvedRepos) {
-    if (!repo.platforms.includes(ApprovedPlatformKind_v2.REX)) {
-      continue
+    if (!repo.platforms.includes(ApprovedPlatformKindVer2.REX)) {
+      continue;
     }
-    const versions = repo.versions.sort((a, b) => Date.parse(a.commit_metadata.committed_at) - Date.parse(b.commit_metadata.committed_at))
+    const versions = repo.versions.sort(
+      (a, b) => Date.parse(a.commit_metadata.committed_at) - Date.parse(b.commit_metadata.committed_at)
+    );
 
     for (const version of versions) {
-      if (archiveVersion && version.min_code_version > archiveVersion) { continue }
+      if (archiveVersion && version.min_code_version > archiveVersion) { continue; }
 
       for (const book of version.commit_metadata.books) {
-        let sha = version.commit_sha
+        let sha = version.commit_sha;
         if (version.commit_sha.length > 7) {
-          console.warn('Truncating sha because some magic somewhere in the pipeline does it too', version.commit_sha)
-          sha = version.commit_sha.substr(0, 7)
+          // tslint:disable-next-line:no-console
+          console.warn('Truncating sha because some magic somewhere in the pipeline does it too', version.commit_sha);
+          sha = version.commit_sha.substr(0, 7);
         }
-        results[book.uuid] = sha
+        results[book.uuid] = sha;
       }
     }
   }
 
-  return results
-}
+  return results;
+};
 
 const transformData = () => {
   const { data } = argv as { data?: string };
@@ -283,14 +287,14 @@ const transformData = () => {
     throw new Error('data argument is missing');
   }
 
-  let parsed: ApprovedBooksAndVersions_v1;
+  let parsed: ApprovedBooksAndVersionsVer1;
   let results;
   try {
     parsed = JSON.parse(data);
-    if (isApprovedBooksAndVersions_v2(parsed)) {
-      results = transformData_v2(parsed)
-    } else if (isApprovedBooksAndVersions_v1(parsed)) {
-      results = transformData_v1(parsed)
+    if (isApprovedBooksAndVersionsVer2(parsed)) {
+      results = transformDataVer2(parsed);
+    } else if (isApprovedBooksAndVersionsVer1(parsed)) {
+      results = transformDataVer1(parsed);
     } else {
       throw new Error('Data is valid JSON, but has wrong structure. See ApprovedBooksAndVersions interface.');
     }
@@ -301,7 +305,7 @@ const transformData = () => {
   // this script is used in the update-content/script.bash
   // so we write results to the terminal to assign them to a variable
   process.stdout.write(JSON.stringify(results));
-  return results
+  return results;
 };
 
 transformData();
