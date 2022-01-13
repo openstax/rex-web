54ee2dd0ef5691d3fb1b80a9bc6aa02004c2bdfe
Dante Soares <dante.m.soares@rice.edu>
2022-01-13 10:40:01 -0300

    Readded concourse/prerender-workers

---
+++ concourse/prerender-workers/script.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -eux
+
+set -o pipefail
+
+destination=$(pwd)/release
+
+# shellcheck disable=SC1091
+source build-configs/config.env
+# shellcheck disable=SC2046
+export $(cut -d= -f1 build-configs/config.env)
+
+cd rex-web
+
+yarn install
+yarn build:clean
+yarn prerender:manage
+
+cp -r build/* "$destination"
