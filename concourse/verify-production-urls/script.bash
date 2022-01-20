0398d044ac2436a2db67234478eb6870b35e29ae
Dante Soares <dante.m.soares@rice.edu>
2022-01-20 10:43:10 -0300

    Changed verify-production-urls to verify only the production bucket

--- concourse/verify-production-urls/script.bash
+++ concourse/verify-production-urls/script.bash
@@ -2,63 +2,41 @@
 set -euo pipefail
 
 production_url=https://openstax.org
+current_release_id=$(curl -s "${production_url}/rex/environment.json" | jq .release_id -r)
 
 get_bucket_release_files () {
-  if [ "$1" == "production" ]; then
-    key=$PROD_AWS_ACCESS_KEY_ID
-    secret=$PROD_AWS_SECRET_ACCESS_KEY
-    bucket=$PROD_UNIFIED_S3_BUCKET
-  else
-    key=$SAND_AWS_ACCESS_KEY_ID
-    secret=$SAND_AWS_SECRET_ACCESS_KEY
-    bucket=$SAND_UNIFIED_S3_BUCKET
-  fi
-
-  AWS_ACCESS_KEY_ID=$key AWS_SECRET_ACCESS_KEY=$secret aws s3api list-objects \
-  --bucket "$bucket" --prefix "rex/releases/$2/books/" --output json | \
-  jq -r '.Contents[] | .Key' | sed -e "s/.*\/\(books\/.*\)/\1/"
+  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  aws s3api list-objects --bucket "$PROD_UNIFIED_S3_BUCKET" --prefix "rex/releases/$1/books/" \
+  --output json | jq -r '.Contents[] | .Key' | sed -e "s/.*\/\(books\/.*\)/\1/"
 }
 
 count_files () { wc -l <<< "$1" | awk '{$1=$1};1'; }
 
 # Bash array difference (with newline-separated inputs)
 # Adapted from https://stackoverflow.com/a/28161520
-# Returns files in $prod_files but not in $uploaded_files
+# Returns files in $1 but not in $2
 # Algorithm:
-# 1. Concatenate all entries, newline-separated
-# 2. Sort entries
+# 1. Concatenate all entries, one on each line
+# 2. Sort lines
 # 3. Remove all lines that appear more than once
 # The array to be removed appears twice in the input so none of its entries are added to the output
 diff_file_lists () { printf "%s\n%s\n%s\n" "$1" "$2" "$2" | sort | uniq -u; }
 
-current_prod_release_id=$(curl -s "${production_url}/rex/environment.json" | jq .release_id -r)
-current_prod_files=$(get_bucket_release_files production "$current_prod_release_id")
-current_prod_number=$(count_files "$current_prod_files")
+current_files=$(get_bucket_release_files "$current_release_id")
+current_number=$(count_files "$current_files")
 
-echo "testing $current_prod_number urls from current production release $current_prod_release_id"
+echo "testing $current_number urls from current release $current_release_id"
 
-new_release_prod_files=$(get_bucket_release_files production "$REACT_APP_RELEASE_ID")
-new_release_prod_number=$(count_files "$new_release_prod_files")
+new_release_files=$(get_bucket_release_files "$REACT_APP_RELEASE_ID")
+new_release_number=$(count_files "$new_release_files")
 
-echo "found $new_release_prod_number production urls in new release $REACT_APP_RELEASE_ID"
+echo "found $new_release_number urls in new release $REACT_APP_RELEASE_ID"
 
-missing_prod_files=$(diff_file_lists "$current_prod_files" "$new_release_prod_files")
+missing_files=$(diff_file_lists "$current_files" "$new_release_files")
 
-if [ -n "$missing_prod_files" ]; then
-  printf "MISSING PRODUCTION PATHS:\n%s\n" "$missing_prod_files"
+if [ -n "$missing_files" ]; then
+  printf "MISSING PATHS:\n%s\n" "$missing_files"
   exit 1
 fi
 
-new_release_sand_files=$(get_bucket_release_files sandbox "$REACT_APP_RELEASE_ID")
-new_release_sand_number=$(count_files "$new_release_sand_files")
-
-echo "found $new_release_sand_number sandbox urls in new release $REACT_APP_RELEASE_ID"
-
-missing_sand_files=$(diff_file_lists "$current_prod_files" "$new_release_sand_files")
-
-if [ -n "$missing_sand_files" ]; then
-  printf "MISSING SANDBOX PATHS:\n%s\n" "$missing_sand_files"
-  exit 1
-fi
-
-echo 'all current production release files also present in the new release'
+echo 'all current release files also present in the new release'
