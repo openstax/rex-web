87309e281b820b0c9b8bebb1e26be80b22ca734e
Beth Shook <es66@rice.edu>
2022-01-24 15:35:28 -0600

    Spec fixes and remove unneeded condition for CenteredContentRow padding

--- src/app/content/components/CenteredContentRow.tsx
+++ src/app/content/components/CenteredContentRow.tsx
@@ -2,23 +2,17 @@ import React from 'react';
 import styled, { css } from 'styled-components/macro';
 import theme from '../../theme';
 import { State } from '../types';
-import { sidebarDesktopWithToolbarWidth } from './constants';
 
 // tslint:disable-next-line: variable-name
-const Wrapper = styled.div<{isTocOpen: State['tocOpen'], isSearchOpen: boolean}>`
+const Wrapper = styled.div<{verticalNavOpen: State['tocOpen']}>`
   grid-column: 1 / -1;
   grid-row: 1;
   justify-self: center;
   width: 100%;
   overflow: visible; /* so sidebar position: sticky works */
   margin: 0 auto;
-  ${(props) => theme.breakpoints.desktopMedium(css`
-    padding-left: ${(props.isVerticalNavOpen || props.isVerticalNavOpen ===  null)
-      ? sidebarDesktopWithToolbarWidth + 'rem'
-      : 0};
-  `)}
+  padding-left: 0;
   ${theme.breakpoints.mobile(css`
-    padding-left: 0;
     grid-column-start: 2;
   `)}
   ${theme.breakpoints.mobileMedium(css`
