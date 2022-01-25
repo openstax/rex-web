48d32a5fe591b206d22a8d1a47640a8109798681
Beth Shook <es66@rice.edu>
2022-01-25 14:16:34 -0600

    remove redirects logic from content versions script

--- script/update-content-versions-and-check-for-archived-slugs.ts
+++ script/update-content-versions-and-check-for-archived-slugs.ts
@@ -5,7 +5,6 @@ import { ARCHIVE_URL, REACT_APP_ARCHIVE_URL, REACT_APP_OS_WEB_API_URL } from '..
 import books from '../src/config.books';
 import createArchiveLoader from '../src/gateways/createArchiveLoader';
 import createOSWebLoader from '../src/gateways/createOSWebLoader';
-import updateRedirectsData from './utils/update-redirects-data';
 
 export interface SimpleBook {
   bookId: string;
@@ -21,28 +20,6 @@ const bookLoader = (newArchiveUrl?: string) => {
   }),
   createOSWebLoader(`${ARCHIVE_URL}${REACT_APP_OS_WEB_API_URL}`)
 ); };
-
-async function updateRedirections(bookId: string, currentVersion: string, newVersion: string, newArchive?: string) {
-  if (currentVersion === newVersion) {
-    return 0;
-  }
-
-  const currentBook = await bookLoader()(bookId, currentVersion)
-    .catch((error) => {
-      // tslint:disable-next-line: no-console
-      console.log(`error while loading book ${bookId} with defaultVersion ${currentVersion}`);
-      throw error;
-    });
-
-  const newBook = await bookLoader(newArchive)(bookId, newVersion)
-    .catch((error) => {
-      // tslint:disable-next-line: no-console
-      console.log(`error while loading book ${bookId} with newVersion ${newVersion}`);
-      throw error;
-    });
-
-  return updateRedirectsData(currentBook, newBook);
-}
 
 async function processBook(book: SimpleBook, newArchive?: string) {
   const {bookId, versionNumber} = book;
@@ -67,14 +44,8 @@ async function processBook(book: SimpleBook, newArchive?: string) {
 
   fs.writeFileSync(booksPath, JSON.stringify(updatedBooksConfig, undefined, 2) + '\n', 'utf8');
 
-  // defaultVersion will be undefined when we add a new book.
-  // In this case we don't need to updateRedirections because there is nothing to update.
-  const newRedirectionsCounter = defaultVersion
-    ? await updateRedirections(bookId, defaultVersion, newVersion, newArchive)
-    : 0;
-
   // tslint:disable-next-line: no-console
-  console.log(`updated ${title} and added ${newRedirectionsCounter} new redirections`);
+  console.log(`updated ${title}`);
 }
 
 export default processBook;
