8d9351b3603ba5f9b9532795a39bfc91a7820143
Beth Shook <es66@rice.edu>
2022-02-10 17:24:26 -0600

    Add comment

--- src/app/navigation/hooks/closeModalHook.ts
+++ src/app/navigation/hooks/closeModalHook.ts
@@ -1,6 +1,7 @@
 import { AppServices, MiddlewareAPI } from '../../types';
 
 export const closeModal = (services: MiddlewareAPI & AppServices) => () => {
+  // this is undefined if previous record is not on rex
   if (services.history.location.state === undefined) {
     services.history.replace({
       search: '',
