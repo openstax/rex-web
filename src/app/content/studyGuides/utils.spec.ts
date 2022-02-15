656b12aa788922bda83284b34176429ad1b41626
Piotr Kozłowski <piotr@obrandowani.pl>
2022-02-15 19:07:46 +0100

    1343 Store color filters for SG in the URL (#1208)
    * Make change to allow git push
    
    * Revert change
    
    * Add colors and locationIds to persistentQueryParms
    
    * Add color/chapter filters to persistent query params selector
    
    * Remove location change  action logic from study guide modal reducer
    
    * Revert reducer change
    
    * remove log
    
    * Remove location change action conditions from modal logic
    
    * remove color and locationIds from persistent query params
    
    * add logs
    
    * Remove logs
    
    * Lint fix
    
    * Remove logs and create action hook to call loadmore on receiveSGTotalCounts
    
    * Lint fix
    
    * Lint fix
    
    * Revert change
    
    * revert change
    
    * Remove unneeded code
    
    * Remove unused variable
    
    * Fix modal rerender issue
    
    * Dispatch loadMoreStudyGuides from locationChange
    
    * Remove comment
    
    * Remove whitespace
    
    * Replace book/page check with summaryIsOpen check in loadMore
    
    * remove setSummaryFilters action from reducer
    
    * Make loadMore hook listen to receiveStudyGuidesCounts action so it loads study guides in case of modal reload
    
    * Update tests so study guides modal evaluates as open
    
    * Update test so study guides modal evaluates as open
    
    * Remove setSummaryFilters from reducer
    
    * Restore deleted whitespace
    
    * lint fix
    
    * Stop hooking loadMore into setFilters action
    
    * Remove unneeded action
    
    * Remove setSummaryFilters action from actions.ts
    
    * Replace updateQuery hook with helper function and remove filters from default sg modal query
    
    * Remove unused hook
    
    * Fix summary filters selector to match old openStudyGuides logic
    
    * Refactor openStudyGuides
    
    * Fix firstChapter selector
    
    * Lint fix and update sg selectors
    
    * Clean up SG filter selectors
    
    * Refactor default filter selectors
    
    * Remove commented code
    
    * Debugging openStudyGuides
    
    * Debugging study guides first loading via url params
    
    * Reverse order of selectors
    
    * Better name for selector argument
    
    * Store history stack depth in history state object
    
    * RAdd conditional logic to close modal hook
    
    * Update closeModal, selectors, and reducer tests
    
    * Fixing tests
    
    * update closemodalhook and test
    
    * Update SG and Filters tests
    
    * Update Filters test
    
    * Lint fix
    
    * Fixing tests
    
    * Update openModalHook
    
    * Update openStudyGuide tests
    
    * Lint fix
    
    * Debugging loadMore hook tests
    
    * Fix typo in test
    
    * Remove logs
    
    * fix remove filter test
    
    * Add logs for debugging
    
    * Comment failing expect
    
    * lint fix
    
    * fix typo
    
    * fix typo
    
    * fix typo
    
    * lint fix
    
    * Fix bug overriding SG filters from state
    
    * Update query logic for open SGs hook
    
    * Correct openStudyGuides hook conditional
    
    * log ordered SGs
    
    * Fix loadMore test
    
    * Remove whitespace
    
    * Update Filter test
    
    * Fix code warnings
    
    * Add studyGuides utils test for coverage
    
    * Add showStudyGuides test for coverage
    
    * Update studyguides test for coverage
    
    * lint fix
    
    * lint fix
    
    * revert unnecessary change
    
    * revert unnecessary change
    
    * revert unneeded changes for PR
    
    * cleanup for PR
    
    * Remove comment
    
    * Remove unneeded wrapper from closeModal hook
    
    * Add comment
    
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>
    Co-authored-by: Beth Shook <es66@rice.edu>
    Co-authored-by: tom <tom.g.woodward@gmail.com>

---
+++ src/app/content/studyGuides/utils.spec.ts
@@ -0,0 +1,38 @@
+import { HighlightColorEnum } from '@openstax/highlights-client';
+import createTestStore from '../../../test/createTestStore';
+import { Store } from '../../types';
+import { getFiltersFromQuery, updateQueryFromFilterChange } from './utils';
+
+describe('utils', () => {
+
+  describe('getFiltersFromQuery', () => {
+    it('get multiple colors and locationIds from the query', () => {
+      const output = getFiltersFromQuery({ colors: ['green', 'yellow', 'akljwef'], locationIds: ['1', '2'] });
+      expect(output).toEqual(
+        { colors: [HighlightColorEnum.Green, HighlightColorEnum.Yellow], locationIds: ['1', '2'] }
+      );
+    });
+
+    it('get single color and locationId from the query', () => {
+      const output = getFiltersFromQuery({ colors: ['green'], locationIds: ['1'] });
+      expect(output).toEqual({ colors: [HighlightColorEnum.Green], locationIds: ['1'] });
+    });
+  });
+
+  describe('updateQueryFromFilterChange', () => {
+    let store: Store;
+    let dispatch: jest.SpyInstance;
+
+    beforeEach(() => {
+      store = createTestStore();
+      dispatch = jest.spyOn(store, 'dispatch');
+    });
+
+    it('noops if match wansn\'t in the navigation state', () => {
+      const state = store.getState();
+      updateQueryFromFilterChange(store.dispatch, state, { colors: {new: [], remove: [HighlightColorEnum.Green]} });
+
+      expect(dispatch).not.toHaveBeenCalled();
+    });
+  });
+});
