26bdaaa12a3a2fce00f9c173dad3427ade63db25
Josiah Ivey <josiah.ivey@gmail.com>
2022-02-23 15:03:36 -0800

    fix lint

--- src/helpers/PromiseCollector.spec.ts
+++ src/helpers/PromiseCollector.spec.ts
@@ -59,11 +59,11 @@ describe('PromiseCollector', () => {
     });
   });
 
-  it('swallows errors when removing promises with .finally', async () => {
+  it('swallows errors when removing promises with .finally', async() => {
     // The "fail" case of this test is with a process exit code
     // of ERR_UNHANDLED_REJECTION, not a typical jest fail
-    await expect(async () => {
-      const p1 = new Promise(() => { throw new Error('error') })
+    await expect(async() => {
+      const p1 = new Promise(() => { throw new Error('error'); });
       collector.add(p1);
       await collector.calm();
     }).rejects.toMatchObject(Error('error'));
