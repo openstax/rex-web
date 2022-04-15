27f6aa30b0da421dbdce3e467cd6c48312d30d16
Josiah Ivey <josiah.ivey@gmail.com>
2022-04-15 10:04:27 -0700

    add spec for version query param

---
+++ src/helpers/mathjax.spec.ts
@@ -0,0 +1,48 @@
+import { assertDocument, assertWindow } from '../app/utils';
+import { startMathJax, typesetMath } from './mathjax';
+import { startMathJax as startMathJax2, typesetMath as typesetMath2 } from './mathjax/v2';
+import { startMathJax as startMathJax3, typesetMath as typesetMath3 } from './mathjax/v3';
+
+jest.mock('./mathjax/v2', () => ({
+  startMathJax: jest.fn(),
+  typesetMath: jest.fn(),
+}));
+
+jest.mock('./mathjax/v3', () => ({
+  startMathJax: jest.fn(),
+  typesetMath: jest.fn(),
+}));
+
+describe('v2', () => {
+  it('starts to the correct version', () => {
+    startMathJax();
+    expect(startMathJax2).toHaveBeenCalled();
+  });
+
+  it('calls the correct version', () => {
+    const element = assertDocument().createElement('div');
+    typesetMath(element);
+    expect(typesetMath2).toHaveBeenCalled();
+  });
+});
+
+describe('v3', () => {
+  beforeEach(() => {
+    Object.defineProperty(assertWindow(), 'location', {
+      value: {
+        search: 'mathjax-version=3',
+      }
+    });
+  });
+
+  it('starts the correct version', () => {
+    startMathJax();
+    expect(startMathJax3).toHaveBeenCalled();
+  });
+
+  it('calls the correct version', () => {
+    const element = assertDocument().createElement('div');
+    typesetMath(element);
+    expect(typesetMath3).toHaveBeenCalled();
+  });
+});
--- src/helpers/mathjax.ts
+++ src/helpers/mathjax.ts
@@ -3,26 +3,21 @@ import { assertWindow } from '../app/utils';
 import { startMathJax as startMathJax2, typesetMath as typesetMath2 } from './mathjax/v2';
 import { startMathJax as startMathJax3, typesetMath as typesetMath3 } from './mathjax/v3';
 
-interface MathJaxWindow extends Window {
-  __MJX_VERSION?: number;
+const getMathJaxQueryParam = () => {
+  const params = new URLSearchParams(assertWindow().location.search);
+  return params.get('mathjax-version');
 }
 
 export const startMathJax = () => {
-  const window: MathJaxWindow = assertWindow();
-  const params = new URLSearchParams(window.location.search);
-  const version = params.get('mathjax-version');
-
-  if (version === '3') {
-    window.__MJX_VERSION = 3;
+  if (getMathJaxQueryParam() === '3') {
     startMathJax3();
   } else {
-    window.__MJX_VERSION = 2;
     startMathJax2();
   }
 };
 
-export const typesetMath = (container: Element, windowImpl: MathJaxWindow = assertWindow()) => {
-  if (windowImpl.__MJX_VERSION === 3) {
+export const typesetMath = (container: Element, windowImpl: Window = assertWindow()) => {
+  if (getMathJaxQueryParam() === '3') {
     return typesetMath3(container, windowImpl);
   } else {
     return typesetMath2(container, windowImpl);
