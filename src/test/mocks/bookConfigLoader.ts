f9b7d3f86ef9ae5e873ec9bcca37ae20e7fcadf3
Beth Shook <es66@rice.edu>
2022-04-22 13:08:15 -0500

    update mock bookconfigloader

--- src/test/mocks/bookConfigLoader.ts
+++ src/test/mocks/bookConfigLoader.ts
@@ -2,11 +2,19 @@ import createBookConfigLoader from '../../gateways/createBookConfigLoader';
 import { book } from './archiveLoader';
 
 const localBookConfig = {
-  [book.id]: { defaultVersion: book.version },
+  [book.id]: { defaultVersion: book.version, archiveOverride: 'codeversion' },
 };
 
 export default (): ReturnType<typeof createBookConfigLoader> => {
   const resolveBookVersion = (uuid: string) => localBookConfig[uuid];
+  const resolveArchiveVersion = (uuid: string) => localBookConfig[uuid].archiveOverride;
+
+  const mockGetArchiveVersion = jest.fn((uuid: string) => {
+    const archiveVersion = resolveArchiveVersion(uuid);
+    return archiveVersion
+      ? Promise.resolve(archiveVersion)
+      : Promise.resolve(undefined);
+  });
 
   const mockGetBookVersionFromUUID = jest.fn((uuid: string) => {
     const bookVersion = resolveBookVersion(uuid);
@@ -16,6 +24,7 @@ export default (): ReturnType<typeof createBookConfigLoader> => {
   });
 
   return {
+    getArchiveVersion: (uuid: string) => mockGetArchiveVersion(uuid),
     getBookVersionFromUUID: (uuid: string) => mockGetBookVersionFromUUID(uuid),
   };
 };
