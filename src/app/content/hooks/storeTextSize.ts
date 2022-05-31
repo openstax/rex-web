7d79400e0276dd2f2102e9c59cfaaba7294f770e
Josiah Ivey <josiah.ivey@gmail.com>
2022-05-31 15:18:21 -0700

    inline window check

--- src/app/content/hooks/storeTextSize.ts
+++ src/app/content/hooks/storeTextSize.ts
@@ -4,12 +4,9 @@ import { textSize } from '../selectors';
 
 const hookBody: ActionHookBody<typeof setTextSize> = (services) => async() => {
   const { getState } = services;
-
   const state = getState();
 
-  if (window) {
-    window.localStorage.setItem('textSize', textSize(state).toString());
-  }
+  typeof window !== 'undefined' && window.localStorage.setItem('textSize', textSize(state).toString());
 };
 
 export default hookBody;
