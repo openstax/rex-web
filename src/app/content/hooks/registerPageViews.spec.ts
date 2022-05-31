e5dc09c65b712da3b23507afc52be6d309fe739e
Beth Shook <es66@rice.edu>
2022-05-31 09:37:35 -0500

    fix indent size

--- src/app/content/hooks/registerPageViews.spec.ts
+++ src/app/content/hooks/registerPageViews.spec.ts
@@ -11,69 +11,69 @@ import registerPageView from './registerPageView';
 declare const window: Window;
 
 describe('registerPageView', () => {
-    let hook: ReturnType<typeof registerPageView>;
-    let client: GoogleAnalyticsClient;
-    let mockGa: any;
-    let store: Store;
-    let helpers: ReturnType<typeof createTestServices> & MiddlewareAPI;
+  let hook: ReturnType<typeof registerPageView>;
+  let client: GoogleAnalyticsClient;
+  let mockGa: any;
+  let store: Store;
+  let helpers: ReturnType<typeof createTestServices> & MiddlewareAPI;
 
-    const action = {
-      action: 'PUSH' as 'PUSH',
-      location: {
-        ...assertWindow().location,
-        state: {},
-      },
-      match: {
-        params: {
-          book: {
-            slug: 'book-slug-1',
-          },
-          page: {
-            slug: 'test-page-1',
-          },
+  const action = {
+    action: 'PUSH' as 'PUSH',
+    location: {
+      ...assertWindow().location,
+      state: {},
+    },
+    match: {
+      params: {
+        book: {
+          slug: 'book-slug-1',
         },
-        route: routes.content,
-        state: {},
+        page: {
+          slug: 'test-page-1',
+        },
       },
-      query: {},
+      route: routes.content,
+      state: {},
+    },
+    query: {},
+  };
+
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    store = createTestStore();
+    client = googleAnalyticsClient;
+    mockGa = jest.fn<UniversalAnalytics.ga, []>();
+    window.ga = mockGa;
+    client.setTrackingIds(['foo']);
+
+    helpers = {
+      ...createTestServices(),
+      dispatch: store.dispatch,
+      getState: store.getState,
     };
 
-    beforeEach(() => {
-      jest.restoreAllMocks();
-      store = createTestStore();
-      client = googleAnalyticsClient;
-      mockGa = jest.fn<UniversalAnalytics.ga, []>();
-      window.ga = mockGa;
-      client.setTrackingIds(['foo']);
-
-      helpers = {
-        ...createTestServices(),
-        dispatch: store.dispatch,
-        getState: store.getState,
-      };
-
-      hook = registerPageView(helpers);
-    });
-
-    it('registers a page view if pathname unchanged and modal query changed', async() => {
-      jest.spyOn(selectNavigation, 'query').mockReturnValue({});
-
-      await hook(action);
-      expect(mockGa).toHaveBeenCalledWith('tfoo.send', {hitType: 'pageview', page: '/'});
-    });
-
-    it('registers a page view if pathname changed', async() => {
-      jest.spyOn(selectNavigation, 'pathname').mockReturnValue('foo');
-
-      await hook(action);
-      expect(mockGa).toHaveBeenCalledWith('tfoo.send', {hitType: 'pageview', page: 'foo'});
-    });
-
-    it('does not register a page view if pathname and modal query are unchanged', async() => {
-      await hook(action);
-      expect(mockGa).toHaveBeenCalledWith('tfoo.send', {hitType: 'pageview', page: '/'});
-      mockGa.mockReset();
-      await hook(action);
-      expect(mockGa).not.toHaveBeenCalledWith('tfoo.send', {hitType: 'pageview', page: '/'});
-    });
+    hook = registerPageView(helpers);
   });
+
+  it('registers a page view if pathname unchanged and modal query changed', async () => {
+    jest.spyOn(selectNavigation, 'query').mockReturnValue({});
+
+    await hook(action);
+    expect(mockGa).toHaveBeenCalledWith('tfoo.send', { hitType: 'pageview', page: '/' });
+  });
+
+  it('registers a page view if pathname changed', async () => {
+    jest.spyOn(selectNavigation, 'pathname').mockReturnValue('foo');
+
+    await hook(action);
+    expect(mockGa).toHaveBeenCalledWith('tfoo.send', { hitType: 'pageview', page: 'foo' });
+  });
+
+  it('does not register a page view if pathname and modal query are unchanged', async () => {
+    await hook(action);
+    expect(mockGa).toHaveBeenCalledWith('tfoo.send', { hitType: 'pageview', page: '/' });
+    mockGa.mockReset();
+    await hook(action);
+    expect(mockGa).not.toHaveBeenCalledWith('tfoo.send', { hitType: 'pageview', page: '/' });
+  });
+});
