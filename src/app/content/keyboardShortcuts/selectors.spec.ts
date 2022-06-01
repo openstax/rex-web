6a1c7d2ee196e383738ed311f487ac3ee4fd5abb
Dante Soares <dante.m.soares@rice.edu>
2022-06-01 12:45:01 -0500

    Added Keyboard shortcuts menu based on other modals (#1591)
    * Added Keyboard shortcuts menu based on other modals
    Track keyboard shortcuts menu open/close events in analytics
    Open keyboard shortcuts menu via the hidden link or keyboard shortcut
    
    * Fixed most of the styles...
    
    * Finished keyboard shortcuts menu styles
    
    * Fixed lint errors
    
    * Fixed failing specs and added a spec for opening the modal with the keyboard shortcut
    
    * Added a spec to cover opening the keyboard shortcuts menu from the hidden link
    
    * Added Spanish translations
    Use table layout in desktop mode so column sizes are consistent
    
    * Added vertical-align: middle to table-cells to center the text a little more
    
    * Added Polish translations for the Keyboard shortcuts menu
    
    * Translation changes
    
    * Fixed keyboard shortcuts overlay z-index and added a modal max-width
    
    * Updated KeyboardShortcuts snapshots
    
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>
    Co-authored-by: firadecki <97086939+firadecki@users.noreply.github.com>

---
+++ src/app/content/keyboardShortcuts/selectors.spec.ts
@@ -0,0 +1,23 @@
+import { AppState } from '../../types';
+import { modalUrlName } from './constants';
+import * as selectors from './selectors';
+
+describe('isKeyboardShortcutsOpen', () => {
+  it('returns true if it has a query with the modal url', () => {
+    const rootState = ({navigation: {query: {modal: modalUrlName}}} as any) as AppState;
+
+    expect(selectors.isKeyboardShortcutsOpen(rootState)).toBe(true);
+  });
+
+  it('returns false if it doesn\'t have a query', () => {
+    const rootState = ({ navigation: {} } as any) as AppState;
+
+    expect(selectors.isKeyboardShortcutsOpen(rootState)).toBe(false);
+  });
+
+  it('returns false if the query doesnt have "modal" url', () => {
+    const rootState = ({navigation: {query: {}}} as any) as AppState;
+
+    expect(selectors.isKeyboardShortcutsOpen(rootState)).toBe(false);
+  });
+});
