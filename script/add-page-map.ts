10596e119561f9ee41c046738540918cb4986c57
Beth Shook <es66@rice.edu>
2022-06-24 14:17:29 -0500

    add logging to script

--- script/add-page-map.ts
+++ script/add-page-map.ts
@@ -7,7 +7,14 @@ import { readFile, writeFile } from '../src/helpers/fileUtils';
 (global as any).fetch = fetch;
 
 const addPageMap = () => {
-  const args = argv.string('mapPath').argv as any as {
+  const {
+    mapPath,
+    bookSlug,
+    bookTitle,
+    bookId,
+    canonicalBookTitle,
+    canonicalBookId,
+  } = argv.string('mapPath').argv as any as {
     mapPath: string,
     bookSlug: string,
     bookTitle: string,
@@ -16,20 +23,26 @@ const addPageMap = () => {
     canonicalBookId: string,
   };
 
-  // tslint:disable-next-line: max-line-length
-  let mapString = `// tslint:disable: object-literal-sort-keys\n// tslint:disable: max-line-length\n\nimport { CanonicalBookMap } from '../canonicalBookMap';\n\nexport default {\n  /* ${args.bookTitle} */ '${args.bookId}': [\n    /* ${args.canonicalBookTitle} */ ['${args.canonicalBookId}', {\n`;
+  if (!mapPath || !bookSlug || !bookId || !canonicalBookId) {
+    console.log('Mapping failed. Missing one or more arguments.');
+    process.exit(1);
+  }
 
-  const mapFile = readFile(args.mapPath);
+  // tslint:disable-next-line: max-line-length
+  let mapString = `// tslint:disable: object-literal-sort-keys\n// tslint:disable: max-line-length\n\nimport { CanonicalBookMap } from '../canonicalBookMap';\n\nexport default {\n  /* ${bookTitle} */ '${bookId}': [\n    /* ${canonicalBookTitle} */ ['${canonicalBookId}', {\n`;
+
+  const mapFile = readFile(mapPath);
   const parsedMap = JSON.parse(mapFile);
   const changedIds = parsedMap.filter((row: {[key: string]: string}) => row.moduleId !== row.canonicalId);
 
   changedIds.forEach((row: {[key: string]: string}) => {
     // tslint:disable-next-line: max-line-length
-    mapString += `      /* ${row.module} to the same module in ${args.canonicalBookTitle} */\n      '${row.moduleId}': '${row.canonicalId}',\n`;
+    mapString += `      /* ${row.module} to the same module in ${canonicalBookTitle} */\n      '${row.moduleId}': '${row.canonicalId}',\n`;
   });
   mapString += `    }],\n  ],\n} as CanonicalBookMap;\n`;
-  const newMap = path.resolve(__dirname, `../src/canonicalBookMap/${args.bookSlug}.ts`);
+  const newMap = path.resolve(__dirname, `../src/canonicalBookMap/${bookSlug}.ts`);
   writeFile(newMap, mapString);
+  console.log('Mapping complete. Make sure to import new map to canonicalBookMap.ts');
 };
 
 addPageMap();
