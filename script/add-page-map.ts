a61d37f176f54a75127c8941c20a8d80506b7393
Beth Shook <es66@rice.edu>
2022-06-24 13:59:09 -0500

    add beginning and ending of map file to string to be written

--- script/add-page-map.ts
+++ script/add-page-map.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import path from 'path';
 import argv from 'yargs';
+import { CANONICAL_MAP } from '../src/canonicalBookMap';
 import { readFile, writeFile } from '../src/helpers/fileUtils';
 
 (global as any).fetch = fetch;
@@ -9,16 +10,24 @@ const addPageMap = () => {
   const args = argv.string('mapPath').argv as any as {
     mapPath: string,
     bookSlug: string,
+    bookTitle: string,
+    bookId: string,
+    canonicalBookTitle: string,
+    canonicalBookId: string,
   };
+
+  // tslint:disable-next-line: max-line-length
+  let mapString = `// tslint:disable: object-literal-sort-keys\n// tslint:disable: max-line-length\n\nimport { CanonicalBookMap } from '../canonicalBookMap';\n\nexport default {\n  /* ${args.bookTitle} */ '${args.bookId}': [\n    /* ${args.canonicalBookTitle} */ ['${args.canonicalBookId}', {\n`;
 
   const mapFile = readFile(args.mapPath);
   const parsedMap = JSON.parse(mapFile);
-
   const changedIds = parsedMap.filter((row: {[key: string]: string}) => row.moduleId !== row.canonicalId);
-  let mapString = '';
+
   changedIds.forEach((row: {[key: string]: string}) => {
-    mapString += `/* ${row.module} to the same module */\n'${row.moduleId}': '${row.canonicalId}',\n`;
-    });
+    // tslint:disable-next-line: max-line-length
+    mapString += `      /* ${row.module} to the same module in ${args.canonicalBookTitle} */\n      '${row.moduleId}': '${row.canonicalId}',\n`;
+  });
+  mapString += `    }],\n  ],\n} as CanonicalBookMap;\n`;
   const newMap = path.resolve(__dirname, `../src/canonicalBookMap/${args.bookSlug}.ts`);
   writeFile(newMap, mapString);
 };
