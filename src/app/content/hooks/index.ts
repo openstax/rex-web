f5067ccc80a6a57c4b150c21d358107e9fbeaea2
tom <tom.g.woodward@gmail.com>
2022-06-30 12:14:50 -0400

    Add some comments

--- src/app/content/hooks/index.ts
+++ src/app/content/hooks/index.ts
@@ -22,8 +22,19 @@ export default [
   ...studyGuidesHooks,
   ...practiceQuestionsHooks,
   receivePageNotFoundId,
+  /*
+   * clears meta on locationChange, in case the new route doesn't call setHead
+   * */
   routeHook(routes.content, locationChangeBody),
+  /*
+   * sets metadata based on the loaded content
+   */
   actionHook(actions.receivePage, receiveContentBody),
+  /*
+   * meta added by prerendering is removed after hydration calls the initial locationChange.
+   * processing receiveContent on locationChange pops it back in again. in subsequent navigation
+   * this noops because the book data has been cleared by the navigation.
+   */
   actionHook(navigationActions.locationChange, receiveContentBody),
   actionHook(actions.receivePage, kineticEnabled),
   actionHook(actions.setTextSize, storeTextSize),
