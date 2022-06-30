96d1b0e70090c20d10b93f6a04a240e1a60ae6e2
tom <tom.g.woodward@gmail.com>
2022-06-30 15:05:40 -0400

    testing something

--- src/test/browserutils.ts
+++ src/test/browserutils.ts
@@ -56,6 +56,8 @@ export const navigate = async(target: puppeteer.Page, path: string) => {
 };
 
 export const finishRender = async(target: puppeteer.Page) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await target.waitFor('body[data-rex-loaded="true"]');
   const screenshot = (): Buffer => target.screenshot() as unknown as Buffer;
 
   let lastScreen: Buffer | undefined;
@@ -70,6 +72,8 @@ export const finishRender = async(target: puppeteer.Page) => {
     await new Promise((resolve) => setTimeout(resolve, 300));
     lastScreen = newScreen;
   }
+
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
 };
 
 export const scrollDown = (target: puppeteer.Page) => target.evaluate(() => {
