53cb7694d2bbf13d7cc26ea14a5359c843c831c8
Beth Shook <es66@rice.edu>
2022-07-06 10:58:12 -0500

    remove map changes

--- src/canonicalBookMap.ts
+++ src/canonicalBookMap.ts
@@ -35,6 +35,9 @@ export const CANONICAL_MAP: CanonicalBookMap = {
   /* Algebra & Trigonometry */ '13ac107a-f15f-49d2-97e8-60ab2e3b519c' : [
     /* College Algebra 2e */ ['35d7cce2-48dd-4403-b6a5-e828cb5a17da', {}],
     /* Precalculus 2e */ ['f021395f-fd63-46cd-ab95-037c6f051730', {}],
+    /* College Algebra */ ['9b08c294-057f-4201-9f48-5d6ad992740d', {}],
+    /* Precalculus */ ['fd53eae1-fa23-47c7-bb1b-972349835c3c', {}],
+    /* Algebra & Trigonometry 2e */ ['eaefdaf1-bda0-4ada-a9fe-f1c065bfcc4e', {}],
   ],
   /* Anatomy & Physiology */ '14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22': [
     /* Anatomy & Physiology 2e */ ['4fd99458-6fdf-49bc-8688-a6dc17a1268d', {}],
@@ -56,6 +59,8 @@ export const CANONICAL_MAP: CanonicalBookMap = {
   ],
   /* College Algebra with Corequisite Support */ '507feb1e-cfff-4b54-bc07-d52636cecfe3': [
     /* College Algebra 2e */ ['35d7cce2-48dd-4403-b6a5-e828cb5a17da', {}],
+    /* College Algebra */ ['9b08c294-057f-4201-9f48-5d6ad992740d', {}],
+    /* College Algebra with Corequisite Support 2e */ ['59024a63-2b1a-4631-94c5-ae275a77b587', {}],
   ],
   /* Psychology */ '4abf04bf-93a0-45c3-9cbc-2cefd46e68cc' : [
     /* Psychology 2e */ ['06aba565-9432-40f6-97ee-b8a361f118a8', {
@@ -350,6 +355,7 @@ export const CANONICAL_MAP: CanonicalBookMap = {
   ],
   /* Precalculus */ 'fd53eae1-fa23-47c7-bb1b-972349835c3c' : [
     /* College Algebra 2e */ ['35d7cce2-48dd-4403-b6a5-e828cb5a17da', {}],
+    /* College Algebra */ ['9b08c294-057f-4201-9f48-5d6ad992740d', {}],
     /* Precalculus 2e */ ['f021395f-fd63-46cd-ab95-037c6f051730', {}],
   ],
   /* Principles of Accounting Vol. 2 */ '920d1c8a-606c-4888-bfd4-d1ee27ce1795': [
