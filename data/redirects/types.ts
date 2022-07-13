bae779dab33a2ed929d5bffad4eb3fdb268ec277
Beth Shook <es66@rice.edu>
2022-07-13 18:49:50 -0500

    revert unneeded changes

--- data/redirects/types.ts
+++ data/redirects/types.ts
@@ -1,2 +1,2 @@
-export type RedirectsData = Array<{ bookId: string, pageId: string, pathname: string, redirectBookId?: string }>;
+export type RedirectsData = Array<{ bookId: string, pageId: string, pathname: string }>;
 export type Redirects = Array<{ from: string, to: string }>;
