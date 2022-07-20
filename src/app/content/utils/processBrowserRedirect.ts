d8f43db26e723064aab73b8baab906f5694b4c03
Beth Shook <es66@rice.edu>
2022-07-20 15:22:56 -0500

    remove logs

--- src/app/content/utils/processBrowserRedirect.ts
+++ src/app/content/utils/processBrowserRedirect.ts
@@ -2,13 +2,11 @@ import { History } from 'history';
 import { Redirects } from '../../../../data/redirects/types';
 
 export const processBrowserRedirect = async(services: {history: History}) => {
-  console.log('process: ', services.history.location.pathname);
   const redirects: Redirects = await fetch('/rex/redirects.json')
     .then((res) => res.json())
     .catch(() => []);
 
   for (const {from, to} of redirects) {
-    console.log('from: ', from);
     if (from === services.history.location.pathname) {
       services.history.replace(to);
       return true;
