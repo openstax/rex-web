c44fcf4a50a8dc37af6c3e0526bf36e2ff920ecf
tom <tom.g.woodward@gmail.com>
2022-08-03 14:02:17 -0400

    fix loader specs

--- src/gateways/createArchiveLoader.spec.ts
+++ src/gateways/createArchiveLoader.spec.ts
@@ -1,4 +1,5 @@
 import { resetModules } from '../test/utils';
+import createArchiveLoader from './createArchiveLoader';
 
 const mockFetch = (code: number, data: any) => jest.fn(() => Promise.resolve({
   json: () => Promise.resolve(data),
@@ -8,7 +9,6 @@ const mockFetch = (code: number, data: any) => jest.fn(() => Promise.resolve({
 
 describe('archiveLoader', () => {
   const fetchBackup = fetch;
-  const url = () => 'url';
 
   afterEach(() => {
     resetModules();
@@ -22,74 +22,99 @@ describe('archiveLoader', () => {
       });
 
       it('requests data from archive url for book', () => {
-        require('./createArchiveLoader').default(url).book('coolid', 'version').load();
-        expect(fetch).toHaveBeenCalledWith('url/contents/coolid@version.json');
+        const booksConfig = {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}};
+        createArchiveLoader().book('coolid', {booksConfig}).load();
+        expect(fetch).toHaveBeenCalledWith('/test/archive/contents/coolid@version.json');
       });
 
       it('uses pinned archive path for book', () => {
-        require('../config.books').pinnedbook = {archiveOverride: 'other', defaultVersion: 'unused'};
-        require('./createArchiveLoader').default(url).book('pinnedbook', 'version').load();
-        expect(fetch).toHaveBeenCalledWith('other/contents/pinnedbook@version.json');
+        const booksConfig = {
+          archiveUrl: '/test/archive',
+          books: {pinnedbook: {defaultVersion: 'version', archiveOverride: '/test/override'}},
+        };
+        createArchiveLoader().book('pinnedbook', {booksConfig}).load();
+        expect(fetch).toHaveBeenCalledWith('/test/override/contents/pinnedbook@version.json');
       });
 
       it('ignores pinned archive path for book when disablePerBookPinning is passed', () => {
-        require('../config.books').pinnedbook = {archiveOverride: 'other', defaultVersion: 'unused'};
-        require('./createArchiveLoader').default(url, {disablePerBookPinning: true})
-          .book('pinnedbook', 'version').load();
-        expect(fetch).not.toHaveBeenCalledWith('other/contents/pinnedbook@version.json');
-        expect(fetch).toHaveBeenCalledWith('url/contents/pinnedbook@version.json');
+        const booksConfig = {
+          archiveUrl: '/test/archive',
+          books: {pinnedbook: {defaultVersion: 'version', archiveOverride: '/test/override'}},
+        };
+        createArchiveLoader({disablePerBookPinning: true})
+          .book('pinnedbook', {booksConfig}).load();
+        expect(fetch).not.toHaveBeenCalledWith('/test/override/contents/pinnedbook@version.json');
+        expect(fetch).toHaveBeenCalledWith('/test/archive/contents/pinnedbook@version.json');
       });
 
       it('requests data from archive url for page', () => {
-        require('./createArchiveLoader').default(url).book('coolid', 'version').page('coolpageid').load();
+        const booksConfig = {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}};
+        createArchiveLoader().book('coolid', {booksConfig}).page('coolpageid').load();
 
-        expect(fetch).toHaveBeenCalledWith('url/contents/coolid@version:coolpageid.json');
+        expect(fetch).toHaveBeenCalledWith('/test/archive/contents/coolid@version:coolpageid.json');
       });
 
       it('returns cached book data', async() => {
-        const archiveLoader = require('./createArchiveLoader').default(url);
-        const one = await archiveLoader.book('coolid', 'version').load();
-        const two = archiveLoader.book('coolid', 'version').cached();
+        (global as any).fetch = mockFetch(200, {version: 'version', id: 'coolid'});
+        const archiveLoader = createArchiveLoader();
+        const booksConfig = {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}};
+        const one = await archiveLoader.book('coolid', {booksConfig}).load();
+        const two = archiveLoader.book('coolid', {booksConfig}).cached();
+        const three = archiveLoader.forBook(one).cached();
 
         expect(fetch).toHaveBeenCalledTimes(1);
-        expect(two).toBe(one);
+        expect(two).toBe(three);
+        expect(three).toBe(one);
       });
 
       it('returns cached page data', async() => {
-        const archiveLoader = require('./createArchiveLoader').default(url);
-        const one = await archiveLoader.book('coolid', 'version').page('coolpageid').load();
-        const two = archiveLoader.book('coolid', 'version').page('coolpageid').cached();
+        const archiveLoader = createArchiveLoader();
+        const booksConfig = {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}};
+        const one = await archiveLoader.book('coolid', {booksConfig}).page('coolpageid').load();
+        const two = archiveLoader.book('coolid', {booksConfig}).page('coolpageid').cached();
 
         expect(fetch).toHaveBeenCalledTimes(1);
         expect(two).toBe(one);
       });
 
       it('returns versioned book data', async() => {
-        require('./createArchiveLoader').default(url).book('coolid', 'version').load();
-        expect(fetch).toHaveBeenCalledWith('url/contents/coolid@version.json');
+        const booksConfig = {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}};
+        createArchiveLoader().book('coolid', {booksConfig}).load();
+        expect(fetch).toHaveBeenCalledWith('/test/archive/contents/coolid@version.json');
       });
 
       it('returns versioned page data', async() => {
-        await require('./createArchiveLoader').default(url).book('coolid', 'version').page('pageid').load();
+        const booksConfig = {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}};
+        await createArchiveLoader().book('coolid', {booksConfig}).page('pageid').load();
 
-        expect(fetch).toHaveBeenCalledWith('url/contents/coolid@version:pageid.json');
+        expect(fetch).toHaveBeenCalledWith('/test/archive/contents/coolid@version:pageid.json');
       });
 
       it('memoizes requests', async() => {
-        const archiveLoader = require('./createArchiveLoader').default(url);
-        await archiveLoader.book('coolid', 'version').load();
-        await archiveLoader.book('coolid2', 'version').load();
-        await archiveLoader.book('coolid', 'version').load();
-        await archiveLoader.book('coolid1', 'version').load();
-        await archiveLoader.book('coolid', 'version').load();
-        await archiveLoader.book('coolid2', 'version').load();
+        const archiveLoader = createArchiveLoader();
+        const booksConfig = {
+          archiveUrl: '/test/archive',
+          books: {
+            coolid: {defaultVersion: 'version'},
+            coolid1: {defaultVersion: 'version'},
+            coolid2: {defaultVersion: 'version'},
+          },
+        };
+        await archiveLoader.book('coolid', {booksConfig}).load();
+        await archiveLoader.book('coolid2', {booksConfig}).load();
+        await archiveLoader.book('coolid', {booksConfig}).load();
+        await archiveLoader.book('coolid1', {booksConfig}).load();
+        await archiveLoader.book('coolid', {booksConfig}).load();
+        await archiveLoader.book('coolid2', {booksConfig}).load();
 
         expect(fetch).toHaveBeenCalledTimes(3);
       });
 
       it('returns original content url', async() => {
-        expect(require('./createArchiveLoader').default(url).book('coolid', 'version').page('pageid').url())
-          .toEqual('url/contents/coolid@version:pageid.json');
+        expect(createArchiveLoader().book('coolid', {
+          booksConfig: {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}},
+        }).page('pageid').url())
+          .toEqual('/test/archive/contents/coolid@version:pageid.json');
       });
     });
 
@@ -98,13 +123,17 @@ describe('archiveLoader', () => {
       let error: Error | null = null;
 
       try {
-        await require('./createArchiveLoader').default(url).book('coolid', 'version').load();
+        await createArchiveLoader().book('coolid', {
+          booksConfig: {archiveUrl: '/test/archive', books: {coolid: {defaultVersion: 'version'}}},
+        }).load();
       } catch (e) {
         error = e;
       }
 
       if (error) {
-        expect(error.message).toEqual('Error response from archive "url/contents/coolid@version.json" 404: not found');
+        expect(error.message).toEqual(
+          'Error response from archive "/test/archive/contents/coolid@version.json" 404: not found'
+        );
       } else {
         expect(error).toBeTruthy();
       }
