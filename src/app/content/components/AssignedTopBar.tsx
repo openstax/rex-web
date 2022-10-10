b7ad57319b9f358104b5a679563b911740927694
JD Ivey <josiah.ivey@gmail.com>
2022-10-10 06:44:51 -0700

    Assignable toolbar (#1746)
    * wip get top bar to render
    
    * internalize resizer callbacks, hook up dispatch
    
    * update snapshot and add spec
    
    * fix indentation
    
    * 🩹 prevent text resizer dropdown looking bad in narrow sizes
    
    * lint all the things
    
    * update snapshots
    
    * use TopBarWrapper
    
    * fix lint
    
    * match wrapper components style
    
    * snapshots
    
    * bump checks
    
    * update snap
    
    Co-authored-by: staxly[bot] <35789409+staxly[bot]@users.noreply.github.com>
    Co-authored-by: tom <tom.g.woodward@gmail.com>

---
+++ src/app/content/components/AssignedTopBar.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled, { css } from 'styled-components';
+import theme from '../../theme';
+import { setTextSize } from '../actions';
+import * as selectContent from '../selectors';
+import { LinkedArchiveTreeSection } from '../types';
+import { shadow, TopBarWrapper } from './Topbar/styled';
+import { TextResizer } from './Topbar/TextResizer';
+
+// tslint:disable-next-line:variable-name
+const StyledTopBarWrapper = styled(TopBarWrapper)`
+  ${shadow}
+
+  && {
+    top: 0;
+  }
+
+  background-color: ${theme.color.neutral.base};
+  color: ${theme.color.text.default};
+  display: flex;
+`;
+
+// tslint:disable-next-line:variable-name
+const StyledSectionTitle = styled.h2`
+  font-size: 1.8rem;
+  line-height: 2.1rem;
+  letter-spacing: 0.03px;
+  margin-left: 1.6rem;
+  ${theme.breakpoints.mobile(css`
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  `)}
+`;
+
+// tslint:disable-next-line:variable-name
+export const AssignedTopBar = (props: {
+  section: LinkedArchiveTreeSection;
+}) => {
+
+  const bookTheme = useSelector(selectContent.bookTheme);
+  const textSize = useSelector(selectContent.textSize);
+  const dispatch = useDispatch();
+
+  return (
+    <StyledTopBarWrapper>
+      <StyledSectionTitle dangerouslySetInnerHTML={{ __html:  props.section?.title }} />
+      <TextResizer
+        bookTheme={bookTheme}
+        setTextSize={(value) => dispatch(setTextSize(value))}
+        textSize={textSize}
+        data-testid='text-resizer'
+        mobileToolbarOpen={false}
+        mobileVariant={false}
+      />
+    </StyledTopBarWrapper>
+  );
+};
