4a224e85f965b1c4a842fa23410aaaa5b27f3852
Beth Shook <es66@rice.edu>
2023-01-27 15:52:53 -0600

    rename vars

--- src/app/navigation/hooks/closeModalHook.ts
+++ src/app/navigation/hooks/closeModalHook.ts
@@ -2,8 +2,8 @@ import queryString from 'querystring';
 import { AppServices, MiddlewareAPI } from '../../types';
 
 export const closeModal = (services: MiddlewareAPI & AppServices) => () => {
-  const currentQuery = services.history.location.search;
-  const parsedQuery = queryString.parse(currentQuery?.substring(1)) || {};
+  const queryStr = services.history.location.search;
+  const parsedQuery = queryString.parse(queryStr?.substring(1)) || {};
   const {modal, query, target} = parsedQuery;
 
   // this is undefined if previous record is not on rex
@@ -14,11 +14,11 @@ export const closeModal = (services: MiddlewareAPI & AppServices) => () => {
   // in case page is loaded with both search term and modal in the query
   // in which case receiveSearchHook will have added a previous record
   } else if (modal && query) {
-    const newQuery = queryString.stringify({
+    const newQueryString = queryString.stringify({
       query,
       target,
     });
-    services.history.replace(`?${newQuery}`);
+    services.history.replace(`?${newQueryString}`);
   } else {
     services.history.goBack();
   }
