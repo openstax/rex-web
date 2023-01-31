5081db36c7227cb35799da7f058941784d0828c1
Beth Shook <es66@rice.edu>
2023-01-30 22:34:39 -0600

    remove error thrown for repeated book checks

--- src/app/content/utils/canonicalUrl.ts
+++ src/app/content/utils/canonicalUrl.ts
@@ -33,29 +33,24 @@ export async function getCanonicalUrlParams(
   while (canonicalMap.length && !done) {
     for (const [id, CANONICAL_PAGES_MAP] of canonicalMap) {
       mapsChecked.push(canonicalMap);
+      // this is just to get the book, don't want to load unless necessary
       const useCurrentBookAsCanonical = book.id === id && hasOSWebData(book);
       canonicalBook = useCurrentBookAsCanonical ? book : await getBook(id);
       canonicalPageId = CANONICAL_PAGES_MAP[canonicalPageId] || canonicalPageId;
       const treeSection = findArchiveTreeNodeById(canonicalBook.tree, canonicalPageId);
 
-      // if no matching page was found in current canonical book, fall back to the last match found
+      // if no matching page found in current canonical book, fall back to the last one found
       if (!treeSection && canonicalBookWithPage) {
+        done = true;
         break;
-      // else if a matching page is found, store it
+      // if a matching page is found, store it
       } else if (treeSection) {
         canonicalBookWithPage = {canonicalBook, treeSection};
-      // else we can stop this iteration and check the next book in the map
-      } else if (!treeSection) {
-        continue;
       }
 
-      // check if canonical book maps to another book
+      // check if canonical book maps to another book not yet checked
       const newMap = CANONICAL_MAP[canonicalBook.id] ? getCanonicalMap(canonicalBook.id) : [];
-      done = !newMap.length || isEqual(canonicalMap, newMap);
-      // throw if the new map has already been checked
-      if (!done && mapsChecked.find((map) => isEqual(map, newMap))) {
-        throw new Error(`Loop encountered in map for ${canonicalBook.id}`);
-      }
+      done = !newMap.length || !!mapsChecked.find((map) => isEqual(map, newMap));
       canonicalMap = newMap;
     }
   }
@@ -67,7 +62,6 @@ export async function getCanonicalUrlParams(
   }
   if (canonicalBookWithPage && canonicalBookWithPage.treeSection) {
     const pageInBook = assertDefined(canonicalBookWithPage.treeSection.slug, 'Expected page to have slug.');
-    console.log('returning: ', canonicalBookWithPage.canonicalBook.title);
     return {book: {slug: (canonicalBookWithPage.canonicalBook as BookWithOSWebData).slug}, page: {slug: pageInBook}};
   }
 
