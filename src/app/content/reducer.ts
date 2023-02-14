204ce173af92a05e49ad5d5af9345da79eb7d01a
Beth Shook <es66@rice.edu>
2023-02-14 14:58:26 -0600

    add check before omitting page from state

--- src/app/content/reducer.ts
+++ src/app/content/reducer.ts
@@ -88,7 +88,10 @@ function reduceContent(state: State, action: AnyAction) {
       return reduceReceiveBook(state, action);
     }
     case getType(actions.requestPage):
-      return {...omit(['page'], state), loading: {...state.loading, page: action.payload}};
+      return {
+      ...(isEqual(action.payload, state.page) ? state : omit(['page'], state)),
+      loading: {...state.loading, page: action.payload},
+    };
     case getType(actions.receivePage): {
       return reduceReceivePage(state, action);
     }
