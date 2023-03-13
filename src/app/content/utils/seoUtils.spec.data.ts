8a1c9796660f5491a43bb5184aa9ce8657ecdd41
Thomas Woodward <tom.g.woodward@gmail.com>
2023-03-13 12:06:59 -0400

    fix spec coverage (#1897)

--- src/app/content/utils/seoUtils.spec.data.ts
+++ src/app/content/utils/seoUtils.spec.data.ts
@@ -31,6 +31,16 @@ export const contentPageShort = {
   slug: 'best-practices',
 };
 
+export const contentPageWithMath = {
+  title: 'Introduction With Math',
+  abstract: '',
+  content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="page" id="d1fd537-77fb-4eac-8a8a-60bbaa747b6d" class="introduction" data-cnxml-to-html-ver="2.1.0"><p>For example, take <span xmlns="http://www.w3.org/1999/xhtml" class="os-math-in-para"><math display="inline"></math></span> a look at the image above. This image is of the Andromeda Galaxy, which contains billions of individual stars, huge clouds of gas, and dust. Two smaller galaxies are also visible as bright blue spots in the background. At a staggering 2.5 million light years from the Earth, this galaxy is the nearest one to our own galaxy (which is called the Milky Way). The stars and planets that make up Andromeda might seem to be the furthest thing from most people’s regular, everyday lives. But Andromeda is a great starting point to think about the forces that hold together the universe.</p><p>The forces that cause Andromeda to act as it does are the same forces we contend with here on Earth, whether we are planning to send a rocket into space or simply raise the walls for a new home. The same gravity that causes the stars of Andromeda to rotate and revolve also causes water to flow over hydroelectric dams here on Earth. Tonight, take a moment to look up at the stars. The forces out there are the same as the ones here on Earth. Through a study of physics, you may gain a greater understanding of the interconnectedness of everything we can see and know in this universe.</p></div></body></html>',
+  id: '6',
+  version: '1.0',
+  revised: '',
+  slug: 'introduction-with-math',
+};
+
 export const eocPage = {
   title: 'Review Questions',
   content: '<html xmlns="http://www.w3.org/1999/xhtml"><body><div data-type="composite-page" id="d1fd537-77fb-4eac-8a8a-60bbaa747b6d" data-cnxml-to-html-ver="2.1.0"><section><p>Religion describes the beliefs, values, and practices related to sacred or spiritual concerns. Social theorist Émile Durkheim defined religion as a “unified system of beliefs and practices relative to sacred things” (1915). Max Weber believed religion could be a force for social change. Karl Marx viewed religion as a tool used by capitalist societies to perpetuate inequality. Religion is a social institution, because it includes beliefs and practices that serve the needs of society. Religion is also an example of a cultural universal, because it is found in all societies in one form or another. Functionalism, conflict theory, and interactionism all provide valuable ways for sociologists to understand religion.</p></section></div></body></html>',
