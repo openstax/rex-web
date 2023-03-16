096bb209b92f78d44c971670f0c21ef3c570db2d
Malar Natarajan <Malarvizhi.Natarajan@rice.edu>
2023-03-16 07:34:59 -0500

    change screen resolution

--- pytest-selenium/tests/conftest.py
+++ pytest-selenium/tests/conftest.py
@@ -7,7 +7,7 @@ import pytest
 from utils import utility
 
 # Window resolutions. Pytest takes these inputs backwards.
-DESKTOP = (1500, 1080)
+DESKTOP = (1600, 1300)
 # this used to be 414x738, but it looks like chrome won't resize lower than 500
 MOBILE = (500, 738)
 
