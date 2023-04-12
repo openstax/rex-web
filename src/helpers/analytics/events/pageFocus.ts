539c80be3a2fdf66b7177417212802bdbc80470c
Dante Soares <dante.m.soares@rice.edu>
2023-04-12 16:58:16 -0500

    Fixed types

--- src/helpers/analytics/events/pageFocus.ts
+++ src/helpers/analytics/events/pageFocus.ts
@@ -2,13 +2,12 @@ import { stateChange } from '@openstax/event-capture-client/events';
 import { Document } from '@openstax/types/lib.dom';
 import * as selectContent from '../../../app/content/selectors';
 import * as archiveTreeUtils from '../../../app/content/utils/archiveTreeUtils';
-import { AppState } from '../../../app/types';
 import { AnalyticsEvent } from './event';
 
-export const selector = (_state: AppState) => ({});
+export const selector = selectContent.bookAndPage;
 
 export const track = (
-  {book, page}: ReturnType<typeof selectContent.bookAndPage>,
+  {book, page}: ReturnType<typeof selector>,
   document: Document
 ): AnalyticsEvent | void => {
   const focus = document.hasFocus();
