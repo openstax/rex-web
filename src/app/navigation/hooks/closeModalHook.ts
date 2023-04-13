6d027dcbd04713e98049fbeb84261cf444086f3e
Dante Soares <dante.m.soares@rice.edu>
2023-04-13 14:59:50 -0500

    Fixed some more coverage

--- src/app/navigation/hooks/closeModalHook.ts
+++ src/app/navigation/hooks/closeModalHook.ts
@@ -3,7 +3,7 @@ import { AppServices, MiddlewareAPI } from '../../types';
 
 export const closeModal = (services: MiddlewareAPI & AppServices) => () => {
   const queryStr = services.history.location.search;
-  const parsedQuery = queryString.parse(queryStr?.substring(1)) || {};
+  const parsedQuery = queryString.parse(queryStr?.substring(1));
   const {modal, ...rest} = parsedQuery;
 
   // this is undefined if previous record is not on rex
