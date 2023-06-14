cdb191770893a7ce5c60e4af63d90a1def278c7f
Roy Johnson <roy.e.johnson@rice.edu>
2023-06-14 14:56:40 -0500

    Refactoring
    useKeyTermPair hook
    
    Prettier breaks lint

--- src/app/content/search/components/SearchResultsSidebar/SearchResultHits.tsx
+++ src/app/content/search/components/SearchResultsSidebar/SearchResultHits.tsx
@@ -21,43 +21,68 @@ interface SearchResultHitsProps {
   hits: SearchResultHit[];
   testId: string;
   getPage: (hit: SearchResultHit) => ArchiveTreeSection;
-  onClick: (result: { result: SearchResultHit, highlight: number }) => void;
+  onClick: (result: { result: SearchResultHit; highlight: number }) => void;
   selectedResult: SelectedResult | null;
   queryParams: OutputParams;
 }
+type OneSearchResultHitProps = Omit<SearchResultHitsProps, 'hits'> & {
+  hit: SearchResultHit;
+  loader: any;
+};
 
-// tslint:disable-next-line: variable-name
-const SearchResultHits = ({
-  activeSectionRef, book, hits, getPage, testId, onClick, selectedResult, queryParams,
-}: SearchResultHitsProps) => {
-  const [keyTerms, setKeyTerms] = React.useState({});
-  const { archiveLoader } = useServices();
+function useKeyTermPair({
+  hit,
+  loader,
+}: {
+  hit: SearchResultHit;
+  loader: any;
+}): ReturnType<typeof getKeyTermPair> | undefined {
+  const [pair, setPair] = React.useState(undefined as any);
 
   React.useEffect(() => {
-    const keyTermsHits = hits.filter(isKeyTermHit);
-    const loader = archiveLoader.forBook(book);
+    const fn = async() => {
+      const content = await loadPageContent(
+        loader,
+        stripIdVersion(hit.source.pageId)
+      );
 
-    const getKeyTermsPages = async() => {
-      keyTermsHits.forEach(async(hit) => {
-        const content = await loadPageContent(loader, stripIdVersion(hit.source.pageId));
-        setKeyTerms((pages) => ({ ...pages, [hit.source.elementId]: getKeyTermPair(content, hit.source.elementId) }));
-      });
+      setPair(getKeyTermPair(content, hit.source.elementId));
     };
+    if (isKeyTermHit(hit)) {
+      fn();
+    }
+  }, [hit, loader]);
 
-    getKeyTermsPages();
-  }, [hits, book, archiveLoader]);
+  return pair;
+}
 
-  return <React.Fragment>
-    {hits.map((hit) => {
-      // inefficient data structure for search results should be addressed in the future
-      // https://app.zenhub.com/workspaces/openstax-unified-5b71aabe3815ff014b102258/issues/openstax/unified/1750
-      if (isKeyTermHit(hit)) {
-        const pair = (keyTerms as { [key: string]: any })[hit.source.elementId];
-        hit.highlight.term = pair?.term || '...';
-        hit.highlight.visibleContent = pair?.definition ? [pair.definition] : hit.highlight.visibleContent;
-      }
+// tslint:disable-next-line: variable-name
+const OneSearchResultHit = ({
+  activeSectionRef,
+  book,
+  hit,
+  getPage,
+  testId,
+  onClick,
+  selectedResult,
+  queryParams,
+  loader,
+}: OneSearchResultHitProps) => {
+  const pair = useKeyTermPair({ hit, loader });
 
-      return hit.highlight.visibleContent?.map((highlight: string, index: number) => {
+  // inefficient data structure for search results should be addressed in the future
+  // https://app.zenhub.com/workspaces/openstax-unified-5b71aabe3815ff014b102258/issues/openstax/unified/1750
+  if (isKeyTermHit(hit)) {
+    hit.highlight.term = '...';
+    if (pair) {
+      hit.highlight.term = pair.term;
+      hit.highlight.visibleContent = [pair.definition];
+    }
+  }
+
+  return (
+    <React.Fragment>
+      {hit.highlight.visibleContent?.map((highlight: string, index: number) => {
         const thisResult = { result: hit, highlight: index };
         const isSelected = isEqual(selectedResult, thisResult);
         const target: SearchScrollTarget = {
@@ -66,34 +91,74 @@ const SearchResultHits = ({
           type: 'search',
         };
 
-        return <Styled.SectionContentPreview
-          selectedResult={isSelected}
-          data-testid={testId}
-          key={index}
-          book={book}
-          page={getPage(hit)}
-          scrollTarget={target}
-          queryParams={queryParams}
-          onClick={() => onClick(thisResult)}
-          {...isSelected && activeSectionRef ? { ref: activeSectionRef } : {}}
-        >
-          {isKeyTermHit(hit)
-            ? <RelatedKeyTermContent keyTermHit={hit} />
-            : (<Styled.SimpleResult>
-              <div tabIndex={-1} dangerouslySetInnerHTML={{ __html: highlight }} />
-            </Styled.SimpleResult>
+        return (
+          <Styled.SectionContentPreview
+            selectedResult={isSelected}
+            data-testid={testId}
+            key={index}
+            book={book}
+            page={getPage(hit)}
+            scrollTarget={target}
+            queryParams={queryParams}
+            onClick={() => onClick(thisResult)}
+            {...(isSelected && activeSectionRef
+              ? { ref: activeSectionRef }
+              : {})}
+          >
+            {isKeyTermHit(hit) ? (
+              <RelatedKeyTermContent keyTermHit={hit} />
+            ) : (
+              <Styled.SimpleResult>
+                <div
+                  tabIndex={-1}
+                  dangerouslySetInnerHTML={{ __html: highlight }}
+                />
+              </Styled.SimpleResult>
             )}
-        </Styled.SectionContentPreview>;
-      });
-    })}
-  </React.Fragment>;
+          </Styled.SectionContentPreview>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+// tslint:disable-next-line: variable-name
+const SearchResultHits = ({
+  activeSectionRef,
+  book,
+  hits,
+  getPage,
+  testId,
+  onClick,
+  selectedResult,
+  queryParams,
+}: SearchResultHitsProps) => {
+  const { archiveLoader } = useServices();
+  const loader = archiveLoader.forBook(book);
+
+  return (
+    <React.Fragment>
+      {hits.map((hit, index) => (
+        <OneSearchResultHit
+          hit={hit}
+          key={index}
+          activeSectionRef={activeSectionRef}
+          book={book}
+          getPage={getPage}
+          testId={testId}
+          onClick={onClick}
+          selectedResult={selectedResult}
+          queryParams={queryParams}
+          loader={loader}
+        />
+      ))}
+    </React.Fragment>
+  );
 };
 
 // tslint:disable-next-line:variable-name
-export const ConnectedSearchResultHits = connect(
-  (state: AppState) => ({
-    queryParams: navSelect.persistentQueryParameters(state),
-  })
-)(SearchResultHits);
+export const ConnectedSearchResultHits = connect((state: AppState) => ({
+  queryParams: navSelect.persistentQueryParameters(state),
+}))(SearchResultHits);
 
 export default ConnectedSearchResultHits;
