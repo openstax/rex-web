3a7d4cca719eece3d39787f4146c4bd5071d75ea
Roy Johnson <roy.e.johnson@rice.edu>
2023-06-28 11:28:18 -0500

    Skip key terms results when selecting first search result

--- src/app/content/search/utils.ts
+++ src/app/content/search/utils.ts
@@ -17,11 +17,20 @@ const ACCEPTED_DOC_TYPES = [
   SearchResultHitSourceElementTypeEnum.Paragraph,
 ];
 
+const findFirstResultPage = (container: SearchResultContainer): SearchResultPage => isSearchResultChapter(container)
+  ? findFirstResultPage(container.contents[0])
+  : container;
+
+const skipKeyTerms = (results: SearchResultContainer[]): SearchResultContainer | undefined => {
+  return results.find(r => {
+    const first = findFirstResultPage(r)?.results[0];
+
+    return first && first.source.elementType !== 'key_term';
+  });
+};
+
 export const getFirstResult = (book: {tree: ArchiveTree}, results: SearchResult): SelectedResult | null => {
-  const [result] = getFormattedSearchResults(book.tree, results);
-  const findFirstResultPage = (container: SearchResultContainer): SearchResultPage => isSearchResultChapter(container)
-    ? findFirstResultPage(container.contents[0])
-    : container;
+  const result = skipKeyTerms(getFormattedSearchResults(book.tree, results));
 
   const firstResultPage = result && findFirstResultPage(result);
   const firstResult = firstResultPage && firstResultPage.results[0];
