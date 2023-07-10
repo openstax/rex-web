9c7cb3586792d32b1f42bd2d7a9b44d47550910a
tom <tom.g.woodward@gmail.com>
2023-07-10 12:33:39 -0400

    avoid writing tests

--- src/helpers/dataLayer.ts
+++ src/helpers/dataLayer.ts
@@ -1,10 +1,12 @@
+import { fnIf } from '@openstax/ts-utils/misc/helpers';
+import noop from 'lodash/noop';
+
 /*
  * oxDLF is provided by https://github.com/openstax/analytics-helpers/blob/main/src/data-layer-factories.ts
  */
 export const callDLF = (method: string, ...args: any[]) => {
-  if (typeof window === 'undefined' || typeof (window as any).oxDLF === 'undefined') {
-    return;
-  }
-
-  (window as any).oxDLF[method](...args);
+  const win: any = fnIf(typeof window !== 'undefined', window, {});
+  const oxDLF = fnIf(typeof win.oxDLF !== 'undefined', win.oxDLF, {});
+  const operation = fnIf(typeof oxDLF[method] !== 'undefined', oxDLF[method], noop);
+  operation(...args);
 };
