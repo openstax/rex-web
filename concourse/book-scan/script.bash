3f042d4a9aa1430d42481206061837d63cf544f4
Dante Soares <dante.m.soares@rice.edu>
2023-08-11 17:53:52 -0500

    Run yarn before book-scan

--- concourse/book-scan/script.bash
+++ concourse/book-scan/script.bash
@@ -6,4 +6,6 @@ cloudfront_environment=$(< cloudfront-environment/version.txt)
 
 cd rex-web
 
+yarn
+
 node script/entry.js domVisitor errorsExist --rootUrl="https://$cloudfront_environment" --queryString="validateLinks"
