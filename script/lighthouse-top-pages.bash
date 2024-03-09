309f55eb54c4dff813eab3f04e746314a1668eb2
Dante Soares <dante.m.soares@rice.edu>
2024-03-08 18:50:42 -0600

    Download/save lighthouse reports

--- script/lighthouse-top-pages.bash
+++ script/lighthouse-top-pages.bash
@@ -15,9 +15,22 @@ lighthouse_base_report_path=/tmp/lighthouse-reports
 
 mkdir -p "$lighthouse_base_report_path"
 
+bucket_name=sandbox-unified-web-lighthouse
+
+# Note: this probably fails due to pagination if there are > 1,000 prefixes
+last_prefix=$(aws s3api list-objects-v2 --bucket sandbox-unified-web-lighthouse --delimiter / \
+              --query 'CommonPrefixes != 'null' && sort_by(CommonPrefixes, &Prefix)[-1].Prefix' --output text)
+
+if [ "$last_prefix" != None ]; then
+  aws s3 sync "s3://$bucket_name/$last_prefix" "$lighthouse_base_report_path"
+fi
+
 LIGHTHOUSE_PAGES=$(jq --compact-output --null-input '$ARGS.positional | map("https://openstax.org" + .)' \
                       --args -- "${lighthouse_paths[@]}") \
 LIGHTHOUSE_BASE_REPORT_PATH="$lighthouse_base_report_path" \
 REACT_APP_ENV=test \
 SERVER_MODE=built \
 yarn jest --config "$project_dir/jest-puppeteer.config.json" "$project_dir/src/lighthouse.prerenderspec.ts"
+
+current_date=$(date '+%Y-%m-%d')
+aws s3 sync "$lighthouse_base_report_path" "s3://$bucket_name/$current_date"
