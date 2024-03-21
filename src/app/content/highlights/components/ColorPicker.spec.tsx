2893f0b5dda15505f80cd522d7873733030aebed
Roy Johnson <roy.e.johnson@rice.edu>
2024-03-21 17:04:10 -0500

    [DISCO-146] Fix colorpicker focus

--- src/app/content/highlights/components/ColorPicker.spec.tsx
+++ src/app/content/highlights/components/ColorPicker.spec.tsx
@@ -89,7 +89,7 @@ describe('ColorPicker', () => {
     const onRemove = jest.fn();
     const {root} = renderToDom(
       <TestContainer>
-        <ColorPicker onChange={onChange} onRemove={onRemove} />
+        <ColorPicker color={highlightStyles[0].label} onChange={onChange} onRemove={onRemove} />
       </TestContainer>
     );
     const rg = root.querySelector('[role="radiogroup"]') as HTMLDivElement;
