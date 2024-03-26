46073afbdcc227fb9968b70a74961685c045039d
Roy Johnson <roy.e.johnson@rice.edu>
2024-03-26 16:24:15 -0500

    Group checkboxes in dropdowns

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -9,6 +9,18 @@ export interface ColorSet {
   darkest?: string;
 }
 
+export const hiddenButAccessible = `
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0,0,0,0);
+  white-space: nowrap;
+  border: 0;
+`;
+
 const textColors = {
   black: '#000',
   default: '#424242',
