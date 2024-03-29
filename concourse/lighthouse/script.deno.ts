9b67cf758458eaf389011700ad4201d9c88b866a
Dante Soares <dante.m.soares@rice.edu>
2024-03-29 10:56:20 -0500

    Renamed previous report to most recent report and removed unused code

--- concourse/lighthouse/script.deno.ts
+++ concourse/lighthouse/script.deno.ts
@@ -3,22 +3,20 @@ import asyncPool from 'tiny-async-pool';
 import { checkLighthouse, ScoreTargets } from '../../src/test/browserutils';
 
 if (!process.env.LIGHTHOUSE_PAGES ||
-    !process.env.LIGHTHOUSE_PREVIOUS_REPORT_DIR ||
+    !process.env.LIGHTHOUSE_MOST_RECENT_REPORT_DIR ||
     !process.env.LIGHTHOUSE_REPORT_DIR) {
   throw new Error(
-    'You must define the LIGHTHOUSE_PAGES, LIGHTHOUSE_PREVIOUS_REPORT_DIR and LIGHTHOUSE_REPORT_DIR env variables'
+    'You must define the LIGHTHOUSE_PAGES, LIGHTHOUSE_MOST_RECENT_REPORT_DIR and LIGHTHOUSE_REPORT_DIR env variables'
   );
 }
 
 const pages: string[] = JSON.parse(process.env.LIGHTHOUSE_PAGES);
-const previousReportDir = process.env.LIGHTHOUSE_PREVIOUS_REPORT_DIR;
-const reportDir = process.env.LIGHTHOUSE_REPORT_DIR;
 
 await asyncPool(1, pages, async(pageUrl) => {
   const filename = `${pageUrl.replace(/[^a-z0-9]+/gi, '-')}.json`;
   const targets = await new Promise<ScoreTargets | undefined>(
     (resolve) => readFile(
-      `${process.env.LIGHTHOUSE_PREVIOUS_REPORT_DIR}/${filename}`,
+      `${process.env.LIGHTHOUSE_MOST_RECENT_REPORT_DIR}/${filename}`,
       { encoding: 'utf8' },
       (err, data) => err ? resolve(undefined) : resolve(JSON.parse(data))
     )
