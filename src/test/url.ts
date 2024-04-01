2c4bf0e1fc6f2a6d415985f3d38efd8ed661b917
Dante Soares <dante.m.soares@rice.edu>
2024-04-01 18:31:07 -0500

    Load url function's PORT from src/config instead of jest-puppeteer config

--- src/test/url.ts
+++ src/test/url.ts
@@ -1,3 +1,5 @@
-const url = (path: string) => `http://localhost:${puppeteerConfig.server.port}/${path.replace(/^\/+/, '')}`;
+import { PORT } from '../config';
+
+const url = (path: string) => `http://localhost:${PORT}/${path.replace(/^\/+/, '')}`;
 
 export default url;
