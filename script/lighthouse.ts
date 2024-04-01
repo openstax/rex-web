02befc04707c65759b3d85b25dca707870826aea
Dante Soares <dante.m.soares@rice.edu>
2024-04-01 15:25:12 -0500

    Ensure puppeteer is setup before loading browserutils

--- script/lighthouse.ts
+++ script/lighthouse.ts
@@ -1,32 +1,45 @@
 import { readFile, writeFile } from 'fs';
+import puppeteer from 'puppeteer';
 import asyncPool from 'tiny-async-pool';
 import argv from 'yargs';
-import { checkLighthouse, ScoreTargets } from '../src/test/browserutils';
+import type { ScoreTargets } from '../src/test/browserutils';
 
 const {
   pages, mostRecentReportDir, reportDir,
 } = argv.string('pages').string('mostRecentReportDir').string('reportDir').argv;
 
-if (!pages) { throw new Error('You must specify some --pages to test'); }
+async function run() {
+  if (!pages) { throw new Error('You must specify some --pages to test'); }
 
-const pageArray: string[] = JSON.parse(pages);
+  browser = await puppeteer.launch();
 
-await asyncPool(1, pageArray, async(pageUrl) => {
-  const filename = `${pageUrl.replace(/[^a-z0-9]+/gi, '-')}.json`;
-  const targets = mostRecentReportDir ? await new Promise<ScoreTargets | undefined>(
-    (resolve) => readFile(
-      `${mostRecentReportDir}/${filename}`,
-      { encoding: 'utf8' },
-      (err, data) => err ? resolve(undefined) : resolve(JSON.parse(data))
-    )
-  ) : undefined;
+  // This checks the global browser variable so it needs to occur after it is initialized
+  const { checkLighthouse } = require('../src/test/browserutils');
 
-  const result = await checkLighthouse(browser, pageUrl, targets);
+  const pageArray: string[] = JSON.parse(pages);
 
-  if (reportDir) {
-    await new Promise<void>((resolve) => writeFile(
-      `${reportDir}/${filename}`,
-      JSON.stringify(result), () => resolve()
-    ));
-  }
+  await asyncPool(1, pageArray, async(pageUrl) => {
+    const filename = `${pageUrl.replace(/[^a-z0-9]+/gi, '-')}.json`;
+    const targets = mostRecentReportDir ? await new Promise<ScoreTargets | undefined>(
+      (resolve) => readFile(
+        `${mostRecentReportDir}/${filename}`,
+        { encoding: 'utf8' },
+        (err, data) => err ? resolve(undefined) : resolve(JSON.parse(data))
+      )
+    ) : undefined;
+
+    const result = await checkLighthouse(browser, pageUrl, targets);
+
+    if (reportDir) {
+      await new Promise<void>((resolve) => writeFile(
+        `${reportDir}/${filename}`,
+        JSON.stringify(result), () => resolve()
+      ));
+    }
+  });
+}
+
+run().then(null, (err) => {
+  console.error(err); // tslint:disable-line:no-console
+  process.exit(1);
 });
