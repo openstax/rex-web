b5a765d134af650094b7bb96f9373101a3ca5a1e
Dante Soares <dante.m.soares@rice.edu>
2024-04-02 14:01:40 -0500

    Use correct dir for concourse inputs/outputs

--- concourse/lighthouse/script.bash
+++ concourse/lighthouse/script.bash
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+base_dir=$(pwd)
+
 cd rex-web
 
 yarn install --network-timeout 60000
 
 node script/entry.js lighthouse --pages="$LIGHTHOUSE_PAGES" \
-                                --mostRecentReportDir="$LIGHTHOUSE_MOST_RECENT_REPORT_DIR" \
-                                --reportDir="$LIGHTHOUSE_REPORT_DIR"
+                                --mostRecentReportDir="$base_dir/$LIGHTHOUSE_MOST_RECENT_REPORT_DIR" \
+                                --reportDir="$base_dir/$LIGHTHOUSE_REPORT_DIR"
