74d5ce4c7d27e810efc9fa8e55a017b2b60b389f
Beth Shook <es66@rice.edu>
2024-04-12 12:00:49 -0500

    fix browser redirect for non rex targets

--- src/app/content/utils/processBrowserRedirect.ts
+++ src/app/content/utils/processBrowserRedirect.ts
@@ -1,13 +1,21 @@
 import { History } from 'history';
 import { Redirects } from '../../../../data/redirects/types';
+import { assertWindow } from '../../utils';
 
 export const processBrowserRedirect = async(services: {history: History}) => {
+  const window = assertWindow();
+  console.log('loc', window.location)
   const redirects: Redirects = await fetch('/rex/redirects.json')
     .then((res) => res.json())
     .catch(() => []);
 
+
   for (const {from, to} of redirects) {
     if (from === services.history.location.pathname) {
+      if (!to.startsWith('/books')) {
+        window.location.pathname = to;
+        return true;
+      }
       services.history.replace(to);
       return true;
     }
