cf6af9d6076dfddb39538d07d0ad8d14891bdc29
Roy Johnson <roy.e.johnson@rice.edu>
2024-04-16 15:50:51 -0500

    Simplify!

--- src/app/components/PageTitleConfirmation.tsx
+++ src/app/components/PageTitleConfirmation.tsx
@@ -1,37 +1,27 @@
 import React from 'react';
-import { Element, EventListener } from '@openstax/types/lib.dom';
 
-export default function announcePageTitle(title: string) {
-  const el = document?.getElementById('page-title-confirmation');
-
-  el?.dispatchEvent(
-    new CustomEvent('page-title', {
-      detail: title,
-    })
-  );
+type FunctionWithProperty = {
+  (title: string): void;
+  fn?: (title: string) => void;
 }
+
+const announcePageTitle: FunctionWithProperty = (title: string) => {
+  if (announcePageTitle?.fn) {
+    announcePageTitle.fn(title);
+  }
+}
+
+export default announcePageTitle;
 
 export function PageTitleConfirmation({ className }: { className: string }) {
   const [docTitle, setDocTitle] = React.useState('');
-  const ref = React.useRef<Element | null>(null);
 
-  React.useEffect(() => {
-    const el = ref.current;
-
-    if (!el) { return; }
-    const listener = ((e: CustomEvent) => {
-      setDocTitle(e.detail);
-    }) as EventListener;
-
-    el.addEventListener('page-title', listener);
-    return () => el.removeEventListener('page-title', listener);
-  }, []);
+  announcePageTitle.fn = setDocTitle;
 
   return (
     <div
       id='page-title-confirmation'
       className={className}
-      ref={ref}
       aria-live='polite'
     >
       {`Loaded page "${docTitle}"`}
