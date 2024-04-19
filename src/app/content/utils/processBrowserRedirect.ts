aac5c6922a78e3e66eee37d9cd4c30ac25262679
Beth Shook <beth.shook@gmail.com>
2024-04-19 12:28:29 -0500

    browser redirects to non-rex targets (#2222)
    * fix browser redirect for non rex targets
    
    * remove log
    
    * remove whitespace
    
    * update specs
    
    * lint
    
    * try a thing
    
    * lint and update spec
    
    * fix specs
    
    * tidy
    
    * lint
    
    * add service to prerender script
    
    * remove repetitive bits
    
    * exclude router from appoptions type
    
    * lint
    
    * lint fix
    
    * remove router from test services
    
    * lint fix
    
    * update browser redirect logic and specs
    
    * restore router to test services
    
    * test cleanup
    
    * cleanup
    ;
    
    * lint fix
    
    * pass empty array for test routes
    
    * remove check for redirects before dispatching pageNotFound
    
    * add external route
    
    * fix tests
    
    * lint fix
    
    * update specs and lint fix
    
    ---------
    
    Co-authored-by: Beth Shook <es66@rice.edu>
    Co-authored-by: tom <tom.g.woodward@gmail.com>

--- src/app/content/utils/processBrowserRedirect.ts
+++ src/app/content/utils/processBrowserRedirect.ts
@@ -1,14 +1,22 @@
 import { History } from 'history';
 import { Redirects } from '../../../../data/redirects/types';
+import { RouterService } from '../../navigation/routerService';
+import { Dispatch } from '../../types';
+import { replace } from '../../navigation/actions';
+import { AnyMatch } from '../../navigation/types';
 
-export const processBrowserRedirect = async(services: {history: History}) => {
+export const processBrowserRedirect = async(services: {
+  router: RouterService,
+  history: History,
+  dispatch: Dispatch
+}) => {
   const redirects: Redirects = await fetch('/rex/redirects.json')
     .then((res) => res.json())
     .catch(() => []);
 
   for (const {from, to} of redirects) {
     if (from === services.history.location.pathname) {
-      services.history.replace(to);
+      services.dispatch(replace(services.router.findRoute(to) as AnyMatch));
       return true;
     }
   }
