67712b06629185ebf52b68c2514a41daca2806bc
Dante Soares <dante.m.soares@rice.edu>
2025-01-22 17:42:30 -0600

    Updated ts-utils (but it won't build)

--- src/gateways/createUserLoader.ts
+++ src/gateways/createUserLoader.ts
@@ -1,22 +1,12 @@
-import { ApiUser } from '@openstax/ts-utils/services/authProvider';
 import { browserAuthProvider } from '@openstax/ts-utils/services/authProvider/browser';
 import { assertWindow } from '../app/utils';
-
-// TODO - update tst to include these fields
-export type AccountsUser = ApiUser & {
-  consent_preferences: {
-    accepted: string[],
-    rejected: string[],
-  } | null,
-  self_reported_role: string;
-  using_openstax: boolean;
-};
 
 export default (url: string) => {
   const authProvider = browserAuthProvider({window: assertWindow()})({auth: {accountsBase: url}});
 
   return {
     getAuthorizedFetchConfig: authProvider.getAuthorizedFetchConfig,
-    getCurrentUser: authProvider.getUser as () => Promise<AccountsUser | undefined>,
+    getCurrentUser: authProvider.getUser,
+    updateUser: authProvider.updateUser,
   };
 };
