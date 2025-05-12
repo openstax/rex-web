1909c27bea5b944da48e38221920405d8d1d33e5
Prabhdip Gill <pg53@rice.edu>
2025-05-11 18:39:51 -0700

    Fix linter issues

--- src/app/content/components/Page/MediaModal.tsx
+++ src/app/content/components/Page/MediaModal.tsx
@@ -3,8 +3,11 @@ import styled from 'styled-components/macro';
 
 const Overlay = styled.div`
   position: fixed;
-  top: 0; left: 0; right: 0; bottom: 0;
-  background-color: rgba(0, 0, 0, 0.7);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.9);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,12 +15,16 @@ const Overlay = styled.div`
 `;
 
 const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  vertical-align: middle;
+  align-items: center;
   background-color: white;
   padding: 1rem;
   margin: 2rem;
   border-radius: 1rem;
-  max-width: 90%;
-  max-height: 90%;
+  min-width: 100%;
+  min-height: 90%;
   overflow: auto;
 `;
 
