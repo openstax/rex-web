7150543810c87296c381bc35f2d830c321552bad
Prabhdip Gill <pg53@rice.edu>
2025-06-24 10:49:28 -0700

    Refactor test

--- src/app/content/components/Page/MediaModal.spec.tsx
+++ src/app/content/components/Page/MediaModal.spec.tsx
@@ -3,37 +3,31 @@ import MediaModal from './MediaModal';
 import React from 'react';
 
 describe('MediaModal', () => {
-  const childContent = <div>Test Content</div>;
   const mockClose = jest.fn();
+
+  const renderMediaModal = (isOpen: boolean) =>
+    renderer.create(
+      <MediaModal isOpen={isOpen} onClose={mockClose}>
+        <div>Test Content</div>
+      </MediaModal>
+    );
 
   beforeEach(() => {
     mockClose.mockReset();
   });
 
   it('does not render when isOpen is false', () => {
-    const tree = renderer.create(
-      <MediaModal isOpen={false} onClose={mockClose}>
-        {childContent}
-      </MediaModal>
-    ).toJSON();
+    const tree = renderMediaModal(false).toJSON();
     expect(tree).toBeNull();
   });
 
   it('renders correctly when isOpen is true', () => {
-    const tree = renderer.create(
-      <MediaModal isOpen={true} onClose={mockClose}>
-        {childContent}
-      </MediaModal>
-    ).toJSON();
+    const tree = renderMediaModal(true).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('calls onClose when overlay is clicked', () => {
-    const component = renderer.create(
-      <MediaModal isOpen={true} onClose={mockClose}>
-        {childContent}
-      </MediaModal>
-    );
+    const component = renderMediaModal(true);
 
     const overlay = component.root
     .findAllByType('div')
@@ -50,11 +44,7 @@ describe('MediaModal', () => {
   });
 
   it('calls onClose when close button is clicked', () => {
-    const component = renderer.create(
-      <MediaModal isOpen={true} onClose={mockClose}>
-        {childContent}
-      </MediaModal>
-    );
+    const component = renderMediaModal(true);
 
     const closeButton = component.root.findAllByType('button')[0];
 
