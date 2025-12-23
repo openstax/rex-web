3e3af8397ba11ff962c68277d7a7faf9741c9659
Roy Johnson <roy.e.johnson@rice.edu>
2025-12-23 09:53:40 -0600

    Lint, syntax, test clean-up

--- src/app/content/components/TableOfContents/index.spec.tsx
+++ src/app/content/components/TableOfContents/index.spec.tsx
@@ -9,6 +9,7 @@ import { mockCmsBook } from '../../../../test/mocks/osWebLoader';
 import { renderToDom } from '../../../../test/reactutils';
 import TestContainer from '../../../../test/TestContainer';
 import * as reactUtils from '../../../reactUtils';
+import * as mediaUtils from '../../../mediaQueryUtils';
 import { AppState, Store } from '../../../types';
 import { assertWindow } from '../../../utils';
 import * as actions from '../../actions';
@@ -106,9 +107,9 @@ describe('TableOfContents', () => {
   });
 
   it('opens and closes', () => {
-    jest.spyOn(reactUtils, 'useMatchMobileQuery')
+    jest.spyOn(mediaUtils, 'useMatchMobileQuery')
       .mockReturnValue(true);
-    jest.spyOn(reactUtils, 'useMatchMobileMediumQuery')
+    jest.spyOn(mediaUtils, 'useMatchMobileMediumQuery')
       .mockReturnValue(true);
     const component = renderer.create(Component);
 
@@ -126,7 +127,7 @@ describe('TableOfContents', () => {
   });
 
   it('focuses when opening', () => {
-    jest.spyOn(reactUtils, 'useMatchMobileMediumQuery')
+    jest.spyOn(mediaUtils, 'useMatchMobileMediumQuery')
       .mockReturnValue(true);
 
     const { root } = renderToDom(<TestContainer store={store}>
