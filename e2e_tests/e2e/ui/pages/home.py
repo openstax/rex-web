83fa8e8d0c4abd4abf3f50af8d1f59e1ea67cc02
Ottó <om9@rice.edu>
2026-02-11 20:06:46 -0600

    changing selector12-fail investigation3

--- e2e_tests/e2e/ui/pages/home.py
+++ e2e_tests/e2e/ui/pages/home.py
@@ -581,10 +581,10 @@ class HomeRex:
     async def click_book_details_page_link(self):
         await self.page.get_by_label("Astronomy").click()
 
-    # MIGHT BE REMOVED once failure investigation is completed!
+    # Clears blockers/overlays
+
     @pytest.mark.asyncio
     async def clear_all_blockers(self):
-        # This finds ALL matching divs and removes them from the DOM
         await self.page.evaluate(
             """() => {
             document.querySelectorAll('div[class*="ClickBlocker"]').forEach(el => el.remove());
