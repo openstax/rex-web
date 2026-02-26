05f768d01208e142167ae393330fd5c50bf79c64
Ottó <om9@rice.edu>
2026-02-26 16:31:48 -0600

    adds new test for study guides

---
+++ e2e_tests/e2e/ui/pages/studyguides.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+class StudyGuides:
+    def __init__(self, page):
+        self.page = page
+
+    @property
+    def study_guides_icon(self):
+        return self.page.get_by_label("Guides")
+
+    @property
+    def study_guides_page(self):
+        return self.page.get_by_test_id("show-studyguides-body")
+
+    @property
+    def study_guides_other_filters(self):
+        return self.page.get_by_label("Applied filters")
+
+    @property
+    def study_guides_filter_by_chapter(self):
+        return self.page.get_by_label("Filter study guides by Chapter")
+
+    @property
+    def study_guides_filter_by_color(self):
+        return self.page.get_by_label("Filter study guides by Color")
+
+    @property
+    def study_guides_filter_by_chapter_dropdown(self):
+        return self.page.get_by_role("heading", name="The History of Management")
+
+    @property
+    def study_guides_filter_by_color_dropdown(self):
+        return self.page.locator("#guide-filter-color")
