25407bab1d2233f3e2827284863beedbe5a7f072
Ottó <om9@rice.edu>
2026-03-03 16:32:46 -0600

    additions and fixes as per fails

--- e2e_tests/e2e/osweb/test_osweb_homepage_links.py
+++ e2e_tests/e2e/osweb/test_osweb_homepage_links.py
@@ -1,21 +1,24 @@
 import pytest
 
+from e2e_tests.e2e.ui.fixtures.ui import chrome_page_unlogged
 from e2e_tests.e2e.ui.pages.home import HomeRex
 
 
 @pytest.mark.asyncio
-async def test_osweb_homepage_interested_link(chrome_page, base_url):
+async def test_osweb_homepage_interested_link(chrome_page_unlogged, base_url):
 
     # GIVEN: Playwright, chromium and the rex_base_url
 
     # WHEN: The Home page is fully loaded
-    await chrome_page.goto(base_url)
-    home = HomeRex(chrome_page)
+    await chrome_page_unlogged.goto(base_url)
+    home = HomeRex(chrome_page_unlogged)
+
+    await chrome_page_unlogged.keyboard.press("Escape")
 
     # THEN: I'm interested page opens
     await home.click_interested_link()
 
-    assert "interest" in chrome_page.url
+    assert "interest" in chrome_page_unlogged.url
 
     await home.click_iam_dropdown_in_interested()
 
@@ -27,20 +30,22 @@ async def test_osweb_homepage_interested_link(chrome_page, base_url):
 
 
 @pytest.mark.asyncio
-async def test_osweb_homepage_try_assignable_link(chrome_page, base_url):
+async def test_osweb_homepage_try_assignable_link(chrome_page_unlogged, base_url):
 
     # GIVEN: Playwright, chromium and the rex_base_url
 
     # WHEN: The Home page is fully loaded
-    await chrome_page.goto(base_url)
-    home = HomeRex(chrome_page)
+    await chrome_page_unlogged.goto(base_url)
+    home = HomeRex(chrome_page_unlogged)
+
+    await chrome_page_unlogged.keyboard.press("Escape")
 
     # THEN: Try OpenStax Assignable page opens
     await home.click_try_assignable_link()
 
-    assert "assignable" in chrome_page.url
+    assert "assignable" in chrome_page_unlogged.url
 
-    if "staging" not in chrome_page.url:
+    if "staging" not in chrome_page_unlogged.url:
         # THEN: Number of books available in assignables is 31 (as of Feb. 2026)
         assert await home.available_book_list() >= 31
 
