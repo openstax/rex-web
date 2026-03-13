ce838c2bbde8e2c8c1c71ff9bfdaafacc5d92bfa
OpenStaxClaude <tw33+clauderunner@rice.edu>
2026-03-13 19:25:53 +0000

    Migrate Details.tsx icons to inline SVG
    - Replace styled-icons CaretRight and CaretDown with inline SVG components
    - Add SVG paths from Font Awesome Free (MIT License)
    - Use currentColor for fill to allow CSS color control
    - Maintain aria-hidden attribute for accessibility
    - Icon sizing controlled by existing CSS (.details-expand-icon, .details-collapse-icon)
    - Legacy styled-icons exports remain in Details.legacy.ts for backward compatibility

--- src/app/components/Details.tsx
+++ src/app/components/Details.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import classNames from 'classnames';
-import { CaretDown } from 'styled-icons/fa-solid/CaretDown';
-import { CaretRight } from 'styled-icons/fa-solid/CaretRight';
 import { HTMLDetailsElement } from '@openstax/types/lib.dom';
 import '../../polyfill/details';
 // Note: Details.css is imported globally from src/app/index.tsx to ensure consistent
@@ -9,17 +7,49 @@ import '../../polyfill/details';
 
 export const iconSize = 1.7;
 
-interface IconProps extends React.SVGAttributes<SVGElement> {
+interface IconProps extends React.SVGAttributes<SVGSVGElement> {
   className?: string;
   size?: number | string;
 }
 
+/**
+ * Expand icon (caret-right) for Details component.
+ * SVG path from Font Awesome Free (https://fontawesome.com - MIT License)
+ */
 export function ExpandIcon({ className, ...props }: IconProps) {
-  return <CaretRight className={classNames('details-expand-icon', className)} {...props} />;
+  return (
+    <svg
+      className={classNames('details-expand-icon', className)}
+      viewBox="0 0 256 512"
+      aria-hidden="true"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+      />
+    </svg>
+  );
 }
 
+/**
+ * Collapse icon (caret-down) for Details component.
+ * SVG path from Font Awesome Free (https://fontawesome.com - MIT License)
+ */
 export function CollapseIcon({ className, ...props }: IconProps) {
-  return <CaretDown className={classNames('details-collapse-icon', className)} {...props} />;
+  return (
+    <svg
+      className={classNames('details-collapse-icon', className)}
+      viewBox="0 0 320 512"
+      aria-hidden="true"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+      />
+    </svg>
+  );
 }
 
 export function Summary({
