e3e71b0c9c0e4556fc3133e638169e4121054682
Roy Johnson <roy.e.johnson@rice.edu>
2026-07-30 15:52:14 -0500

    Dropdown still needs styled for now

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -5,6 +5,7 @@ import isUndefined from 'lodash/fp/isUndefined';
 import omitBy from 'lodash/fp/omitBy';
 import React, { ReactNode } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
+import styled from 'styled-components/macro';
 import classNames from 'classnames';
 import { useFocusLost, useTrapTabNavigation, focusableItemQuery } from '../reactUtils';
 import { useOnEsc } from '../reactUtils';
